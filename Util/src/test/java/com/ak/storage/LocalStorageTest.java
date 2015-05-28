3a07a42d8fe3373028a743a624507955164c9aa3
Alexander Kobelev <ak.mail.ru@gmail.com>
2015-05-28 07:12:14 +0300

    #F-13 Fixed Current-Carrying Electrode Size

--- Util/src/test/java/com/ak/storage/LocalStorageTest.java
+++ Util/src/test/java/com/ak/storage/LocalStorageTest.java
@@ -5,12 +5,32 @@ import org.testng.annotations.Test;
 
 public final class LocalStorageTest {
   @Test
-  public void testStorage() {
+  public void testBooleanStorage() {
     Storage<Boolean> storage = new LocalStorage<>(LocalStorageTest.class.getSimpleName(),
         "testBooleanStorage", Boolean.class);
     for (boolean b : new boolean[] {true, false}) {
       storage.save(b);
       Assert.assertEquals(storage.get().booleanValue(), b);
+    }
+  }
+
+  @Test
+  public void testIntegerStorage() {
+    Storage<Integer> storage = new LocalStorage<>(LocalStorageTest.class.getSimpleName(),
+        "testIntStorage", Integer.class);
+    for (int n : new int[] {1, 12}) {
+      storage.save(n);
+      Assert.assertEquals(storage.get().intValue(), n);
+    }
+  }
+
+  @Test
+  public void testStringStorage() {
+    Storage<String> storage = new LocalStorage<>(LocalStorageTest.class.getSimpleName(),
+        "testStringStorage", String.class);
+    for (String s : new String[] {"name", "last"}) {
+      storage.save(s);
+      Assert.assertEquals(storage.get(), s);
     }
   }
 }
