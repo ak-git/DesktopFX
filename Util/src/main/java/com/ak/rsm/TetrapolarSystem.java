607e0354936b304e729bf76913459d40af9a0426
Alexander Kobelev <ak.mail.ru@gmail.com>
2015-09-26 19:46:09 +0300

    #F-14 In Progress Resistivity Sounding Measurements

---
+++ Util/src/main/java/com/ak/rsm/TetrapolarSystem.java
@@ -0,0 +1,73 @@
+package com.ak.rsm;
+
+import java.util.Objects;
+
+import javax.measure.Quantity;
+import javax.measure.Unit;
+import javax.measure.quantity.ElectricResistance;
+import javax.measure.quantity.Length;
+
+import tec.uom.se.quantity.Quantities;
+import tec.uom.se.unit.Units;
+
+public final class TetrapolarSystem implements Cloneable {
+  private final Quantity<Length> sPotentialUnitSI;
+  private final Quantity<Length> lCurrentCarringSI;
+
+  public TetrapolarSystem(double sPU, double lCC, Unit<Length> unit) {
+    if (sPU >= lCC) {
+      throw new IllegalArgumentException();
+    }
+    sPotentialUnitSI = Quantities.getQuantity(sPU, unit).to(Units.METRE);
+    lCurrentCarringSI = Quantities.getQuantity(lCC, unit).to(Units.METRE);
+  }
+
+  /**
+   * Gets <b>apparent</b> specific resistance which is correspond to 1-layer model.
+   *
+   * @param resistance in Ohms.
+   * @return <b>apparent</b> specific resistance in Ohm-m.
+   */
+  public double getApparent(Quantity<ElectricResistance> resistance) {
+    double sSI = sPotentialUnitSI.getValue().doubleValue();
+    double lSI = lCurrentCarringSI.getValue().doubleValue();
+    return 0.5 * Math.PI * resistance.to(Units.OHM).getValue().doubleValue() / (1.0 / (lSI - sSI) - 1.0 / (lSI + sSI));
+  }
+
+  public Quantity<Length> getS() {
+    return sPotentialUnitSI;
+  }
+
+  public Quantity<Length> getL() {
+    return lCurrentCarringSI;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof TetrapolarSystem)) {
+      return false;
+    }
+
+    TetrapolarSystem that = (TetrapolarSystem) o;
+    return Objects.equals(sPotentialUnitSI, that.sPotentialUnitSI) && Objects.equals(lCurrentCarringSI, that.lCurrentCarringSI);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(sPotentialUnitSI, lCurrentCarringSI);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s x %s", sPotentialUnitSI, lCurrentCarringSI);
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    throw new CloneNotSupportedException();
+  }
+}
+
---
+++ Util/src/test/java/com/ak/rsm/TetrapolarSystemTest.java
@@ -0,0 +1,43 @@
+package com.ak.rsm;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.ElectricResistance;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import tec.uom.se.quantity.Quantities;
+
+import static tec.uom.se.unit.MetricPrefix.MILLI;
+import static tec.uom.se.unit.Units.METRE;
+import static tec.uom.se.unit.Units.OHM;
+
+public final class TetrapolarSystemTest {
+  @DataProvider(name = "tetrapolar-systems", parallel = true)
+  public Object[][] tetrapolarSystems() {
+    return new Object[][] {
+        {new TetrapolarSystem(1.0, 2.0, METRE), new TetrapolarSystem(1000.0, 2000.0, MILLI(METRE)), true},
+        {new TetrapolarSystem(1.0, 2.0, MILLI(METRE)), new TetrapolarSystem(1.0, 3.0, MILLI(METRE)), false}
+    };
+  }
+
+  @DataProvider(name = "tetrapolar-systems-2", parallel = true)
+  public Object[][] tetrapolarSystems2() {
+    return new Object[][] {
+        {new TetrapolarSystem(0.030, 0.06, METRE), Quantities.getQuantity(1000, MILLI(OHM)), Math.PI * 9.0 / 400.0},
+        {new TetrapolarSystem(30.0, 90.0, MILLI(METRE)), Quantities.getQuantity(1.0 / Math.PI, OHM), 3.0 / 50.0},
+        {new TetrapolarSystem(40.0, 80.0, MILLI(METRE)), Quantities.getQuantity(1.0 / Math.PI, OHM), 3.0 / 100.0},
+    };
+  }
+
+  @Test(dataProvider = "tetrapolar-systems")
+  public void testEquals(TetrapolarSystem system1, TetrapolarSystem system2, boolean equals) {
+    Assert.assertEquals(system1.equals(system2), equals, String.format("%s compared with %s", system1, system2));
+  }
+
+  @Test(dataProvider = "tetrapolar-systems-2")
+  public void testApparentResistivity(TetrapolarSystem system, Quantity<ElectricResistance> resistance,
+                                      double specificResistance) {
+    Assert.assertEquals(system.getApparent(resistance), specificResistance, 1.0e-6);
+  }
+}
