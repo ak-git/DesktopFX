172b5e45ad16cb1a9b377a0e8f3d128213a6de91
Alexander Kobelev <ak.mail.ru@gmail.com>
2015-12-03 21:34:46 +0300

    #F-28 Fixed Test case for com.ak.util.LineFileCollector

--- Util/src/test/java/com/ak/util/LineFileCollectorTest.java
+++ Util/src/test/java/com/ak/util/LineFileCollectorTest.java
@@ -11,8 +11,8 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -22,12 +22,12 @@ public class LineFileCollectorTest {
   private LineFileCollectorTest() {
   }
 
-  @BeforeMethod
+  @BeforeClass
   public void setUp() {
     out = Paths.get(LineFileCollectorTest.class.getName() + ".txt");
   }
 
-  @AfterMethod
+  @AfterClass
   public void tearDown() throws IOException {
     Files.deleteIfExists(out);
   }
