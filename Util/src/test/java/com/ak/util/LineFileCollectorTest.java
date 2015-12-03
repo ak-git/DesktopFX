150ca04bb06473ccd8efb8afc1279fdd16a68602
Alexander Kobelev <ak.mail.ru@gmail.com>
2015-12-03 21:21:43 +0300

    #F-28 Fixed Test case for com.ak.util.LineFileCollector

---
+++ Util/src/test/java/com/ak/util/LineFileCollectorTest.java
@@ -0,0 +1,60 @@
+package com.ak.util;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LineFileCollectorTest {
+  private Path out;
+
+  private LineFileCollectorTest() {
+  }
+
+  @BeforeMethod
+  public void setUp() {
+    out = Paths.get(LineFileCollectorTest.class.getName() + ".txt");
+  }
+
+  @AfterMethod
+  public void tearDown() throws IOException {
+    Files.deleteIfExists(out);
+  }
+
+  @DataProvider(name = "stream")
+  public static Object[][] intStream() {
+    return new Object[][] {
+        {(Supplier<Stream<String>>) () -> IntStream.rangeClosed(-1, 1).mapToObj(value -> String.format("%d", value))}
+    };
+  }
+
+  @Test(dataProvider = "stream")
+  public void testVertical(Supplier<Stream<String>> stream) throws IOException {
+    stream.get().collect(new LineFileCollector<>(out, LineFileCollector.Direction.VERTICAL));
+    Assert.assertTrue(Files.readAllLines(out, Charset.forName("windows-1251")).stream().collect(Collectors.joining()).
+        equals(stream.get().collect(Collectors.joining())));
+  }
+
+  @Test(dataProvider = "stream")
+  public void testHorizontal(Supplier<Stream<String>> stream) throws IOException {
+    stream.get().collect(new LineFileCollector<>(out, LineFileCollector.Direction.HORIZONTAL));
+    Assert.assertTrue(Files.readAllLines(out, Charset.forName("windows-1251")).stream().collect(Collectors.joining()).
+        equals(stream.get().collect(Collectors.joining("\t"))));
+  }
+
+  @Test(expectedExceptions = UnsupportedOperationException.class)
+  public void testCombiner() {
+    new LineFileCollector<>(out, LineFileCollector.Direction.HORIZONTAL).combiner().apply(null, null);
+  }
+}
