5a061e51818d58c9f347fd0bd92b830df2ff9955
Alexander Kobelev <ak.mail.ru@gmail.com>
2016-05-15 15:38:52 +0300

    Аппроксимация эллипсом для Сафоновой Реализовано искажение координат

--- Util/src/main/java/com/ak/eye/CircleInequality.java
+++ Util/src/main/java/com/ak/eye/CircleInequality.java
@@ -2,19 +2,18 @@ package com.ak.eye;
 
 import java.util.function.DoubleUnaryOperator;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import java.util.stream.Stream;
 
 public final class CircleInequality implements DoubleUnaryOperator {
-  private final Iterable<Point> points;
+  private final Stream<Point> points;
 
-  public CircleInequality(Iterable<Point> points) {
+  public CircleInequality(Stream<Point> points) {
     this.points = points;
   }
 
   @Override
   public double applyAsDouble(double r) {
-    return StreamSupport.stream(points.spliterator(), true).
-        map(point -> StrictMath.pow(r - StrictMath.hypot(point.x(), point.y()), 2)).
+    return points.map(point -> StrictMath.pow(r - StrictMath.hypot(point.x(), point.y()), 2)).
         collect(Collectors.averagingDouble(Double::doubleValue));
   }
 }
--- Util/src/test/java/com/ak/math/EyeTest.java
+++ Util/src/test/java/com/ak/math/EyeTest.java
@@ -1,8 +1,8 @@
 package com.ak.math;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.DoubleUnaryOperator;
+import java.util.stream.StreamSupport;
 
 import com.ak.eye.CircleInequality;
 import com.ak.eye.Point;
@@ -16,28 +16,33 @@ import org.apache.commons.math3.optim.nonlinear.scalar.ObjectiveFunction;
 import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.NelderMeadSimplex;
 import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizer;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class EyeTest {
   private EyeTest() {
   }
 
-  @Test
-  public void testCircleInequality() {
-    Iterable<Point> points = Arrays.asList(new Point(2.0, 0.0), new Point(-1.0, 0.0));
-    DoubleUnaryOperator inequality = new CircleInequality(points);
-    Assert.assertEquals(inequality.applyAsDouble(2.0), 1.0 / 2.0, Float.MIN_NORMAL);
-    Assert.assertEquals(inequality.applyAsDouble(1.0), 1.0 / 2.0, Float.MIN_NORMAL);
-    Assert.assertEquals(inequality.applyAsDouble(3.0), (1.0 + 4.0) / 2.0, Float.MIN_NORMAL);
-    Assert.assertEquals(inequality.applyAsDouble(4.0), (4.0 + 9.0) / 2.0, Float.MIN_NORMAL);
-    Assert.assertEquals(inequality.applyAsDouble(5.0), (9.0 + 16.0) / 2.0, Float.MIN_NORMAL);
+  @DataProvider(name = "circle", parallel = true)
+  public static Object[][] singleLayerParameters() {
+    return new Object[][] {
+        {Arrays.asList(new Point(2.0, 0.0), new Point(-1.0, 0.0)), 2.0, 1.0 / 2.0},
+        {Arrays.asList(new Point(2.0, 0.0), new Point(-1.0, 0.0)), 5.0, (9.0 + 16.0) / 2.0},
+        {Arrays.asList(new Point(2.0, 0.0), new Point(-1.0, 0.0), new Point(1.0, 0.0)), 1.0, 1.0 / 3.0},
+    };
+  }
+
+  @Test(dataProvider = "circle")
+  public void testCircleInequality(Iterable<Point> points, double radiusEstimate, double error) {
+    DoubleUnaryOperator inequality = new CircleInequality(StreamSupport.stream(points.spliterator(), true));
+    Assert.assertEquals(inequality.applyAsDouble(radiusEstimate), error, Float.MIN_NORMAL);
   }
 
   @Test
   public static void testRosenbrockNelderMeadSimplex() {
     SimplexOptimizer optimizer = new SimplexOptimizer(-1, 1.0e-3);
     PointValuePair optimum = optimizer.optimize(new MaxEval(100), new ObjectiveFunction(new Ellipse()),
-        GoalType.MINIMIZE, new NelderMeadSimplex(1, 0.1), new InitialGuess(new double[] {1.0})
+        GoalType.MINIMIZE, new NelderMeadSimplex(2, 0.1), new InitialGuess(new double[] {1.0, 1.0})
     );
     System.out.println(Arrays.toString(optimum.getPoint()));
   }
@@ -45,8 +50,10 @@ public class EyeTest {
   private static class Ellipse implements MultivariateFunction {
     @Override
     public double value(double[] x) {
-      List<Point> points = PointLoader.INSTANCE.getPoints();
-      return new CircleInequality(points).applyAsDouble(x[0]);
+      double r = x[0];
+      double transform = Math.sqrt(x[1]);
+      return new CircleInequality(StreamSupport.stream(PointLoader.INSTANCE.getPoints().spliterator(), true).
+          map(point -> new Point(point.x() * transform, point.y() / transform))).applyAsDouble(r);
     }
   }
 }
