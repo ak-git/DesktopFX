945f88a09786178af4636e7bd2cd535ba2d94134
Alexander Kobelev <ak.mail.ru@gmail.com>
2016-06-28 08:08:16 +0300

    Аппроксимация эллипсом для Сафоновой Найден центр и угол поворота эллипса

--- Util/src/test/java/com/ak/math/EyeTest.java
+++ Util/src/test/java/com/ak/math/EyeTest.java
@@ -11,11 +11,18 @@ import javax.annotation.Nonnull;
 
 import javafx.util.Builder;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static java.lang.Math.toRadians;
 import static java.lang.StrictMath.cos;
+import static java.lang.StrictMath.pow;
 import static java.lang.StrictMath.sin;
 
 public class EyeTest {
@@ -36,16 +43,48 @@ public class EyeTest {
   public void testCircle(List<Vector2D> points, double cx, double cy, double radius) {
   }
 
-  @DataProvider(name = "ellipse", parallel = true)
+  @DataProvider(name = "ellipse")
   public static Object[][] ellipse() {
     return new Object[][] {
-        {new EllipsePoints(45).transform(0.5 * 0.5).build(), 0.0, -3.4},
-        {new EllipsePoints(1).transform(0.5 * 0.5).build(), 0.0, -4.0},
+        {new EllipsePoints(1).transform(0.5 * 0.5).rotate(30).move(10.0, -1.0).build()},
+        {new EllipsePoints(1).transform(0.5 * 0.5).move(10.0, -1.0).rotate(30).build()},
+        {new EllipsePoints(1).transform(0.5 * 0.5).move(10, 10).rotate(45).build()},
+        {new EllipsePoints(1).build()},
     };
   }
 
   @Test(dataProvider = "ellipse")
-  public void testEllipse(List<Vector2D> points, double cx, double cy) {
+  public void testEllipse(@Nonnull List<Vector2D> points) {
+    if (points.size() < 5) {
+      throw new IllegalArgumentException(points.toString());
+    }
+    double[][] coeff = new double[points.size()][5];
+    for (int i = 0; i < points.size(); i++) {
+      double x = points.get(i).getX();
+      double y = points.get(i).getY();
+      coeff[i][0] = pow(x, 2.0);
+      coeff[i][1] = 2.0 * x * y;
+      coeff[i][2] = pow(y, 2.0);
+      coeff[i][3] = 2.0 * x;
+      coeff[i][4] = 2.0 * y;
+    }
+    RealMatrix matrix = new Array2DRowRealMatrix(coeff, false);
+    DecompositionSolver solver = new SingularValueDecomposition(matrix).getSolver();
+
+    double[] b = new double[points.size()];
+    Arrays.fill(b, -1.0);
+    RealVector solution = solver.solve(new ArrayRealVector(b, false));
+    double a11 = solution.getEntry(0);
+    double a12 = solution.getEntry(1);
+    double a22 = solution.getEntry(2);
+    double a13 = solution.getEntry(3);
+    double a23 = solution.getEntry(4);
+
+    double phi = StrictMath.atan(2.0 * a12 / (a11 - a22)) / 2.0;
+    double d = a11 * a22 - a12 * a12;
+    double x0 = -(a13 * a22 - a12 * a23) / d;
+    double y0 = -(a11 * a23 - a13 * a12) / d;
+    System.out.printf("%.1f (%.1f; %.1f) %n", Math.toDegrees(phi), x0, y0);
   }
 }
 
@@ -69,6 +108,17 @@ class EllipsePoints implements Builder<List<Vector2D>> {
     pointStream = pointStream.map(point -> {
       double k = Math.sqrt(bToa);
       return new Vector2D(point.getX() / k, point.getY() * k);
+    });
+    return this;
+  }
+
+  @Nonnull
+  EllipsePoints rotate(double angleGrad) {
+    pointStream = pointStream.map(point -> {
+      double x = point.getX();
+      double y = point.getY();
+      double theta = toRadians(angleGrad);
+      return new Vector2D(x * cos(theta) - y * sin(theta), x * sin(theta) + y * cos(theta));
     });
     return this;
   }
