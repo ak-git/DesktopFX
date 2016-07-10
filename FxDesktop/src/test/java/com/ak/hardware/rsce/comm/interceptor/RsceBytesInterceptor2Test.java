4daf0a26b985c503d9edc819a2291fa17557c571
Alexander Kobelev <ak.mail.ru@gmail.com>
2016-07-10 08:55:13 +0300

    #F-45 Fixed Add findbugs checking: add test cases

--- FxDesktop/src/test/java/com/ak/hardware/rsce/comm/interceptor/RsceBytesInterceptor2Test.java
+++ FxDesktop/src/test/java/com/ak/hardware/rsce/comm/interceptor/RsceBytesInterceptor2Test.java
@@ -37,7 +37,55 @@ public final class RsceBytesInterceptor2Test {
         {new byte[] {0x01, 0x03, 0x04, 0x21, 0x33}, RsceCommandFrame.simple(CATCH, STATUS_I_SPEED_ANGLE)},
         {new byte[] {0x01, 0x08, 0x03, 0x01, 0x09, 0x0F, (byte) 0x9F, (byte) 0xA2, (byte) 0xEE, 0x22},
             new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_ANGLE).
-                addParam((byte) 0x01).addParam((short) 3849).addParam((short) 41631).build()}
+                addParam((byte) 0x01).addParam((short) 3849).addParam((short) 41631).build()},
+
+        {new byte[] {0x01, 0x05, 0x0C, 0x00, 0x00, (byte) 0xD9, 0x0F}, RsceCommandFrame.precise(CATCH, STATUS_I_SPEED_ANGLE, (short) 0)},
+        {new byte[] {0x01, 0x05, 0x0C, 0x20, 0x4E, 0x40, (byte) 0xFB}, RsceCommandFrame.precise(CATCH, STATUS_I_SPEED_ANGLE, (short) 20000)},
+        {new byte[] {0x01, 0x05, 0x0C, (byte) 0xE0, (byte) 0xB1, 0x50, (byte) 0xBB}, RsceCommandFrame.precise(CATCH, STATUS_I_SPEED_ANGLE, (short) 45536)},
+        {new byte[] {0x01, 0x05, 0x0C, (byte) 0xA0, 0x0F, (byte) 0xE1, 0x0B}, RsceCommandFrame.precise(CATCH, STATUS_I_SPEED_ANGLE, (short) 4000)},
+
+        {new byte[] {0x01, 0x06, 0x01, 0x01, 0x09, 0x0F, (byte) 0x9F, (byte) 0xA2},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I).addParam((byte) 0x01).addParam((short) 3849).build()},
+        {new byte[] {0x01, 0x06, 0x01, 0x02, (byte) 0xD9, 0x0F, 0x32, 0x62},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I).addParam((byte) 0x02).addParam((short) 4057).build()},
+        {new byte[] {0x01, 0x06, 0x01, 0x03, 0x40, (byte) 0xFB, 0x08, 0x75},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I).addParam((byte) 0x03).addParam((short) 64320).build()},
+        {new byte[] {0x01, 0x06, 0x01, 0x04, (byte) 0xE0, (byte) 0xB1, 0x40, 0x43},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I).addParam((byte) 0x04).addParam((short) 45536).build()},
+
+        {new byte[] {0x01, 0x08, 0x02, 0x01, 0x09, 0x0F, (byte) 0x9F, (byte) 0xA2, (byte) 0xEF, (byte) 0xF3},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_SPEED).addParam((byte) 0x01).addParam((short) 3849).addParam((short) 41631).build()},
+        {new byte[] {0x01, 0x08, 0x02, 0x02, (byte) 0xD9, 0x0F, 0x03, 0x00, 0x7B, (byte) 0x8A},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_SPEED).addParam((byte) 0x02).addParam((short) 4057).addParam((short) 3).build()},
+        {new byte[] {0x01, 0x08, 0x02, 0x03, 0x40, (byte) 0xFB, 0x00, 0x75, (byte) 0xE8, 0x33},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_SPEED).addParam((byte) 0x03).addParam((short) 64320).addParam((short) 29952).build()},
+        {new byte[] {0x01, 0x08, 0x01, 0x04, (byte) 0xE0, (byte) 0xB1, 0x40, 0x43, (byte) 0xEF, (byte) 0xC0},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I).addParam((byte) 0x04).addParam((short) 45536).addParam((short) 17216).build()},
+
+        {new byte[] {0x01, 0x08, 0x03, 0x01, 0x09, 0x0F, (byte) 0x9F, (byte) 0xA2, (byte) 0xEE, 0x22},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_ANGLE).addParam((byte) 0x01).addParam((short) 3849).addParam((short) 41631).build()},
+        {new byte[] {0x01, 0x08, 0x03, 0x02, (byte) 0xD9, 0x0F, 0x03, 0x00, 0x7A, 0x5B},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_ANGLE).addParam((byte) 0x02).addParam((short) 4057).addParam((short) 3).build()},
+        {new byte[] {0x01, 0x08, 0x03, 0x03, 0x40, (byte) 0xFB, 0x00, 0x75, (byte) 0xE9, (byte) 0xE2},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_ANGLE).addParam((byte) 0x03).addParam((short) 64320).addParam((short) 29952).build()},
+        {new byte[] {0x01, 0x08, 0x03, 0x04, (byte) 0xE0, (byte) 0xB1, 0x40, 0x43, (byte) 0xEE, 0x22},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_ANGLE).addParam((byte) 0x04).addParam((short) 45536).addParam((short) 17216).build()},
+
+        {new byte[] {0x01, 0x0a, 0x04, 0x01, 0x09, 0x0F, (byte) 0x9F, (byte) 0xA2, (byte) 0xEE, 0x22, 0x58, (byte) 0x86},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_SPEED_ANGLE).addParam((byte) 0x01).
+                addParam((short) 3849).addParam((short) 41631).addParam((short) 8942).build()},
+        {new byte[] {0x01, 0x0a, 0x04, 0x02, (byte) 0xD9, 0x0F, 0x03, 0x00, 0x7A, 0x5B, 0x58, (byte) 0x86},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_SPEED_ANGLE).addParam((byte) 0x02).
+                addParam((short) 4057).addParam((short) 3).addParam((short) 23418).build()},
+        {new byte[] {0x01, 0x0a, 0x04, 0x03, 0x40, (byte) 0xFB, 0x00, 0x75, (byte) 0xE9, (byte) 0xE2, 0x58, (byte) 0x86},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_SPEED_ANGLE).addParam((byte) 0x03).
+                addParam((short) 64320).addParam((short) 29952).addParam((short) 58089).build()},
+        {new byte[] {0x01, 0x0a, 0x04, 0x04, (byte) 0xE0, (byte) 0xB1, 0x40, 0x43, (byte) 0xEE, 0x22, 0x58, (byte) 0x86},
+            new RsceCommandFrame.Builder(CATCH, NONE, STATUS_I_SPEED_ANGLE).addParam((byte) 0x04).
+                addParam((short) 45536).addParam((short) 17216).addParam((short) 8942).build()},
+
+        //fake bytes
+        {new byte[] {0x00, 0x01}, RsceCommandFrame.off(CATCH)}
     };
   }
 
