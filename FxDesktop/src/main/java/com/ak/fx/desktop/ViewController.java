4240632cff11d5562de89828f17b32540ea6cef5
Alexander Kobelev <ak.mail.ru@gmail.com>
2016-11-16 21:19:04 +0300

    #F-53 In Progress Update RxJava library to 2.0: fix duplicated MilliGrid instance

--- FxDesktop/src/main/java/com/ak/fx/desktop/ViewController.java
+++ FxDesktop/src/main/java/com/ak/fx/desktop/ViewController.java
@@ -4,15 +4,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.ak.fx.scene.MilliGrid;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public final class ViewController implements Initializable {
-  @Nonnull
+  @Nullable
   @FXML
-  public MilliGrid root = new MilliGrid();
+  public MilliGrid root;
 
   @Override
   public void initialize(@Nonnull URL location, @Nonnull ResourceBundle resources) {
