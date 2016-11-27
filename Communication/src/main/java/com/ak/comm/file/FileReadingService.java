51c6d71f08c037761c5f3fd80084b4b37c83b812
Alexander Kobelev <ak.mail.ru@gmail.com>
2016-11-27 14:56:31 +0300

    #F-53 In Progress Update RxJava library to 2.0: create FileReadingService.java

---
+++ Communication/src/main/java/com/ak/comm/file/FileReadingService.java
@@ -0,0 +1,61 @@
+package com.ak.comm.file;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.Nonnull;
+
+import io.reactivex.internal.subscriptions.EmptySubscription;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+final class FileReadingService implements Publisher<ByteBuffer> {
+  private static final int CAPACITY_4K = 1024 * 4;
+  @Nonnull
+  private final Path fileToRead;
+
+  FileReadingService(@Nonnull Path fileToRead) {
+    Objects.requireNonNull(fileToRead);
+    this.fileToRead = fileToRead;
+  }
+
+  @Override
+  public void subscribe(@Nonnull Subscriber<? super ByteBuffer> s) {
+    s.onSubscribe(EmptySubscription.INSTANCE);
+    if (Files.isRegularFile(fileToRead, LinkOption.NOFOLLOW_LINKS)) {
+      try (ReadableByteChannel readableByteChannel = Files.newByteChannel(fileToRead, StandardOpenOption.READ)) {
+        Logger.getLogger(getClass().getName()).log(Level.INFO, String.format("#%x Open file [ %s ]", hashCode(), fileToRead));
+        ByteBuffer buffer = ByteBuffer.allocate(CAPACITY_4K);
+        while (readableByteChannel.read(buffer) > 0) {
+          buffer.flip();
+          s.onNext(buffer);
+          buffer.clear();
+        }
+        s.onComplete();
+      }
+      catch (Exception e) {
+        Logger.getLogger(getClass().getName()).log(Level.WARNING, fileToRead.toString(), e);
+        s.onError(e);
+      }
+      finally {
+        Logger.getLogger(getClass().getName()).log(Level.INFO, "Close file " + fileToRead);
+      }
+    }
+    else {
+      s.onComplete();
+      Logger.getLogger(getClass().getName()).log(Level.CONFIG, String.format("File [ %s ] is not a regular file", fileToRead));
+    }
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s@%x{file = %s}", getClass().getSimpleName(), hashCode(), fileToRead);
+  }
+}
---
+++ Communication/src/test/java/com/ak/comm/file/FileReadingServiceTest.java
@@ -0,0 +1,59 @@
+package com.ak.comm.file;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+import com.ak.logging.BinaryLogBuilder;
+import com.ak.logging.LocalFileHandler;
+import io.reactivex.Flowable;
+import io.reactivex.subscribers.TestSubscriber;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public final class FileReadingServiceTest {
+  private static final int KILO_BYTE = 1024;
+
+  @DataProvider(name = "files")
+  public Object[][] simple() throws IOException {
+    return new Object[][] {
+        {createFile(-1), 0},
+        {createFile(0), 0},
+        {createFile(14), 14 / 4 + 1}
+    };
+  }
+
+  @Test(dataProvider = "files")
+  public void testFile(@Nonnull Path fileToRead, @Nonnegative int valueCount) throws Exception {
+    TestSubscriber<ByteBuffer> testSubscriber = TestSubscriber.create();
+    Flowable.fromPublisher(new FileReadingService(fileToRead)).subscribe(testSubscriber);
+    testSubscriber.assertValueCount(valueCount);
+    testSubscriber.assertComplete();
+    testSubscriber.assertNoErrors();
+    Files.deleteIfExists(fileToRead);
+  }
+
+  private Path createFile(int kBytes) throws IOException {
+    Path path = new BinaryLogBuilder(String.format("%s %d bytes", getClass().getSimpleName(), kBytes), LocalFileHandler.class).build().getPath();
+    if (kBytes >= 0) {
+      try (WritableByteChannel channel = Files.newByteChannel(path,
+          StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
+        ByteBuffer buffer = ByteBuffer.allocate(KILO_BYTE);
+        for (int i = 0; i < KILO_BYTE; i++) {
+          buffer.put((byte) i);
+        }
+        for (int i = 0; i < kBytes; i++) {
+          buffer.rewind();
+          channel.write(buffer);
+        }
+      }
+    }
+    return path;
+  }
+}
