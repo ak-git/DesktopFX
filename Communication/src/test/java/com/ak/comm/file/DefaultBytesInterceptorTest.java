59b7debf3aa5ad09abd0c7c2cea71e40b7c1bffe
Alexander Kobelev <ak.mail.ru@gmail.com>
2016-12-16 18:50:51 +0300

    #F-50 Fixed Converter from binary frame to integer array: test case for LOG_LEVEL_LEXEMES

--- Communication/src/test/java/com/ak/comm/file/DefaultBytesInterceptorTest.java
+++ Communication/src/test/java/com/ak/comm/file/DefaultBytesInterceptorTest.java
@@ -1,41 +1,53 @@
 package com.ak.comm.file;
 
-import java.nio.file.Path;
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
-import javax.annotation.Nonnegative;
-import javax.annotation.Nonnull;
-
+import com.ak.comm.core.LogLevelSubstitution;
+import com.ak.comm.core.LogLevels;
 import com.ak.comm.interceptor.BytesInterceptor;
 import com.ak.comm.interceptor.simple.DefaultBytesInterceptor;
-import io.reactivex.Flowable;
-import io.reactivex.subscribers.TestSubscriber;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static jssc.SerialPort.BAUDRATE_115200;
 
 public final class DefaultBytesInterceptorTest {
-  @Test(dataProviderClass = FileDataProvider.class, dataProvider = "files")
-  public void testDefaultBytesInterceptor(@Nonnull Path fileToRead, @Nonnegative int bytes) throws Exception {
-    TestSubscriber<Integer> testSubscriber = TestSubscriber.create();
+  private static final Logger LOGGER = Logger.getLogger(DefaultBytesInterceptor.class.getName());
+
+  @Test
+  public void testInterceptorProperties() {
     BytesInterceptor<Integer, Byte> interceptor = new DefaultBytesInterceptor();
-    interceptor.putOut((byte) 1);
     Assert.assertEquals(interceptor.getBaudRate(), BAUDRATE_115200);
     Assert.assertEquals(interceptor.getPingRequest(), Byte.valueOf((byte) 0));
+  }
 
-    Flowable.fromPublisher(new FileReadingService(fileToRead)).flatMapIterable(buffer -> () -> interceptor.apply(buffer).iterator()).
-        subscribe(testSubscriber);
-    testSubscriber.assertNoErrors();
+  @DataProvider(name = "responses")
+  public static Object[][] responses() {
+    return new Object[][] {
+        {new byte[] {0x00}, 0},
+        {new byte[] {0x71}, 113},
+        {new byte[] {(byte) 0xff}, 255},
+    };
+  }
 
-    if (bytes < 0) {
-      testSubscriber.assertNoValues();
-      testSubscriber.assertNotSubscribed();
-      testSubscriber.assertNotComplete();
-    }
-    else {
-      testSubscriber.assertValueCount(bytes);
-      testSubscriber.assertSubscribed();
-      testSubscriber.assertComplete();
-    }
+  @Test(dataProvider = "responses")
+  public void testDefaultBytesInterceptor(byte[] input, Integer response) {
+    BytesInterceptor<Integer, Byte> interceptor = new DefaultBytesInterceptor();
+
+    LogLevelSubstitution.substituteLogLevel(LOGGER, LogLevels.LOG_LEVEL_LEXEMES,
+        () -> {
+          Collection<Integer> frames = interceptor.apply(ByteBuffer.wrap(input)).collect(Collectors.toList());
+          Assert.assertEquals(frames, Collections.singleton(response));
+        },
+        logRecord -> Assert.assertTrue(logRecord.getMessage().endsWith(response.toString()), logRecord.getMessage()));
+
+    LogLevelSubstitution.substituteLogLevel(LOGGER, LogLevels.LOG_LEVEL_LEXEMES,
+        () -> Assert.assertTrue(interceptor.putOut(response.byteValue()).remaining() > 0),
+        logRecord -> Assert.assertTrue(logRecord.getMessage().endsWith(response.byteValue() + " OUT to hardware"), logRecord.getMessage()));
   }
 }
