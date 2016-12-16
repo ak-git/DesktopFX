3dd5043b15d18a08e3e1631fc9cdcb3eeaabf5b7
Alexander Kobelev <ak.mail.ru@gmail.com>
2016-12-16 21:01:17 +0300

    #F-50 Fixed Converter from binary frame to integer array: fix invalid variables

--- Communication/src/main/java/com/ak/comm/converter/AbstractConverter.java
+++ Communication/src/main/java/com/ak/comm/converter/AbstractConverter.java
@@ -1,8 +1,10 @@
 package com.ak.comm.converter;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -30,7 +32,11 @@ public abstract class AbstractConverter<RESPONSE, EV extends Enum<EV> & Variable
 
   @Override
   public final Stream<int[]> apply(@Nonnull RESPONSE response) {
-    Stream<int[]> stream = innerApply(response);
+    Stream<int[]> stream = innerApply(response).peek(ints -> {
+      if (ints.length != variables.size()) {
+        logger.log(Level.SEVERE, String.format("Invalid variables: %s not match %s", variables, Arrays.toString(ints)));
+      }
+    });
     if (logger.isLoggable(LOG_LEVEL_VALUES)) {
       stream = stream.peek(ints -> logger.log(LOG_LEVEL_VALUES, String.format("#%x [ %s ]", hashCode(),
           IntStream.iterate(0, operand -> operand + 1).limit(variables.size()).mapToObj(
