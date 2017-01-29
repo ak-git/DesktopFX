a3841f1d07c089f829fc10d2c13f7e532916085c
Alexander Kobelev <ak.mail.ru@gmail.com>
2017-01-29 14:39:49 +0300

    #F-60 In Progress File Reading Service processing

--- Communication/src/main/java/com/ak/comm/core/AbstractConvertableService.java
+++ Communication/src/main/java/com/ak/comm/core/AbstractConvertableService.java
@@ -1,11 +1,9 @@
 package com.ak.comm.core;
 
 import java.nio.ByteBuffer;
-import java.nio.file.Path;
 import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.annotation.OverridingMethodsMustInvokeSuper;
 
 import com.ak.comm.converter.Converter;
@@ -26,11 +24,6 @@ public abstract class AbstractConvertableService<RESPONSE, REQUEST, EV extends E
     this.bytesInterceptor = bytesInterceptor;
     this.responseConverter = responseConverter;
     workingBuffer = ByteBuffer.allocate(responseConverter.variables().size() * Integer.BYTES);
-  }
-
-  @Nullable
-  public Path getPath() {
-    return byteChannel.getPath();
   }
 
   @OverridingMethodsMustInvokeSuper
