4795c6950f89b3125bb00fd70f7f3d3e9c9b1756
Alexander Kobelev <ak.mail.ru@gmail.com>
2017-02-12 16:29:48 +0300

    #F-60 In Progress File Reading Service processing: refactoring NmisAddressTest

--- FxDesktop/src/test/java/com/ak/comm/bytes/nmis/NmisAddressTest.java
+++ FxDesktop/src/test/java/com/ak/comm/bytes/nmis/NmisAddressTest.java
@@ -9,7 +9,6 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static com.ak.comm.bytes.nmis.NmisAddress.ALIVE;
@@ -20,8 +19,6 @@ import static com.ak.comm.bytes.nmis.NmisAddress.ROTATE_ELBOW;
 import static com.ak.comm.bytes.nmis.NmisAddress.ROTATE_HAND;
 
 public class NmisAddressTest {
-  private static final byte[] EMPTY_BYTES = {};
-
   private NmisAddressTest() {
   }
 
@@ -46,85 +43,19 @@ public class NmisAddressTest {
     Assert.assertEquals(Optional.ofNullable(NmisAddress.find(ByteBuffer.wrap(input))).orElse(ALIVE), address);
   }
 
-  @DataProvider(name = "extractorsNone")
-  public static Object[][] extractorsNone() {
-    return new Object[][] {
-        new Object[] {
-            // alive
-            new byte[] {0x7E, -12, 0x7E, 0x40, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0xC7}
-        },
-        new Object[] {
-            // channel 41 CATCH_ELBOW
-            new byte[] {0x7E, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0xC7}
-        },
-        new Object[] {
-            // NO Data, empty frame
-            new byte[] {0x7e, 0x45, 0x02, 0x3f, 0x00, 0x04}
-        },
-    };
-  }
-
-  @Test(dataProvider = "extractorsNone")
+  @Test(dataProviderClass = NmisExtractorTestProvider.class, dataProvider = "extractorsNone")
   public static void testExtractorNone(byte[] from) {
     Stream.of(NmisAddress.values()).forEach(address ->
         Assert.assertEquals(
             NmisAddress.Extractor.from(address, NmisAddress.FrameField.NONE).extract(ByteBuffer.wrap(from)).count(), 0));
   }
 
-  @DataProvider(name = "extractorValues")
-  public static Object[][] extractorValues() {
-    return new Object[][] {
-        new Object[] {
-            // no Time, alive
-            NmisAddress.Extractor.NONE,
-            new byte[] {0x7E, -12, 0x7E, 0x40, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0xC7},
-            IntStream.empty()
-        },
-        new Object[] {
-            // Time, but NO Data, empty frame
-            NmisAddress.Extractor.DATA_TIME,
-            new byte[] {0x7e, 0x45, 0x02, 0x3f, 0x00, 0x04},
-            IntStream.of(0x3f)
-        },
-        new Object[] {
-            // Time and Data
-            NmisAddress.Extractor.DATA_DATA,
-            new byte[] {0x7e, 0x45, 0x09, 0x44, 0x00, 0x01, 0x05, 0x0b, (byte) 0xe0, (byte) 0xb1, (byte) 0xe1, 0x7a, 0x0d},
-            IntStream.empty()
-        },
-    };
-  }
-
-  @Test(dataProvider = "extractorValues")
+  @Test(dataProviderClass = NmisExtractorTestProvider.class, dataProvider = "extractorValues")
   public static void testExtractorValues(NmisAddress.Extractor extractor, byte[] from, BaseStream<Integer, IntStream> expected) {
     Assert.assertEquals(extractor.extract(ByteBuffer.wrap(from).order(ByteOrder.LITTLE_ENDIAN)).iterator(), expected.iterator());
   }
 
-  @DataProvider(name = "extractorToBuffer")
-  public static Object[][] extractorToBuffer() {
-    return new Object[][] {
-        new Object[] {
-            // no Data, alive
-            NmisAddress.Extractor.NONE,
-            new byte[] {0x7E, -12, 0x7E, 0x40, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0xC7},
-            EMPTY_BYTES
-        },
-        new Object[] {
-            // no Data, empty frame
-            NmisAddress.Extractor.DATA_TIME,
-            new byte[] {0x7e, 0x45, 0x02, 0x3f, 0x00, 0x04},
-            EMPTY_BYTES
-        },
-        new Object[] {
-            // Time and Data
-            NmisAddress.Extractor.DATA_DATA,
-            new byte[] {0x7e, 0x45, 0x09, 0x44, 0x00, 0x01, 0x05, 0x0b, (byte) 0xe0, (byte) 0xb1, (byte) 0xe1, 0x7a, 0x0d},
-            new byte[] {0x01, 0x05, 0x0b, (byte) 0xe0, (byte) 0xb1, (byte) 0xe1, 0x7a}
-        },
-    };
-  }
-
-  @Test(dataProvider = "extractorToBuffer")
+  @Test(dataProviderClass = NmisExtractorTestProvider.class, dataProvider = "extractorToBuffer")
   public static void testExtractorToBuffer(NmisAddress.Extractor extractor, byte[] from, byte[] expected) {
     ByteBuffer destination = ByteBuffer.allocate(expected.length);
     extractor.extract(ByteBuffer.wrap(from), destination);
