29735e5e3bbb47411232229274560457dfe580b9
Alexander Kobelev <ak.mail.ru@gmail.com>
2017-04-01 12:26:47 +0300

    #F-66 In Progress Calibration for Aper: auto interpolators selection

--- Util/src/main/java/com/ak/numbers/Interpolators.java
+++ Util/src/main/java/com/ak/numbers/Interpolators.java
@@ -7,30 +7,51 @@ import javax.inject.Provider;
 
 import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.analysis.interpolation.AkimaSplineInterpolator;
+import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
+import org.apache.commons.math3.analysis.interpolation.UnivariateInterpolator;
 
 public enum Interpolators {
-  ;
+  AKIMA(new AkimaSplineInterpolator(), 5), LINEAR(new LinearInterpolator(), 2);
 
-  public static Provider<IntUnaryOperator> interpolate(@Nonnull Coefficients coefficients) {
-    double[] all = coefficients.get();
-    if ((all.length & 1) == 1) {
-      throw new IllegalArgumentException(String.format("Number %d of coefficients %s is not even", all.length, coefficients.name()));
-    }
-    double[] xValues = new double[all.length / 2];
-    double[] yValues = new double[all.length / 2];
+  @Nonnull
+  private final UnivariateInterpolator interpolator;
+  private final int minPoints;
+
+  Interpolators(@Nonnull UnivariateInterpolator interpolator, int minPoints) {
+    this.interpolator = interpolator;
+    this.minPoints = minPoints;
+  }
+
+  private Provider<IntUnaryOperator> interpolate(@Nonnull double[] coefficients) {
+    double[] xValues = new double[coefficients.length / 2];
+    double[] yValues = new double[coefficients.length / 2];
 
     for (int i = 0; i < xValues.length; i++) {
-      xValues[i] = all[i * 2];
-      yValues[i] = all[i * 2 + 1];
+      xValues[i] = coefficients[i * 2];
+      yValues[i] = coefficients[i * 2 + 1];
     }
 
     return () -> new IntUnaryOperator() {
-      private final UnivariateFunction interpolator = new AkimaSplineInterpolator().interpolate(xValues, yValues);
+      private final UnivariateFunction f = interpolator.interpolate(xValues, yValues);
 
       @Override
       public int applyAsInt(int x) {
-        return (int) Math.round(interpolator.value(Math.min(Math.max(x, xValues[0]), xValues[xValues.length - 1])));
+        return (int) Math.round(f.value(Math.min(Math.max(x, xValues[0]), xValues[xValues.length - 1])));
       }
     };
+  }
+
+  public static Provider<IntUnaryOperator> interpolate(@Nonnull Coefficients coefficients) {
+    double[] xAndY = coefficients.get();
+    if ((xAndY.length & 1) == 1) {
+      throw new IllegalArgumentException(String.format("Number %d of coefficients %s is not even", xAndY.length, coefficients.name()));
+    }
+
+    for (Interpolators i : Interpolators.values()) {
+      if (xAndY.length / 2 >= i.minPoints) {
+        return i.interpolate(xAndY);
+      }
+    }
+    throw new IllegalArgumentException(String.format("Number of points %d from %s is too small", xAndY.length / 2, coefficients.name()));
   }
 }
--- Util/src/test/java/com/ak/numbers/InterpolatorsTest.java
+++ Util/src/test/java/com/ak/numbers/InterpolatorsTest.java
@@ -4,31 +4,50 @@ import java.util.Arrays;
 import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 
+import javax.annotation.Nonnull;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class InterpolatorsTest {
   private enum InterpolatorCoefficients implements Coefficients {
-    INTERPOLATOR_TEST
+    INTERPOLATOR_TEST_AKIMA, INTERPOLATOR_TEST_LINEAR, INTERPOLATOR_TEST_INVALID
   }
 
   private InterpolatorsTest() {
   }
 
-  @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Number 3 of coefficients DIFF is not even")
+  @Test(expectedExceptions = IllegalArgumentException.class,
+      expectedExceptionsMessageRegExp = "Number 3 of coefficients DIFF is not even")
   public static void testInvalidCoefficients() {
     Interpolators.interpolate(SimpleCoefficients.DIFF);
   }
 
-  @Test
-  public static void testInterpolator() {
-    IntUnaryOperator operator = Interpolators.interpolate(InterpolatorCoefficients.INTERPOLATOR_TEST).get();
-    int[] actual = IntStream.rangeClosed(1, 15).map(operator).toArray();
-    Assert.assertTrue(Arrays.equals(actual, new int[] {
-        0, 0, 0, 40, 100,
-        133, 153, 163, 160, 146,
-        120, 83, 33, -27, -100
-    }), Arrays.toString(actual));
+  @Test(expectedExceptions = IllegalArgumentException.class,
+      expectedExceptionsMessageRegExp = "Number of points 1 from INTERPOLATOR_TEST_INVALID is too small")
+  public static void testTooLowCoefficients() {
+    Interpolators.interpolate(InterpolatorCoefficients.INTERPOLATOR_TEST_INVALID);
   }
 
+  @DataProvider(name = "interpolators")
+  public static Object[][] interpolators() {
+    return new Object[][] {
+        {InterpolatorCoefficients.INTERPOLATOR_TEST_AKIMA, new int[] {
+            0, 0, 0, 40, 100,
+            133, 153, 163, 160, 146,
+            120, 83, 33, -27, -100
+        }},
+        {InterpolatorCoefficients.INTERPOLATOR_TEST_LINEAR, new int[] {
+            0, 0, 0, 8, 17, 25, 33, 42, 50, 58, 67, 75, 83, 92, 100
+        }}
+    };
+  }
+
+  @Test(dataProvider = "interpolators")
+  public static void testInterpolator(@Nonnull Coefficients coefficients, @Nonnull int[] expected) {
+    IntUnaryOperator operator = Interpolators.interpolate(coefficients).get();
+    int[] actual = IntStream.rangeClosed(1, 15).map(operator).toArray();
+    Assert.assertTrue(Arrays.equals(actual, expected), Arrays.toString(actual));
+  }
 }
