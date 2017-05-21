04e1ab12314dcc7257b6f12ada66e204509bb668
Alexander Kobelev <ak.mail.ru@gmail.com>
2017-05-21 15:20:31 +0300

    #F-76 In Progress Remove spring-context dependency from Communication

--- Communication/src/main/java/com/ak/comm/file/FileReadingService.java
+++ Communication/src/main/java/com/ak/comm/file/FileReadingService.java
@@ -1,16 +1,16 @@
 package com.ak.comm.file;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.ClosedByInterruptException;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.locks.Lock;
@@ -28,7 +28,6 @@ import com.ak.comm.logging.LogBuilders;
 import io.reactivex.disposables.Disposable;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
-import org.springframework.util.DigestUtils;
 
 import static com.ak.comm.util.LogUtils.LOG_LEVEL_ERRORS;
 
@@ -57,57 +56,71 @@ final class FileReadingService<RESPONSE, REQUEST, EV extends Enum<EV> & Variable
         Files.isReadable(fileToRead)) {
       s.onSubscribe(this);
 
-      try (InputStream in = Files.newInputStream(fileToRead, StandardOpenOption.READ)) {
-        Logger.getLogger(getClass().getName()).log(Level.CONFIG,
-            String.format("#%x Open file [ %s ]", hashCode(), fileToRead));
-        String md5Code = DigestUtils.appendMd5DigestAsHex(in, new StringBuilder()).toString();
+      LOCK.lock();
+      try (SeekableByteChannel seekableByteChannel = Files.newByteChannel(fileToRead, StandardOpenOption.READ)) {
+        Logger.getLogger(getClass().getName()).log(Level.CONFIG, String.format("#%x Open file [ %s ]", hashCode(), fileToRead));
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+        ByteBuffer buffer = ByteBuffer.allocate(CAPACITY_4K);
+        while (seekableByteChannel.read(buffer) > 0 && !isDisposed()) {
+          buffer.flip();
+          md5.update(buffer);
+          buffer.clear();
+        }
+
+        byte[] digest = md5.digest();
+        StringBuilder sb = new StringBuilder(digest.length * 2);
+        for (byte b : digest) {
+          sb.append(String.format("%x", b));
+        }
+        String md5Code = sb.toString();
+
         Path convertedFile = LogBuilders.CONVERTER_FILE.build(md5Code).getPath();
         if (Files.exists(convertedFile, LinkOption.NOFOLLOW_LINKS)) {
           convertedFileChannelProvider = () -> Files.newByteChannel(convertedFile, StandardOpenOption.READ);
           Logger.getLogger(getClass().getName()).log(Level.INFO,
               String.format("#%x File [ %s ] with MD5 = [ %s ] is already processed", hashCode(), fileToRead, md5Code));
-          s.onComplete();
         }
         else {
-          LOCK.lock();
-          try {
-            Logger.getLogger(getClass().getName()).log(Level.INFO,
-                String.format("#%x Read file [ %s ], MD5 = [ %s ]", hashCode(), fileToRead, md5Code));
-            try (ReadableByteChannel readableByteChannel = Files.newByteChannel(fileToRead, StandardOpenOption.READ)) {
-              Path tempConverterFile = LogBuilders.CONVERTER_FILE.build("tempConverterFile").getPath();
-              convertedFileChannelProvider = () -> Files.newByteChannel(tempConverterFile,
-                  StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.TRUNCATE_EXISTING);
+          Logger.getLogger(getClass().getName()).log(Level.INFO,
+              String.format("#%x Read file [ %s ], MD5 = [ %s ]", hashCode(), fileToRead, md5Code));
 
-              boolean readFlag = false;
-              ByteBuffer buffer = ByteBuffer.allocate(CAPACITY_4K);
-              while (readableByteChannel.read(buffer) > 0 && !isDisposed()) {
-                buffer.flip();
-                logBytes(buffer);
-                process(buffer).forEach(s::onNext);
-                buffer.clear();
-                readFlag = true;
-              }
+          Path tempConverterFile = LogBuilders.CONVERTER_FILE.build("tempConverterFile").getPath();
+          convertedFileChannelProvider = () -> Files.newByteChannel(tempConverterFile,
+              StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.TRUNCATE_EXISTING);
 
-              if (!isDisposed()) {
-                if (readFlag) {
-                  Files.copy(tempConverterFile, convertedFile, LinkOption.NOFOLLOW_LINKS, StandardCopyOption.REPLACE_EXISTING);
-                }
-                s.onComplete();
-              }
+          boolean readFlag = false;
+          seekableByteChannel.position(0);
+
+          while (seekableByteChannel.read(buffer) > 0 && !isDisposed()) {
+            buffer.flip();
+            logBytes(buffer);
+            process(buffer).forEach(s::onNext);
+            buffer.clear();
+            readFlag = true;
+          }
+
+          if (!isDisposed()) {
+            if (readFlag) {
+              Files.copy(tempConverterFile, convertedFile, LinkOption.NOFOLLOW_LINKS, StandardCopyOption.REPLACE_EXISTING);
             }
           }
-          finally {
-            LOCK.unlock();
-            Logger.getLogger(getClass().getName()).log(Level.INFO, "Close file " + fileToRead);
-          }
+        }
+
+        if (!isDisposed()) {
+          s.onComplete();
         }
       }
       catch (ClosedByInterruptException e) {
         Logger.getLogger(getClass().getName()).log(Level.CONFIG, fileToRead.toString(), e);
       }
-      catch (IOException e) {
+      catch (IOException | NoSuchAlgorithmException e) {
         Logger.getLogger(getClass().getName()).log(Level.WARNING, fileToRead.toString(), e);
         s.onError(e);
+      }
+      finally {
+        LOCK.unlock();
+        Logger.getLogger(getClass().getName()).log(Level.INFO, "Close file " + fileToRead);
       }
     }
     else {
