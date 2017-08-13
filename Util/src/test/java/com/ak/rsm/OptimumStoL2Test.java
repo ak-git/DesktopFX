c04ce61e3f47e963b4dd78d5d3e11b617dc22c4b
Alexander Kobelev <ak.mail.ru@gmail.com>
2017-08-13 13:18:21 +0300

    #F-88 Fixed Event horizon for Two-layer model

--- Util/src/test/java/com/ak/rsm/OptimumStoL2Test.java
+++ Util/src/test/java/com/ak/rsm/OptimumStoL2Test.java
@@ -24,7 +24,6 @@ public class OptimumStoL2Test {
   private static final double[] EMPTY = {};
   private static final double LCC_SI = 1.0;
   private static final double RHO1_SI = 1.0;
-  private static final double RHO2_SI = Double.POSITIVE_INFINITY;
   private static final double DS = 0.001;
   private static final double PRECISION_S = 0.05;
 
@@ -33,39 +32,41 @@ public class OptimumStoL2Test {
 
   @Test(enabled = false)
   public static void testOptimalS1S2() {
-    DoubleStream.iterate(PRECISION_S * 2, operand -> operand + PRECISION_S).limit(20).
+    DoubleStream.iterate(PRECISION_S * 2, operand -> operand + PRECISION_S).limit(10).
         forEach(h -> {
+          double rho2 = 5.0;
           SimplexOptimizer optimizer = new SimplexOptimizer(-1, 1.0e-5);
-          optimizer.optimize(new MaxEval(30000), new ObjectiveFunction(s1s2 -> getRho1HErrors2(s1s2[0], s1s2[1], h)[0]),
+          optimizer.optimize(new MaxEval(30000), new ObjectiveFunction(s1s2 -> getRho1Rho2Errors2(s1s2[0], s1s2[1], rho2, h)[0]),
               GoalType.MINIMIZE, new NelderMeadSimplex(2, PRECISION_S),
               new InitialGuess(new double[] {0.5, PRECISION_S}));
           PointValuePair optimize = optimizer.optimize();
-          Logger.getLogger(OptimumStoL2Test.class.getName()).
-              info(String.format("h = %.2f; [%.3f - %.3f] err = %.6f", h,
-                  optimize.getPoint()[0], optimize.getPoint()[1], optimize.getValue()));
+          Logger.getLogger(OptimumStoL2Test.class.getName()).info(
+              String.format("h = %.2f; [%.3f - %.3f] errRho1 = %.6f errRho2 = %.6f", h,
+                  optimize.getPoint()[0], optimize.getPoint()[1], optimize.getValue(),
+                  getRho1Rho2Errors2(optimize.getPoint()[0], optimize.getPoint()[1], rho2, h)[1]));
         });
   }
 
   @Test(enabled = false)
-  public static void testRho1AndHErrors() {
+  public static void testRho1Rho2Errors() {
     LineFileBuilder.<double[]>of("%.2f %.2f %.6f").
         xRange(PRECISION_S * 2.0, 1.0 - PRECISION_S, PRECISION_S).
         yRange(PRECISION_S * 2.0, 1.0 - PRECISION_S, PRECISION_S).
-        add("zErrorRho1.txt", value -> value[0]).add("zErrorH.txt", value -> value[1]).
-        generate((sPU1, sPU2) -> getRho1HErrors2(sPU1, sPU2, 0.5));
+        add("zErrorRho1.txt", value -> value[0]).add("zErrorRho2.txt", value -> value[1]).
+        generate((sPU1, sPU2) -> getRho1Rho2Errors2(sPU1, sPU2, 10.0, 0.5));
   }
 
-  private static double[] getRho1HErrors2(double sPU1, double sPU2, double hSI) {
+  private static double[] getRho1Rho2Errors2(double sPU1, double sPU2, double rho2, double hSI) {
     if (sPU2 > sPU1 - PRECISION_S) {
       return new double[] {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY};
     }
-    return getRho1HErrors(sPU1, sPU2, hSI);
+    return getRho1Rho2Errors(sPU1, sPU2, rho2, hSI);
   }
 
-  private static double[] getRho1HErrors(double sPU1, double sPU2, double hSI) {
+  private static double[] getRho1Rho2Errors(double sPU1, double sPU2, double rho2, double hSI) {
     TrivariateFunction model1 = new ResistanceTwoLayer(new TetrapolarSystem(sPU1, LCC_SI, METRE));
     TrivariateFunction model2 = new ResistanceTwoLayer(new TetrapolarSystem(sPU2, LCC_SI, METRE));
-    double[] PREDICTED = {RHO1_SI, hSI};
+    double[] trueValues = {RHO1_SI, rho2};
 
     PointValuePair pointValuePair = Stream.of(EMPTY).
         flatMap(d -> scalarMultiply(d, LCC_SI - DS, LCC_SI + DS)).
@@ -77,31 +78,31 @@ public class OptimumStoL2Test {
           double s1 = doubles[2];
 
           double real1 = new ResistanceTwoLayer(
-              new TetrapolarSystem(s1, lCC, METRE)).value(RHO1_SI, RHO2_SI, hSI);
+              new TetrapolarSystem(s1, lCC, METRE)).value(RHO1_SI, rho2, hSI);
           double real2 = new ResistanceTwoLayer(
-              new TetrapolarSystem(s2, lCC, METRE)).value(RHO1_SI, RHO2_SI, hSI);
+              new TetrapolarSystem(s2, lCC, METRE)).value(RHO1_SI, rho2, hSI);
 
           SimplexOptimizer optimizer = new SimplexOptimizer(-1, 1.0e-14);
-          return optimizer.optimize(new MaxEval(30000), new ObjectiveFunction(rho1AndH -> {
+          return optimizer.optimize(new MaxEval(30000), new ObjectiveFunction(rho1rho2 -> {
                 Inequality inequality = Inequality.logDifference();
-                inequality.applyAsDouble(model1.value(rho1AndH[0], RHO2_SI, rho1AndH[1]), real1);
-                inequality.applyAsDouble(model2.value(rho1AndH[0], RHO2_SI, rho1AndH[1]), real2);
+                inequality.applyAsDouble(model1.value(rho1rho2[0], rho1rho2[1], hSI), real1);
+                inequality.applyAsDouble(model2.value(rho1rho2[0], rho1rho2[1], hSI), real2);
                 return inequality.getAsDouble();
               }),
-              GoalType.MINIMIZE, new NelderMeadSimplex(2, 0.001), new InitialGuess(PREDICTED)
+              GoalType.MINIMIZE, new NelderMeadSimplex(2, 0.001), new InitialGuess(trueValues)
           );
         }).
         max(Comparator.comparingDouble(optimum -> {
           Inequality inequality = Inequality.logDifference();
-          for (int i = 0; i < PREDICTED.length; i++) {
-            inequality.applyAsDouble(optimum.getPoint()[i], PREDICTED[i]);
+          for (int i = 0; i < trueValues.length; i++) {
+            inequality.applyAsDouble(optimum.getPoint()[i], trueValues[i]);
           }
           return inequality.getAsDouble();
         })).orElseThrow(IllegalStateException::new);
 
-    double[] result = new double[PREDICTED.length];
+    double[] result = new double[trueValues.length];
     for (int i = 0; i < result.length; i++) {
-      result[i] = Inequality.proportional().applyAsDouble(pointValuePair.getPoint()[i], PREDICTED[i]);
+      result[i] = Inequality.proportional().applyAsDouble(pointValuePair.getPoint()[i], trueValues[i]);
     }
     return result;
   }
