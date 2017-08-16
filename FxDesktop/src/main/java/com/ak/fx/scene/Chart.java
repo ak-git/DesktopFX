c31097c08bfd1d37e08fba26c9eb14730202aaee
Alexander Kobelev <ak.mail.ru@gmail.com>
2017-08-16 12:11:10 +0300

    #F-87 In Progress Medical chart: add title label

--- FxDesktop/src/main/java/com/ak/fx/scene/Chart.java
+++ FxDesktop/src/main/java/com/ak/fx/scene/Chart.java
@@ -6,11 +6,11 @@ import java.util.stream.Stream;
 
 public final class Chart extends AbstractRegion {
   private final MilliGrid milliGrid = new MilliGrid();
-  private final List<LineDiagram> rectangles = Stream.generate(LineDiagram::new).limit(3).collect(Collectors.toList());
+  private final List<LineDiagram> lineDiagrams = Stream.generate(LineDiagram::new).limit(3).collect(Collectors.toList());
 
   public Chart() {
     getChildren().addAll(milliGrid);
-    getChildren().addAll(rectangles);
+    getChildren().addAll(lineDiagrams);
   }
 
   @Override
@@ -21,28 +21,28 @@ public final class Chart extends AbstractRegion {
   }
 
   private void layoutLineDiagrams(double x, double y, double width, double height) {
-    double dHeight = GridCell.SMALL.maxCoordinate(height * 2 / (1 + rectangles.size()));
+    double dHeight = GridCell.SMALL.maxCoordinate(height * 2 / (1 + lineDiagrams.size()));
 
-    for (LineDiagram rectangle : rectangles) {
+    for (LineDiagram rectangle : lineDiagrams) {
       rectangle.resizeRelocate(x, y, width, dHeight);
     }
 
     if (dHeight >= GridCell.SMALL.getStep() * 2) {
-      for (int i = 0; i < rectangles.size() / 2; i++) {
-        rectangles.get(i).relocate(x, y + GridCell.SMALL.roundCoordinate(height / (rectangles.size() + 1)) * i);
+      for (int i = 0; i < lineDiagrams.size() / 2; i++) {
+        lineDiagrams.get(i).relocate(x, y + GridCell.SMALL.roundCoordinate(height / (lineDiagrams.size() + 1)) * i);
       }
-      if ((rectangles.size() & 1) != 0) {
-        rectangles.get(rectangles.size() / 2).relocate(x, y + GridCell.SMALL.maxCoordinate(height) / 2 - dHeight / 2
+      if ((lineDiagrams.size() & 1) != 0) {
+        lineDiagrams.get(lineDiagrams.size() / 2).relocate(x, y + GridCell.SMALL.maxCoordinate(height) / 2 - dHeight / 2
         );
       }
-      for (int i = 0; i < rectangles.size() / 2; i++) {
-        rectangles.get(rectangles.size() - 1 - i).relocate(x,
+      for (int i = 0; i < lineDiagrams.size() / 2; i++) {
+        lineDiagrams.get(lineDiagrams.size() - 1 - i).relocate(x,
             y + GridCell.SMALL.maxCoordinate(height) -
-                dHeight - GridCell.SMALL.roundCoordinate(height / (rectangles.size() + 1)) * i);
+                dHeight - GridCell.SMALL.roundCoordinate(height / (lineDiagrams.size() + 1)) * i);
       }
     }
     else {
-      rectangles.forEach(rectangle -> rectangle.resize(width, GridCell.SMALL.maxCoordinate(height)));
+      lineDiagrams.forEach(rectangle -> rectangle.resize(width, GridCell.SMALL.maxCoordinate(height)));
     }
   }
 }
--- FxDesktop/src/main/java/com/ak/fx/scene/LineDiagram.java
+++ FxDesktop/src/main/java/com/ak/fx/scene/LineDiagram.java
@@ -2,15 +2,21 @@ package com.ak.fx.scene;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 final class LineDiagram extends AbstractRegion {
+  private static final double LABEL_HEIGHT = GridCell.SMALL.getStep() / 3;
   private final Rectangle rectangle = new Rectangle();
+  private final Text label = new Text("aVL");
 
   LineDiagram() {
     rectangle.setStroke(Color.BLACK);
     rectangle.setFill(null);
     rectangle.setStrokeWidth(3.0);
+    label.setFont(new Font(Font.getDefault().getName(), LABEL_HEIGHT));
     getChildren().add(rectangle);
+    getChildren().add(label);
   }
 
   @Override
@@ -19,5 +25,7 @@ final class LineDiagram extends AbstractRegion {
     rectangle.setY(y);
     rectangle.setWidth(width);
     rectangle.setHeight(height);
+    label.relocate(x + GridCell.SMALL.getStep() + GridCell.POINTS.getStep(),
+        y + height / 4 - LABEL_HEIGHT);
   }
 }
