0a17cc036419ec9b032bb4ba35c6c1f856e6987d
Alexander Kobelev <ak.mail.ru@gmail.com>
2017-08-16 15:55:56 +0300

    #F-87 In Progress Medical chart: simple path

--- FxDesktop/src/main/java/com/ak/fx/scene/LineDiagram.java
+++ FxDesktop/src/main/java/com/ak/fx/scene/LineDiagram.java
@@ -1,31 +1,52 @@
 package com.ak.fx.scene;
 
+import javafx.beans.binding.Bindings;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import static com.ak.fx.scene.GridCell.POINTS;
+import static com.ak.fx.scene.GridCell.SMALL;
+
 final class LineDiagram extends AbstractRegion {
-  private static final double LABEL_HEIGHT = GridCell.SMALL.getStep() / 3;
-  private final Rectangle rectangle = new Rectangle();
-  private final Text label = new Text("aVL");
+  private static final double LABEL_HEIGHT = SMALL.getStep() / 3;
+  private final Rectangle bounds = new Rectangle();
+  private final Text title = new Text("aVL");
+  private final Text centerValue = new Text("0");
+  private final Path path = new Path();
 
   LineDiagram() {
-    rectangle.setStroke(Color.BLACK);
-    rectangle.setFill(null);
-    rectangle.setStrokeWidth(3.0);
-    label.setFont(new Font(Font.getDefault().getName(), LABEL_HEIGHT));
-    getChildren().add(rectangle);
-    getChildren().add(label);
+    bounds.setVisible(false);
+    bounds.setStroke(Color.BLACK);
+    bounds.setFill(null);
+    bounds.setStrokeWidth(3.0);
+
+    title.setFont(Font.font(Font.getDefault().getName(), LABEL_HEIGHT));
+    centerValue.setFont(title.getFont());
+
+    path.setFill(Color.BLACK);
+    getChildren().add(bounds);
+    getChildren().add(title);
+    getChildren().add(centerValue);
+    getChildren().add(path);
+
+    path.getElements().add(new MoveTo(0, 0));
+    path.getElements().add(new LineTo(10, -200));
+    path.translateXProperty().setValue(SMALL.getStep() * 2);
+    path.translateYProperty().bind(Bindings.divide(heightProperty(), 2));
   }
 
   @Override
   void layoutChartChildren(double x, double y, double width, double height) {
-    rectangle.setX(x);
-    rectangle.setY(y);
-    rectangle.setWidth(width);
-    rectangle.setHeight(height);
-    label.relocate(x + GridCell.SMALL.getStep() + GridCell.POINTS.getStep(),
-        y + height / 4 - LABEL_HEIGHT);
+    bounds.setX(x);
+    bounds.setY(y);
+    bounds.setWidth(width);
+    bounds.setHeight(height);
+    title.relocate(x + GridCell.SMALL.getStep() + POINTS.getStep(), y + height / 4 - LABEL_HEIGHT);
+    centerValue.relocate(x + POINTS.getStep() / 4, y + height / 2 - LABEL_HEIGHT - POINTS.getStep() / 4);
   }
 }
