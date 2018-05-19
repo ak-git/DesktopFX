d3fb8f98588cf3e6a72e48e6906957335d1bf750
Alexander Kobelev <ak.mail.ru@gmail.com>
2018-05-19 21:04:48 +0300

    #F-101 In Progress Refresh for Digital Filter: test case for Converter

--- Communication/src/test/java/com/ak/comm/converter/LinkedConverterTest.java
+++ Communication/src/test/java/com/ak/comm/converter/LinkedConverterTest.java
@@ -2,11 +2,17 @@ package com.ak.comm.converter;
 
 import java.nio.ByteOrder;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.Nonnull;
+
 import com.ak.comm.bytes.BufferFrame;
+import com.ak.digitalfilter.DigitalFilter;
+import com.ak.digitalfilter.IntsAcceptor;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -50,5 +56,66 @@ public class LinkedConverterTest {
 
     Assert.assertEquals(linkedConverter.apply(frame).peek(ints -> Assert.assertEquals(ints, output,
         String.format("Actual %s, Expected %s", Arrays.toString(ints), Arrays.toString(output)))).count(), 1);
+  }
+
+  @DataProvider(name = "refresh-variables")
+  public static Object[][] variables3() {
+    return new Object[][] {
+        {new BufferFrame(new byte[] {1, 0, 0, 0, 10}, ByteOrder.BIG_ENDIAN)},
+    };
+  }
+
+  @Test(dataProvider = "refresh-variables")
+  public static void testRefresh(BufferFrame frame) {
+    LinkedConverter<BufferFrame, RefreshVariable, RefreshVariable> linkedConverter =
+        new LinkedConverter<>(
+            new LinkedConverter<>(new ToIntegerConverter<>(RefreshVariable.class, 1), RefreshVariable.class),
+            RefreshVariable.class
+        );
+
+    linkedConverter.refresh();
+    Assert.assertEquals(linkedConverter.apply(frame).count(), 0);
+  }
+
+  public enum RefreshVariable implements DependentVariable<RefreshVariable, RefreshVariable> {
+    OUT;
+
+    @Override
+    public final Class<RefreshVariable> getInputVariablesClass() {
+      return RefreshVariable.class;
+    }
+
+    @Override
+    public final List<RefreshVariable> getInputVariables() {
+      return Collections.singletonList(OUT);
+    }
+
+
+    @Override
+    public DigitalFilter filter() {
+      return new DigitalFilter() {
+        private int refreshCount;
+
+        @Override
+        public void forEach(@Nonnull IntsAcceptor after) {
+        }
+
+        @Override
+        public void reset() {
+          refreshCount++;
+        }
+
+        @Override
+        public int getOutputDataSize() {
+          return 1;
+        }
+
+        @Override
+        public void accept(@Nonnull int... values) {
+          Assert.assertEquals(values, new int[] {10});
+          Assert.assertEquals(refreshCount, 1);
+        }
+      };
+    }
   }
 }
