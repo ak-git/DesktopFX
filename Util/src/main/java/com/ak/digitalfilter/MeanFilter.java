6bd36c18e4fc760c0418b9982af7a916521f7888
Alexander Kobelev <ak.mail.ru@gmail.com>
2018-11-24 23:09:56 +0300

    #F-91 In Progress On-line data for medical chart: new SqrtSumFilter filter

--- Util/src/main/java/com/ak/digitalfilter/MeanFilter.java
+++ Util/src/main/java/com/ak/digitalfilter/MeanFilter.java
@@ -3,21 +3,17 @@ package com.ak.digitalfilter;
 import javax.annotation.Nonnegative;
 
 final class MeanFilter extends AbstractExcessBufferFilter {
-  private long mean;
-  private int length;
-
   MeanFilter(@Nonnegative int averageFactor) {
     super(averageFactor);
   }
 
   @Override
-  int apply(@Nonnegative int nowIndex) {
-    if (checkResetAndClear()) {
-      mean = 0;
-    }
-    length = Math.min(length + 1, length() - 1);
-    mean += get(nowIndex);
-    mean -= get(nowIndex + 1);
-    return (int) (mean / length);
+  int add(int nowIndex) {
+    return get(nowIndex);
+  }
+
+  @Override
+  int sub(int nowIndex) {
+    return get(nowIndex + 1);
   }
 }
--- Util/src/main/java/com/ak/digitalfilter/SqrtSumFilter.java
+++ Util/src/main/java/com/ak/digitalfilter/SqrtSumFilter.java
@@ -3,16 +3,22 @@ package com.ak.digitalfilter;
 import javax.annotation.Nonnegative;
 
 final class SqrtSumFilter extends AbstractExcessBufferFilter {
-  private long sumSqr;
-
   SqrtSumFilter(@Nonnegative int n) {
     super(n);
   }
 
   @Override
-  int apply(@Nonnegative int nowIndex) {
-    sumSqr += get(nowIndex) * get(nowIndex);
-    sumSqr -= get(nowIndex + 1) * get(nowIndex + 1);
-    return (int) Math.sqrt(1.0 * sumSqr / (length() - 1));
+  int add(int nowIndex) {
+    return get(nowIndex) * get(nowIndex);
+  }
+
+  @Override
+  int sub(int nowIndex) {
+    return get(nowIndex + 1) * get(nowIndex + 1);
+  }
+
+  @Override
+  int div() {
+    return (int) Math.sqrt(super.div());
   }
 }
