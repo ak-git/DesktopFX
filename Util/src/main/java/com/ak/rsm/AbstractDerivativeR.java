dd72e3819a6a0f1381f85b9c902f5a280c64dcec
Alexander Kobelev <ak.mail.ru@gmail.com>
2019-06-02 11:39:05 +0300

    #F-127 Fixed Resistivity Sounding Measurements: remove obsolete classes

--- Util/src/main/java/com/ak/rsm/AbstractDerivativeR.java
+++ Util/src/main/java/com/ak/rsm/AbstractDerivativeR.java
@@ -14,10 +14,6 @@ abstract class AbstractDerivativeR implements Cloneable {
     this.electrodes = electrodes;
   }
 
-  final TetrapolarSystem electrodes() {
-    return electrodes;
-  }
-
   final double sumN2kN(double k12, @Nonnegative double hSI) {
     return ResistanceTwoLayer.sum(hSI, n -> pow(n, 2.0) * pow(k12, n), (qn, b) -> qn *
         (-1.0 / pow(hypot(electrodes.radiusMinus(), b), 3.0) + 1.0 / pow(hypot(electrodes.radiusPlus(), b), 3.0)));
