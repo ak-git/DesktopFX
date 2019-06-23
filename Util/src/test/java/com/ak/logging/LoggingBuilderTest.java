d3df8da5a0c50db97df237c239d80fadb90eb576
Alexander Kobelev <ak.mail.ru@gmail.com>
2019-06-23 19:43:51 +0300

    #F-128 In Progress External calibration file in user's profile: test cases

--- Util/src/test/java/com/ak/logging/LoggingBuilderTest.java
+++ Util/src/test/java/com/ak/logging/LoggingBuilderTest.java
@@ -10,22 +10,23 @@ import java.nio.file.StandardOpenOption;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.ak.util.Strings;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class LogBuildersTest {
+public class LoggingBuilderTest {
   private static final Object[][] EMPTY_OBJECTS = {};
 
-  private LogBuildersTest() {
+  private LoggingBuilderTest() {
   }
 
   @DataProvider(name = "logBuilders")
   public static Object[][] logBuilders() {
-    return Stream.of(LogBuilders.values()).
-        map(binaryLogBuilder -> {
+    return Stream.of(LoggingBuilder.values()).
+        map(builder -> {
           try {
-            return new Object[] {binaryLogBuilder.build("02f29f660fa69e6c404c03de0f1e15f9").getPath()};
+            return new Object[] {builder.build(Strings.EMPTY).getPath()};
           }
           catch (IOException e) {
             return new Object[] {null};
@@ -35,12 +36,20 @@ public class LogBuildersTest {
 
 
   @Test(dataProvider = "logBuilders")
-  public static void testLogBuilders(Path path) throws IOException {
+  public static void testBuild(Path path) throws IOException {
     Assert.assertNotNull(path);
     WritableByteChannel channel = Files.newByteChannel(path,
         StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
     channel.write(ByteBuffer.wrap(LogBuildersTest.class.getName().getBytes(Charset.defaultCharset())));
     channel.close();
     Assert.assertTrue(Files.deleteIfExists(path));
+  }
+
+  @Test
+  public void testFileName() {
+    Stream.of(LoggingBuilder.values()).forEach(loggingBuilder -> {
+      Assert.assertTrue(loggingBuilder.fileName().startsWith(loggingBuilder.name().toLowerCase()));
+      Assert.assertTrue(loggingBuilder.fileName().endsWith(".properties"));
+    });
   }
 }
