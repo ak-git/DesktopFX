2e3f05100d4d7ab6a9727abab1fd843e0cb52e74
Alexander Kobelev <ak.mail.ru@gmail.com>
2019-08-19 20:53:25 +0300

    #F-126 In Progress Resistivity Sounding Measurements - prepare to 3 Layers inverse

--- Util/src/test/java/com/ak/util/StringsTest.java
+++ Util/src/test/java/com/ak/util/StringsTest.java
@@ -5,10 +5,10 @@ import javax.annotation.Nonnull;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import tec.uom.se.unit.MetricPrefix;
 import tec.uom.se.unit.Units;
 
 import static com.ak.util.Strings.OHM_METRE;
-
 
 public class StringsTest {
   @DataProvider(name = "strings")
@@ -37,9 +37,10 @@ public class StringsTest {
   }
 
   @Test
-  public void testRho() {
+  public void testRhoH() {
     Assert.assertEquals(Strings.rho(2.1234), String.format("\u03c1 = %.3f %s", 2.123, OHM_METRE));
     Assert.assertEquals(Strings.rho1(2.1234), String.format("\u03c1\u2081 = %.3f %s", 2.123, OHM_METRE));
     Assert.assertEquals(Strings.rho2(20.1236), String.format("\u03c1\u2082 = %.3f %s", 20.124, OHM_METRE));
+    Assert.assertEquals(Strings.h(0.21236, 2), String.format("h\u2082 = %.2f %s", 212.36, MetricPrefix.MILLI(Units.METRE)));
   }
 }
