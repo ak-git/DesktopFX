0bd52b76de1a9efe86f1cdf23e6d01844cde9b3b
Alexander Kobelev <ak.mail.ru@gmail.com>
2019-09-16 17:17:55 +0300

    #F-131 In Progress Optimize layer-model resistance calculations: common variable Lh

--- Util/src/test/java/com/ak/rsm/Log1pApparent3RhoTest.java
+++ Util/src/test/java/com/ak/rsm/Log1pApparent3RhoTest.java
@@ -3,7 +3,6 @@ package com.ak.rsm;
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 
-import com.ak.util.Metrics;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,7 +20,7 @@ public class Log1pApparent3RhoTest {
     TetrapolarSystem system1 = new TetrapolarSystem(smm, lmm, MILLI(METRE));
     TetrapolarSystem system2 = new TetrapolarSystem(lmm, smm, MILLI(METRE));
     double logApparent = log(new Resistance1Layer(system1).getApparent(rOhm)) - log(rho[0]);
-    double logPredicted = new Log1pApparent3Rho(system2.sToL(), Metrics.fromMilli(smm) / hStepSI).
+    double logPredicted = new Log1pApparent3Rho(system2.sToL(), system2.Lh(hStepSI)).
         value(Layers.getK12(rho[0], rho[1]), Layers.getK12(rho[1], rho[2]), p[0], p[1]);
 
     Assert.assertEquals(logApparent, logPredicted, 0.001);
