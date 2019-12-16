495b6f2605ee026e7660bd101ea65c20cd678b43
Alexander Kobelev <ak.mail.ru@gmail.com>
2019-12-16 17:41:25 +0300

    briko calibration fixed

--- FxDesktop/src/test/java/com/ak/comm/converter/aper/sinsin/AperSinSinConverterTest.java
+++ FxDesktop/src/test/java/com/ak/comm/converter/aper/sinsin/AperSinSinConverterTest.java
@@ -36,7 +36,7 @@ public final class AperSinSinConverterTest {
             5, 0, 0, 0,
             (byte) 0xd0, 0x07, 0, 0},
 
-            new int[] {55851, 301742, 1326, 51173, 276467}},
+            new int[] {79640, 301742, 1649, 72969, 276467}},
     };
   }
 
