06ccf91c16843ead69b47a986bb039bce133f4d1
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-07-25 21:19:34 +0300

    #F-143 In Progress Refactoring Medium: simplify test cases

--- Util/src/test/java/com/ak/rsm/Resistance2LayerTest.java
+++ Util/src/test/java/com/ak/rsm/Resistance2LayerTest.java
@@ -1,29 +1,13 @@
 package com.ak.rsm;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.function.IntToDoubleFunction;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 
-import com.ak.inverse.Inequality;
-import com.ak.math.Simplex;
-import com.ak.util.LineFileBuilder;
 import com.ak.util.Metrics;
-import org.apache.commons.math3.optim.PointValuePair;
-import org.apache.commons.math3.optim.SimpleBounds;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static com.ak.rsm.LayersProvider.layer2;
-import static java.lang.StrictMath.log;
 import static tec.uom.se.unit.MetricPrefix.MILLI;
 import static tec.uom.se.unit.Units.METRE;
 
@@ -103,75 +87,5 @@ public class Resistance2LayerTest {
   public void testLayer(@Nonnull double[] rho, @Nonnegative double hmm, @Nonnegative double smm, @Nonnegative double lmm, @Nonnegative double rOhm) {
     TetrapolarSystem system = new TetrapolarSystem(smm, lmm, MILLI(METRE));
     Assert.assertEquals(new Resistance2Layer(system).value(rho[0], rho[1], Metrics.fromMilli(hmm)), rOhm, 0.001);
-  }
-
-  private static double[] calculate(@Nonnegative double relativeError, @Nonnull double[] sToL, double k, @Nonnegative double hToL) {
-    final double L = 1.0;
-
-    TetrapolarSystem[] systems = {
-        new TetrapolarSystem(L * sToL[0], L * sToL[1], METRE),
-        new TetrapolarSystem(L * sToL[1], L, METRE),
-    };
-    double[] rho = {1.0, 1.0 / Layers.getRho1ToRho2(k)};
-    return IntStream.of(2, 5).mapToObj(n -> {
-      int signS1 = (n & 1) == 0 ? 1 : -1;
-      int signL = (n & 2) == 0 ? 1 : -1;
-      int signS2 = (n & 4) == 0 ? 1 : -1;
-
-      TetrapolarSystem[] systemsError = {
-          systems[0].newWithError(relativeError, signS1, signL),
-          systems[1].newWithError(relativeError, signL, signS2)
-      };
-
-      double[] rOhms = LayersProvider.rangeSystems(systemsError, layer2(rho[0], rho[1], hToL));
-      double[] rDiff = LayersProvider.rangeSystems(systems, s -> new DerivativeApparent2Rho(s).value(k, hToL));
-
-      double[] logApparent = rangeSystems(systems.length,
-          index -> log(systems[index].getApparent(rOhms[index])));
-      double[] logDiff = rangeSystems(systems.length,
-          index -> log(Math.abs(rDiff[index])));
-
-      double[] measured = rangeSystems(systems.length, index -> logApparent[index] - logDiff[index]);
-
-      PointValuePair optimize = Simplex.optimizeCMAES(hk -> {
-        double[] logApparentPredicted = rangeSystems(systems.length,
-            index -> new Log1pApparent2Rho(systems[index]).value(hk[1], hk[0])
-        );
-        double[] diffPredicted = rangeSystems(systems.length, index -> new DerivativeApparent2Rho(systems[index]).value(hk[1], hk[0]));
-        double[] predicted = rangeSystems(systems.length, index -> logApparentPredicted[index] - log(Math.abs(diffPredicted[index])));
-        return Inequality.absolute().applyAsDouble(measured, predicted);
-      }, new SimpleBounds(new double[] {0.0, -1.0}, new double[] {hToL, 1.0}), new double[] {hToL, k}, new double[] {0.001, 0.001});
-
-      double eH = Inequality.absolute().applyAsDouble(optimize.getPoint()[0], hToL) / relativeError;
-      double eK = Inequality.absolute().applyAsDouble(optimize.getPoint()[1], k) / relativeError;
-      Logger.getAnonymousLogger().info(() ->
-          String.format("%d [%+d %+d %+d]\th / L = %.4f; eH = %.15f; eK = %.15f; [%s]",
-              n, signS1, signL, signS2, hToL, eH, eK,
-              Arrays.toString(sToL))
-      );
-      return new double[] {eH, eK};
-    }).parallel().collect(Collectors.teeing(
-        Collectors.maxBy(Comparator.comparingDouble(value -> value[0])),
-        Collectors.maxBy(Comparator.comparingDouble(value -> value[1])),
-        (doubles, doubles2) -> Stream.of(doubles, doubles2)
-            .map(d -> d.orElse(new double[] {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY}))
-            .reduce((d1, d2) -> IntStream.range(0, d1.length).mapToDouble(i -> Math.max(d1[i], d2[i])).toArray())
-    )).orElseThrow();
-  }
-
-  @Test(enabled = false)
-  public void testInverseErrorByH() {
-    LineFileBuilder.<double[]>of("%.3f %.3f %.15f")
-        .xStream(() -> DoubleStream.of(-1, -0.1, 0.1, 1.0))
-        .yLog10Range(0.005, 0.5)
-        .add("h.txt", value -> value[0])
-        .add("k.txt", value -> value[1])
-        .generate((k, hToL) -> calculate(1.0e-6,
-            new double[] {0.2, 0.6}, k, hToL)
-        );
-  }
-
-  private static double[] rangeSystems(@Nonnegative int length, @Nonnull IntToDoubleFunction mapper) {
-    return IntStream.range(0, length).mapToDouble(mapper).toArray();
   }
 }
