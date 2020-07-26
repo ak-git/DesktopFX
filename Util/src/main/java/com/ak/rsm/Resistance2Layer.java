9b2f46b602e1093070a2753cad75eba807e64326
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-07-26 19:00:26 +0300

    #F-143 In Progress Refactoring Medium: refactoring NormalizedResistances

--- Util/src/main/java/com/ak/rsm/Resistance2Layer.java
+++ Util/src/main/java/com/ak/rsm/Resistance2Layer.java
@@ -5,27 +5,19 @@ import javax.annotation.Nonnull;
 
 import org.apache.commons.math3.analysis.TrivariateFunction;
 
-import static java.lang.StrictMath.pow;
-
 /**
  * Calculates <b>full</b> resistance R<sub>m-n</sub> (in Ohm) between electrodes for <b>2-layer</b> model.
  */
-final class Resistance2Layer extends AbstractResistanceLayer<Potential2Layer> implements TrivariateFunction {
+final class Resistance2Layer implements TrivariateFunction {
   @Nonnull
-  private final Resistance1Layer resistance1Layer;
+  private final NormalizedResistance2Layer resistance2Layer;
 
   Resistance2Layer(@Nonnull TetrapolarSystem electrodeSystem) {
-    super(electrodeSystem, Potential2Layer::new);
-    resistance1Layer = new Resistance1Layer(electrodeSystem);
+    resistance2Layer = new NormalizedResistance2Layer(electrodeSystem);
   }
 
   @Override
   public double value(@Nonnegative double rho1, @Nonnegative double rho2, @Nonnegative double h) {
-    double result = resistance1Layer.value(rho1);
-    if (Double.compare(rho1, rho2) != 0.0) {
-      double k = Layers.getK12(rho1, rho2);
-      result += (2.0 * rho1 / Math.PI) * Layers.sum(n -> pow(k, n) * apply(r -> r.value(n, h)));
-    }
-    return result;
+    return rho1 * resistance2Layer.applyAsDouble(Layers.getK12(rho1, rho2), h);
   }
 }
