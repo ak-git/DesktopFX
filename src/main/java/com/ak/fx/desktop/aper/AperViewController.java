5a997c40d279133ef73ea08cf89ffbe98e437268
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-07-31 22:39:25 +0300

    #F-148 In Progress Migrate to Spring Boot: aper2 and fix aper context

--- src/main/java/com/ak/fx/desktop/aper/AperViewController.java
+++ src/main/java/com/ak/fx/desktop/aper/AperViewController.java
@@ -4,7 +4,9 @@ import javax.inject.Named;
 
 import com.ak.comm.GroupService;
 import com.ak.comm.bytes.BufferFrame;
+import com.ak.comm.converter.LinkedConverter;
 import com.ak.comm.converter.ToIntegerConverter;
+import com.ak.comm.converter.aper.AperInVariable;
 import com.ak.comm.converter.aper.AperOutVariable;
 import com.ak.comm.interceptor.BytesInterceptor;
 import com.ak.comm.interceptor.simple.RampBytesInterceptor;
@@ -19,7 +21,7 @@ public final class AperViewController
     super(
         new GroupService<>(
             () -> new RampBytesInterceptor(BytesInterceptor.BaudRate.BR_460800, 25),
-            () -> new ToIntegerConverter<>(AperOutVariable.class, 1000)
+            () -> new LinkedConverter<>(new ToIntegerConverter<>(AperInVariable.class, 1000), AperOutVariable.class)
         )
     );
   }
