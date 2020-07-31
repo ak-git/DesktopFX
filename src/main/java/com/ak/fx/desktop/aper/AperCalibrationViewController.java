c45ae7e6e963bbb6cd12c1c6808de717d563bad7
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-07-31 22:33:41 +0300

    #F-148 In Progress Migrate to Spring Boot: aper-calibration context

--- src/main/java/com/ak/fx/desktop/aper/AperCalibrationViewController.java
+++ src/main/java/com/ak/fx/desktop/aper/AperCalibrationViewController.java
@@ -5,21 +5,21 @@ import javax.inject.Named;
 import com.ak.comm.GroupService;
 import com.ak.comm.bytes.BufferFrame;
 import com.ak.comm.converter.ToIntegerConverter;
-import com.ak.comm.converter.aper.AperOutVariable;
+import com.ak.comm.converter.aper.calibration.AperCalibrationVariable;
 import com.ak.comm.interceptor.BytesInterceptor;
 import com.ak.comm.interceptor.simple.RampBytesInterceptor;
 import com.ak.fx.desktop.AbstractViewController;
 import org.springframework.context.annotation.Profile;
 
 @Named
-@Profile("aper")
-public final class AperViewController
-    extends AbstractViewController<BufferFrame, BufferFrame, AperOutVariable> {
-  public AperViewController() {
+@Profile("aper-calibration")
+public final class AperCalibrationViewController
+    extends AbstractViewController<BufferFrame, BufferFrame, AperCalibrationVariable> {
+  public AperCalibrationViewController() {
     super(
         new GroupService<>(
             () -> new RampBytesInterceptor(BytesInterceptor.BaudRate.BR_460800, 25),
-            () -> new ToIntegerConverter<>(AperOutVariable.class, 1000)
+            () -> new ToIntegerConverter<>(AperCalibrationVariable.class, 1000)
         )
     );
   }
