f13d3307eaeb4a484c64870cfcb4d4288484ee7a
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-08-01 18:02:51 +0300

    #F-148 In Progress Migrate to Spring Boot: fix default context

--- src/main/java/com/ak/fx/desktop/DefaultViewController.java
+++ src/main/java/com/ak/fx/desktop/DefaultViewController.java
@@ -4,23 +4,21 @@ import javax.inject.Named;
 
 import com.ak.comm.GroupService;
 import com.ak.comm.bytes.BufferFrame;
-import com.ak.comm.converter.LinkedConverter;
+import com.ak.comm.converter.ADCVariable;
 import com.ak.comm.converter.ToIntegerConverter;
-import com.ak.comm.converter.aper.AperInVariable;
-import com.ak.comm.converter.aper.AperOutVariable;
 import com.ak.comm.interceptor.BytesInterceptor;
-import com.ak.comm.interceptor.simple.RampBytesInterceptor;
+import com.ak.comm.interceptor.simple.FixedFrameBytesInterceptor;
 import org.springframework.context.annotation.Profile;
 
 @Named
 @Profile("default")
 public final class DefaultViewController
-    extends AbstractViewController<BufferFrame, BufferFrame, AperOutVariable> {
+    extends AbstractViewController<BufferFrame, BufferFrame, ADCVariable> {
   public DefaultViewController() {
     super(
         new GroupService<>(
-            () -> new RampBytesInterceptor(BytesInterceptor.BaudRate.BR_460800, 25),
-            () -> new LinkedConverter<>(new ToIntegerConverter<>(AperInVariable.class, 1000), AperOutVariable.class)
+            () -> new FixedFrameBytesInterceptor(BytesInterceptor.BaudRate.BR_460800, 224),
+            () -> new ToIntegerConverter<>(ADCVariable.class, 1000)
         )
     );
   }
