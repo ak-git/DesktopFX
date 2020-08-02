bc568aca98a0dce95fa171fe61f752306ee3f910
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-08-02 15:00:42 +0300

    #F-148 In Progress Migrate to Spring Boot: nmi context refactoring

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -28,7 +28,7 @@ module com.ak.fx.desktop {
   opens com.ak.fx.desktop.rcm to spring.core;
   opens com.ak.fx.desktop to javafx.fxml, spring.core;
   opens com.ak.fx.scene to javafx.fxml, org.testng;
-  opens com.ak.comm.interceptor.nmis to org.testng;
+  opens com.ak.comm.interceptor.nmis to spring.beans, org.testng;
   opens com.ak.comm.interceptor.nmisr to org.testng;
   opens com.ak.comm.interceptor.rcm to spring.beans, org.testng;
   opens com.ak.comm.interceptor.rsce to org.testng;
@@ -36,14 +36,14 @@ module com.ak.fx.desktop {
   opens com.ak.comm.converter.nmis to org.testng;
   opens com.ak.comm.converter.aper to org.testng;
 
-  exports com.ak.comm.converter.rcm to org.testng;
   exports com.ak.comm.converter.aper to javafx.graphics, org.testng;
   exports com.ak.comm.converter.aper.calibration to org.testng;
+  exports com.ak.comm.converter.nmis to spring.beans;
+  exports com.ak.comm.converter.rcm to org.testng;
   exports com.ak.comm.converter.rcm.calibration to org.testng;
 
   exports com.ak.fx.desktop.aper to spring.beans;
   exports com.ak.fx.desktop.dft to spring.beans, spring.context;
-  exports com.ak.fx.desktop.nmis to spring.beans;
   exports com.ak.fx.desktop.nmisr to spring.beans;
   exports com.ak.fx.desktop.rcm to spring.beans, spring.context;
 
