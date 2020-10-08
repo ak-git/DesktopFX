8994c4cf3aba6ddfbbb63203be4352f8badefd8b
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-10-08 12:13:44 +0300

    #F-152 Fixed Cannot access to home user subdirectory on Windows

--- Util/src/main/java/com/ak/logging/OutputOSDirectory.java
+++ Util/src/main/java/com/ak/logging/OutputOSDirectory.java
@@ -4,14 +4,15 @@ import java.nio.file.Path;
 
 import com.ak.util.OSDirectories;
 import com.ak.util.OSDirectory;
+import com.ak.util.PropertiesSupport;
 
 public enum OutputOSDirectory implements OSDirectory {
   WINDOWS, MAC, UNIX;
 
-  private static final String[] PATHS = {"/Downloads/", "/Documents/"};
+  private static final String[] CANDIDATES = {"Downloads", "Documents"};
 
   @Override
   public Path getDirectory() {
-    return OSDirectories.getDirectory(PATHS);
+    return OSDirectories.getDirectory(CANDIDATES).resolve(PropertiesSupport.OUT_CONVERTER_PATH.value());
   }
 }
