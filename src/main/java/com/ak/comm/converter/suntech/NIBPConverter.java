a52dbf412f7826225bc8fcc97f019bb358abc2a7
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-10-10 17:44:22 +0300

    #F-154 In Progress SunTech NIBP protocol: add ping for pressure

--- src/main/java/com/ak/comm/converter/suntech/NIBPConverter.java
+++ src/main/java/com/ak/comm/converter/suntech/NIBPConverter.java
@@ -3,14 +3,10 @@ package com.ak.comm.converter.suntech;
 import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
-import javax.inject.Named;
 
 import com.ak.comm.bytes.suntech.NIBPResponse;
 import com.ak.comm.converter.AbstractConverter;
-import org.springframework.context.annotation.Profile;
 
-@Named
-@Profile("suntech")
 public final class NIBPConverter extends AbstractConverter<NIBPResponse, NIBPVariable> {
   private final int[] out = new int[NIBPVariable.values().length];
 
---
+++ src/main/java/com/ak/fx/desktop/suntech/NIBPViewController.java
@@ -0,0 +1,35 @@
+package com.ak.fx.desktop.suntech;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Named;
+
+import com.ak.comm.GroupService;
+import com.ak.comm.bytes.suntech.NIBPRequest;
+import com.ak.comm.bytes.suntech.NIBPResponse;
+import com.ak.comm.converter.suntech.NIBPConverter;
+import com.ak.comm.converter.suntech.NIBPVariable;
+import com.ak.comm.interceptor.suntech.NIBPBytesInterceptor;
+import com.ak.fx.desktop.AbstractViewController;
+import org.springframework.context.annotation.Profile;
+
+import static com.ak.comm.bytes.suntech.NIBPRequest.GET_CUFF_PRESSURE;
+
+@Named
+@Profile("suntech")
+public final class NIBPViewController extends AbstractViewController<NIBPRequest, NIBPResponse, NIBPVariable> {
+  private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+  public NIBPViewController() {
+    super(new GroupService<>(NIBPBytesInterceptor::new, NIBPConverter::new));
+    executorService.scheduleAtFixedRate(() -> service().write(GET_CUFF_PRESSURE), 0, 100, TimeUnit.MILLISECONDS);
+  }
+
+  @Override
+  public void close() {
+    executorService.shutdownNow();
+    super.close();
+  }
+}
