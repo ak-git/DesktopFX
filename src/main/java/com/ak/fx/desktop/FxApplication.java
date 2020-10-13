d86e08b6b35ef981d9032672a56fdeddf3d36830
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-10-13 09:22:03 +0300

    #F-154 In Progress SunTech NIBP protocol: multi-stage

--- src/main/java/com/ak/fx/desktop/FxApplication.java
+++ src/main/java/com/ak/fx/desktop/FxApplication.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
 import javax.annotation.OverridingMethodsMustInvokeSuper;
@@ -21,6 +24,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class FxApplication extends Application {
   private static final String KEY_PROPERTIES = "keys";
@@ -29,26 +33,37 @@ public class FxApplication extends Application {
 
   @Override
   @OverridingMethodsMustInvokeSuper
-  public void start(@Nonnull Stage stage) throws IOException {
+  public void start(@Nonnull Stage mainStage) throws IOException {
     ResourceBundle resourceBundle = ResourceBundle.getBundle(String.join(".", getClass().getPackageName(), KEY_PROPERTIES));
-    stage.setScene(getFXMLLoader(resourceBundle).get(0).load());
-    stage.setTitle(resourceBundle.getString(KEY_APPLICATION_TITLE));
-    OSDockImage.valueOf(OS.get().name()).setIconImage(stage,
+    List<FXMLLoader> fxmlLoaders = getFXMLLoader(resourceBundle);
+    OSDockImage.valueOf(OS.get().name()).setIconImage(mainStage,
         getClass().getResource(resourceBundle.getString(KEY_APPLICATION_IMAGE))
     );
 
-    Storage<Stage> stageStorage = OSStageStorage.valueOf(OS.get().name()).newInstance(getClass(), String.format("%d", 0));
-    stage.setOnCloseRequest(event -> stageStorage.save(stage));
-    stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-    addEventHandler(stage, () ->
-            Platform.runLater(() -> {
-              stage.setFullScreen(!stage.isFullScreen());
-              stage.setResizable(false);
-              stage.setResizable(true);
-            }),
-        KeyCode.CONTROL, KeyCode.SHORTCUT, KeyCode.F);
-    stage.show();
-    stageStorage.update(stage);
+    List<Stage> stages = Stream
+        .concat(
+            Stream.of(mainStage),
+            IntStream.range(1, fxmlLoaders.size()).mapToObj(i -> new Stage(StageStyle.DECORATED))
+        )
+        .collect(Collectors.toUnmodifiableList());
+
+    for (int i = 0; i < stages.size(); i++) {
+      Storage<Stage> stageStorage = OSStageStorage.valueOf(OS.get().name()).newInstance(getClass(), String.format("%d", i));
+      Stage stage = stages.get(i);
+      stage.setScene(fxmlLoaders.get(i).load());
+      stage.setTitle(resourceBundle.getString(KEY_APPLICATION_TITLE));
+      stage.setOnCloseRequest(event -> stageStorage.save(stage));
+      stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+      addEventHandler(stage, () ->
+              Platform.runLater(() -> {
+                stage.setFullScreen(!stage.isFullScreen());
+                stage.setResizable(false);
+                stage.setResizable(true);
+              }),
+          KeyCode.CONTROL, KeyCode.SHORTCUT, KeyCode.F);
+      stage.show();
+      stageStorage.update(stage);
+    }
   }
 
   @OverridingMethodsMustInvokeSuper
