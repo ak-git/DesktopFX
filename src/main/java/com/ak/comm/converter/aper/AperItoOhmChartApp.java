e3459f55b16e0a113ee64e89a92e7a93d5afdcec
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-10-26 22:12:19 +0300

    #F-155 In Progress Refactoring Aper chain variables: AperStage2UnitsVariableTest

--- src/main/java/com/ak/comm/converter/aper/AperItoOhmChartApp.java
+++ src/main/java/com/ak/comm/converter/aper/AperItoOhmChartApp.java
@@ -7,9 +7,9 @@ import com.ak.numbers.aper.AperCoefficients;
 /**
  * x = ADC, y = R(I-I)
  */
-public final class AperItoOhmChartApp extends AbstractSplineCoefficientsChartApp<ADCVariable, AperOutVariable> {
+public final class AperItoOhmChartApp extends AbstractSplineCoefficientsChartApp<ADCVariable, AperStage2UnitsVariable> {
   public AperItoOhmChartApp() {
-    super(AperCoefficients.ADC_TO_OHM, ADCVariable.ADC, AperOutVariable.CCR);
+    super(AperCoefficients.ADC_TO_OHM, ADCVariable.ADC, AperStage2UnitsVariable.CCR1);
   }
 
   public static void main(String[] args) {
