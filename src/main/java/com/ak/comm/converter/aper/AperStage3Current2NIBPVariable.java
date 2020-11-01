cd9a9adbb46b6865cfb2bacc6197db1827431de7
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-11-01 20:23:55 +0300

    #F-154 Fixed SunTech NIBP protocol: remove 1-st ECG channel

--- src/main/java/com/ak/comm/converter/aper/AperStage3Current2NIBPVariable.java
+++ src/main/java/com/ak/comm/converter/aper/AperStage3Current2NIBPVariable.java
@@ -1,12 +1,21 @@
 package com.ak.comm.converter.aper;
 
+import java.util.Collections;
+import java.util.List;
+
 import com.ak.comm.converter.DependentVariable;
 import com.ak.digitalfilter.DigitalFilter;
 import com.ak.digitalfilter.FilterBuilder;
 import com.ak.numbers.aper.AperRheoCoefficients;
 
 public enum AperStage3Current2NIBPVariable implements DependentVariable<AperStage2UnitsVariable, AperStage3Current2NIBPVariable> {
-  R1, ECG1, R2, ECG2, CCR1, CCR2;
+  R1, R2,
+  ECG {
+    @Override
+    public List<AperStage2UnitsVariable> getInputVariables() {
+      return Collections.singletonList(AperStage2UnitsVariable.ECG2);
+    }
+  }, CCR1, CCR2;
 
   @Override
   public final Class<AperStage2UnitsVariable> getInputVariablesClass() {
