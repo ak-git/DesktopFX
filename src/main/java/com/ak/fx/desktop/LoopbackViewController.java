6671f803a0512bfdb8987c2250bf9484f2bec151
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-11-04 10:42:57 +0300

    #F-156 In Progress Serial ports block each other and do not working together in multiple context: refactoring AbstractViewController

--- src/main/java/com/ak/fx/desktop/LoopbackViewController.java
+++ src/main/java/com/ak/fx/desktop/LoopbackViewController.java
@@ -1,16 +1,12 @@
 package com.ak.fx.desktop;
 
 import java.nio.ByteOrder;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
 
-import com.ak.comm.GroupService;
 import com.ak.comm.bytes.BufferFrame;
 import com.ak.comm.converter.ADCVariable;
 import com.ak.comm.converter.Converter;
@@ -19,20 +15,11 @@ import org.springframework.context.annotation.Profile;
 
 @Named
 @Profile("loopback")
-public final class LoopbackViewController extends AbstractViewController<BufferFrame, BufferFrame, ADCVariable> {
-  private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-
+public final class LoopbackViewController extends AbstractScheduledViewController<BufferFrame, BufferFrame, ADCVariable> {
   @Inject
   public LoopbackViewController(@Nonnull Provider<BytesInterceptor<BufferFrame, BufferFrame>> interceptorProvider,
                                 @Nonnull Provider<Converter<BufferFrame, ADCVariable>> converterProvider) {
-    super(new GroupService<>(interceptorProvider::get, converterProvider::get));
-    executorService.scheduleAtFixedRate(() -> service().write(new BufferFrame(new byte[] {(byte) 0xAA, 0, 0, 1, 0}, ByteOrder.LITTLE_ENDIAN)),
-        0, 1000 / 5, TimeUnit.MILLISECONDS);
-  }
-
-  @Override
-  public void close() {
-    executorService.shutdownNow();
-    super.close();
+    super(interceptorProvider, converterProvider,
+        () -> new BufferFrame(new byte[] {(byte) 0xAA, 0, 0, 1, 0}, ByteOrder.LITTLE_ENDIAN), 5.0);
   }
 }
