59494d06c90f63539f9861a14aa425a0e2438da6
Alexander Kobelev <ak.mail.ru@gmail.com>
2020-11-06 20:40:11 +0300

    #F-156 In Progress Serial ports block each other and do not working together in multiple context: fix MYO channels

--- src/main/java/com/ak/comm/converter/aper/AperStage1Variable.java
+++ src/main/java/com/ak/comm/converter/aper/AperStage1Variable.java
@@ -5,7 +5,6 @@ import javax.measure.Unit;
 import com.ak.comm.converter.Variable;
 import com.ak.digitalfilter.DigitalFilter;
 import com.ak.digitalfilter.FilterBuilder;
-import com.ak.numbers.common.CommonCoefficients;
 import tec.uom.se.unit.MetricPrefix;
 import tec.uom.se.unit.Units;
 
@@ -19,24 +18,7 @@ public enum AperStage1Variable implements Variable<AperStage1Variable> {
 
     @Override
     public DigitalFilter filter() {
-      return FilterBuilder.of().operator(() -> adc -> (int) Math.round((adc - ((1 << 17) * 25)) / 6.0)).iirMATLAB(
-          new double[] {
-              0.9022774304591, 0, 0, 0,
-              0, 0, 0, 0,
-              0, 0, 0, 0,
-              0, 0, 0, 0,
-              0, 0, 0, 0,
-              -0.9022774304591
-          },
-          new double[] {
-              1, 0, 0, 0,
-              0, 0, 0, 0,
-              0, 0, 0, 0,
-              0, 0, 0, 0,
-              0, 0, 0, 0,
-              -0.8045548609183
-          }
-      ).fir(CommonCoefficients.MYO).build();
+      return FilterBuilder.of().operator(() -> adc -> (int) Math.round((adc - ((1 << 17) * 25)) / 6.0)).build();
     }
   },
   CCU1,
