3b658fc894ce58ca6b154b5fa5a8dd510475968b
Alexander Kobelev <ak.mail.ru@gmail.com>
2021-03-13 10:16:49 +0300

    #F-174 Fixed KleiberMYO protocol

---
+++ Communication/src/main/java/com/ak/comm/converter/FloatToIntegerConverter.java
@@ -0,0 +1,23 @@
+package com.ak.comm.converter;
+
+import java.util.stream.Stream;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+import com.ak.comm.bytes.BufferFrame;
+
+public final class FloatToIntegerConverter<V extends Enum<V> & Variable<V>> extends AbstractConverter<BufferFrame, V> {
+  public FloatToIntegerConverter(@Nonnull Class<V> evClass, @Nonnegative int frequency) {
+    super(evClass, frequency);
+  }
+
+  @Override
+  protected Stream<int[]> innerApply(@Nonnull BufferFrame frame) {
+    int[] values = new int[variables().size()];
+    for (int i = 0; i < values.length; i++) {
+      values[i] = Math.round(frame.getFloat(1 + i * Float.BYTES));
+    }
+    return Stream.of(values);
+  }
+}
