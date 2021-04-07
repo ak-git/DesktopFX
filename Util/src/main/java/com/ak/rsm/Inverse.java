184e64932b99ea417f269d9565f66c1860a4a749
Alexander Kobelev <ak.mail.ru@gmail.com>
2021-04-07 18:57:02 +0300

    #F-118 In Progress Inverse problem solution for Rho1-Rho2-H-dH: fix error in K initial

--- Util/src/main/java/com/ak/rsm/Inverse.java
+++ Util/src/main/java/com/ak/rsm/Inverse.java
@@ -159,7 +159,7 @@ enum Inverse {
               return Inequality.absolute().applyAsDouble(subLog, subLogPredicted);
             },
             new SimpleBounds(new double[] {kMinMax[0], 0.0}, new double[] {kMinMax[1], Double.POSITIVE_INFINITY}),
-            new double[] {initialRelative.k12(), initialRelative.h()}, new double[] {0.01, 0.01}
+            new double[] {initialRelative.k12(), initialRelative.h()}, new double[] {0.01 * Math.signum(initialRelative.k12()), 0.01}
         );
 
     PointValuePair kwOptimal = find.apply(
