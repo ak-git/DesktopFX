66e2b471666d564ea4de03b37a819c726de2aff8
Alexander Kobelev <ak.mail.ru@gmail.com>
2021-12-11 15:37:48 +0300

    #F-206 Fixed Fix Rho1 relative and absolute errors calculations

--- Util/src/test/java/com/ak/rsm/InverseErrorsTest.java
+++ Util/src/test/java/com/ak/rsm/InverseErrorsTest.java
@@ -6,6 +6,7 @@ import java.util.stream.DoubleStream;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 import com.ak.math.Simplex;
 import com.ak.util.CSVLineFileBuilder;
@@ -27,20 +28,27 @@ public class InverseErrorsTest {
 
   @Test(dataProvider = "inverseable", enabled = false)
   public void testCSV(@Nonnull Inverseable<? extends Measurement> inverseable) {
-    double absErrorMilli = 0.001;
     CSVLineFileBuilder
-        .of((k, hToL) -> inverseable.errors(
-            Arrays.asList(TetrapolarSystem.systems2(absErrorMilli, 10.0)),
-            new Layer2RelativeMedium(k, hToL)
-        ))
+        .of((k, hToDim) -> single(new double[] {0.1, 1.9}, k, hToDim, inverseable))
         .xRange(-1.0, 1.0, 0.2)
-        .yLogRange(0.01, 1.0)
-        .saveTo("k %s".formatted(inverseable.getClass().getSimpleName()),
-            layers -> "%.4f".formatted(Math.abs(layers.k12AbsError() / layers.k12()) / (absErrorMilli / 50.0)))
-        .saveTo("dk %s".formatted(inverseable.getClass().getSimpleName()),
-            layers -> "%.4f".formatted(layers.k12AbsError() / (absErrorMilli / 50.0)))
-        .saveTo("hToL %s".formatted(inverseable.getClass().getSimpleName()),
-            layers -> "%.4f".formatted(Math.abs(layers.hToLAbsError() * 30 / 50.0) / (absErrorMilli / 50.0)))
+        .yLogRange(0.001, 1.0)
+        .saveTo("kRise %s".formatted(inverseable.getClass().getSimpleName()),
+            layers -> "%.4f".formatted(layers[0]))
+        .saveTo("hRise %s".formatted(inverseable.getClass().getSimpleName()),
+            layers -> "%.4f".formatted(layers[1]))
+        .generate();
+  }
+
+  @Test(dataProvider = "inverseable", enabled = false)
+  public void testCSV2(@Nonnull Inverseable<? extends Measurement> inverseable) {
+    CSVLineFileBuilder
+        .of((k, hToDim) -> single(new double[] {1.0 / 3.0, 5.0 / 3.0}, k, hToDim, inverseable))
+        .xRange(-1.0, 1.0, 0.2)
+        .yLogRange(0.001, 1.0)
+        .saveTo("k2Rise %s".formatted(inverseable.getClass().getSimpleName()),
+            layers -> "%.4f".formatted(layers[0]))
+        .saveTo("h2Rise %s".formatted(inverseable.getClass().getSimpleName()),
+            layers -> "%.4f".formatted(layers[1]))
         .generate();
   }
 
@@ -67,39 +75,51 @@ public class InverseErrorsTest {
   public static Object[][] single() {
     return new Object[][] {
         {1.0 / 3.0, 5.0 / 3.0},
-        {0.2964648042963249, 1.4039961460075951},
+        {0.1, 1.9},
     };
   }
 
   @Test(dataProvider = "single", enabled = false)
   public void testSingle(@Nonnegative double sToL1, @Nonnegative double sToL2) {
-    LOGGER.info("%.5f".formatted(single(new double[] {sToL1, sToL2}, 15.0 / 50.0, InverseStatic.INSTANCE)));
-    LOGGER.info("%.5f".formatted(single(new double[] {sToL1, sToL2}, 15.0 / 50.0, InverseDynamic.INSTANCE)));
+    LOGGER.info(Arrays.toString(single(new double[] {sToL1, sToL2}, 1.0, 25.0 / 50.0, InverseDynamic.INSTANCE)));
+    LOGGER.info(Arrays.toString(single(new double[] {sToL1, sToL2}, 1.0, 25.0 / 50.0, InverseStatic.INSTANCE)));
   }
 
-  private static double single(@Nonnull double[] p, @Nonnull Inverseable<? extends Measurement> inverse) {
-    return DoubleStream.iterate(0.02, h -> h < 1.0, h -> h += 0.02)
-        .map(h -> single(p, h, inverse)).parallel().sum();
+  @Test(dataProvider = "single", enabled = false)
+  public void testSingleSum(@Nonnegative double sToL1, @Nonnegative double sToL2) {
+    LOGGER.info("%.3f".formatted(single(new double[] {sToL1, sToL2}, InverseDynamic.INSTANCE)));
+    LOGGER.info("%.3f".formatted(single(new double[] {sToL1, sToL2}, InverseStatic.INSTANCE)));
   }
 
-  private static double single(@Nonnull double[] p, @Nonnegative double h, @Nonnull Inverseable<?> inverseable) {
-    double k = 1.0;
+  @ParametersAreNonnullByDefault
+  private static double single(double[] p, Inverseable<? extends Measurement> inverse) {
+    return DoubleStream.iterate(0.0001, h -> h < 1.0, h -> h += 0.0001)
+        .map(h -> single(p, 1.0, h, inverse)[0]).parallel().sum();
+  }
 
+  @Nonnull
+  @ParametersAreNonnullByDefault
+  private static double[] single(double[] p, double k, @Nonnegative double hToDim, Inverseable<?> inverseable) {
     double s1L = Math.min(p[0], p[1]);
     double s2L = Math.max(p[0], p[1]);
     double maxD = Math.max(1.0, s2L);
     double s2 = s2L / maxD;
     double L = s2 / s2L;
     double s1 = s1L * L;
-    double absError = 0.001;
+    double absError = 1.0e-4;
 
     RelativeMediumLayers errors = inverseable.errors(
         Arrays.asList(
             TetrapolarSystem.si(absError).s(s1).l(L),
             TetrapolarSystem.si(absError).s(s2).l(L)
         ),
-        new Layer2RelativeMedium(k, h / L)
+        new Layer2RelativeMedium(k, hToDim / L)
     );
-    return errors.hToLAbsError();
+
+    double oneDim = Math.max(Math.max(s1, s2), L);
+    return new double[] {
+        Math.abs(errors.k12AbsError() / errors.k12()) / (absError / oneDim),
+        Math.abs(errors.hToLAbsError() * L / oneDim) / (absError / oneDim),
+    };
   }
 }
