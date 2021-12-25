6debff6980351f9dc469655efa30ddb3b088c7c3
Alexander Kobelev <ak.mail.ru@gmail.com>
2021-12-25 22:47:25 +0300

    #F-207 In Progress Refactoring TetrapolarSystem to generate R in ohms: fix test case

--- src/main/java/com/ak/comm/converter/aper/AperStage5Current1Variable.java
+++ src/main/java/com/ak/comm/converter/aper/AperStage5Current1Variable.java
@@ -1,16 +1,111 @@
 package com.ak.comm.converter.aper;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nullable;
+import javax.measure.Unit;
+
 import com.ak.comm.converter.DependentVariable;
 import com.ak.digitalfilter.DigitalFilter;
+import com.ak.digitalfilter.FilterBuilder;
+import com.ak.rsm.resistance.Resistance;
+import com.ak.rsm.resistance.TetrapolarResistance;
+import tec.uom.se.unit.MetricPrefix;
+
+import static tec.uom.se.unit.Units.METRE;
+import static tec.uom.se.unit.Units.OHM;
 
 public enum AperStage5Current1Variable implements DependentVariable<AperStage4Current1Variable, AperStage5Current1Variable> {
   R1,
   R2,
+  APPARENT_06_18_RHO(6.0, 6.0 * 3.0) {
+    @Override
+    public List<AperStage4Current1Variable> getInputVariables() {
+      return Collections.singletonList(AperStage4Current1Variable.R1);
+    }
+
+    @Override
+    public Unit<?> getUnit() {
+      return MetricPrefix.MILLI(OHM).multiply(METRE);
+    }
+
+    @Override
+    public Set<Option> options() {
+      return Collections.singleton(Option.TEXT_VALUE_BANNER);
+    }
+  },
+  APPARENT_30_18_RHO(6.0 * 3.0, 6.0 * 5.0) {
+    @Override
+    public List<AperStage4Current1Variable> getInputVariables() {
+      return Collections.singletonList(AperStage4Current1Variable.R2);
+    }
+
+    @Override
+    public Unit<?> getUnit() {
+      return APPARENT_06_18_RHO.getUnit();
+    }
+
+    @Override
+    public Set<Option> options() {
+      return APPARENT_06_18_RHO.options();
+    }
+  },
+  APPARENT_07_21_RHO(7.0, 7.0 * 3.0) {
+    @Override
+    public List<AperStage4Current1Variable> getInputVariables() {
+      return Collections.singletonList(AperStage4Current1Variable.R1);
+    }
+
+    @Override
+    public Unit<?> getUnit() {
+      return MetricPrefix.MILLI(OHM).multiply(METRE);
+    }
+
+    @Override
+    public Set<Option> options() {
+      return Collections.singleton(Option.TEXT_VALUE_BANNER);
+    }
+  },
+  APPARENT_35_21_RHO(7.0 * 3.0, 7.0 * 5.0) {
+    @Override
+    public List<AperStage4Current1Variable> getInputVariables() {
+      return Collections.singletonList(AperStage4Current1Variable.R2);
+    }
+
+    @Override
+    public Unit<?> getUnit() {
+      return APPARENT_07_21_RHO.getUnit();
+    }
+
+    @Override
+    public Set<Option> options() {
+      return APPARENT_07_21_RHO.options();
+    }
+  },
   CCR;
+
+  @Nullable
+  private final TetrapolarResistance.PreBuilder<Resistance> builder;
+
+  AperStage5Current1Variable(@Nonnegative double smm, @Nonnegative double lmm) {
+    builder = TetrapolarResistance.milli(smm, lmm);
+  }
+
+  AperStage5Current1Variable() {
+    builder = null;
+  }
 
   @Override
   public final DigitalFilter filter() {
-    return DependentVariable.super.filter();
+    if (builder == null) {
+      return DependentVariable.super.filter();
+    }
+    else {
+      return FilterBuilder.of().operator(() -> rMilli -> (int) Math.round(builder.ofOhms(rMilli).resistivity())).build();
+    }
   }
 
   @Override
