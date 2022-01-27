1f04e65a28a1af20da6ad19c6c94aac511a59df5
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-01-27 11:52:21 +0300

    #F-208 In Progress Refactoring Inverseable classes: Inverse2Test for indentation inverse

--- Util/src/test/java/com/ak/rsm/inverse/Inverse2Test.java
+++ Util/src/test/java/com/ak/rsm/inverse/Inverse2Test.java
@@ -6,7 +6,9 @@ import java.util.DoubleSummaryStatistics;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
 
+import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import com.ak.math.Simplex;
@@ -20,8 +22,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Inverse2Test {
-  @DataProvider(name = "derivative-resistance-with-indent")
-  public static Object[][] derivativeResistanceWithIndent() {
+  @DataProvider(name = "derivative-resistance-with-fixed-indent")
+  public static Object[][] derivativeResistanceWithFixedIndent() {
     return new Object[][] {
         {
             List.of(
@@ -35,7 +37,7 @@ public class Inverse2Test {
   }
 
 
-  @Test(dataProvider = "derivative-resistance-with-indent", invocationCount = 2, enabled = false)
+  @Test(dataProvider = "derivative-resistance-with-fixed-indent", enabled = false)
   @ParametersAreNonnullByDefault
   public void test(Collection<Collection<? extends DerivativeResistivity>> ms, double[] indentations) {
     List<DynamicInverse> dynamicInverses = ms.stream().map(DynamicInverse::new).toList();
@@ -62,6 +64,58 @@ public class Inverse2Test {
     );
     Logger.getAnonymousLogger().info(() -> "%.6f; h = %.1f mm"
         .formatted(kwOptimal.getValue(), Metrics.toMilli(kwOptimal.getPoint()[1] * L))
+    );
+  }
+
+  @DataProvider(name = "derivative-resistance-with-unknown-indent")
+  public static Object[][] derivativeResistanceWithUnknownIndent() {
+    return new Object[][] {
+        {
+            List.of(
+                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0),
+                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 - 0.5),
+                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 - 0.5 * 2)
+            ),
+            Metrics.fromMilli(-1.0)
+        },
+        {
+            List.of(
+                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0),
+                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 + 0.1),
+                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 + 0.1 * 2)
+            ),
+            Metrics.fromMilli(1.0)
+        },
+    };
+  }
+
+
+  @Test(dataProvider = "derivative-resistance-with-unknown-indent", enabled = false)
+  public void test(@Nonnull Collection<Collection<? extends DerivativeResistivity>> ms, double maxIndent) {
+    List<DynamicInverse> dynamicInverses = ms.stream().map(DynamicInverse::new).toList();
+
+    DoubleSummaryStatistics statisticsL = ms.stream().mapToDouble(Measurements::getBaseL).summaryStatistics();
+    if (Double.compare(statisticsL.getMax(), statisticsL.getMin()) != 0) {
+      throw new IllegalStateException(statisticsL.toString());
+    }
+
+    double L = statisticsL.getAverage();
+    PointValuePair kwOptimal = Simplex.optimizeAll(
+        kw -> {
+          Iterator<double[]> iterator = IntStream.range(0, dynamicInverses.size()).mapToObj(i -> {
+            double[] kwIndent = kw.clone();
+            kwIndent[1] += kwIndent[2] * i / L;
+            return kwIndent;
+          }).iterator();
+
+          return dynamicInverses.stream().mapToDouble(value -> value.applyAsDouble(iterator.next()))
+              .reduce(StrictMath::hypot).orElseThrow();
+        },
+        new SimpleBounds(new double[] {-1.0, 0.0, Math.min(maxIndent, 0.0)}, new double[] {1.0, 1.0, Math.max(maxIndent, 0.0)}),
+        new double[] {0.01, 0.01, Math.abs(maxIndent) / 100.0}
+    );
+    Logger.getAnonymousLogger().info(() -> "%.6f; h = %.1f mm; indent = %.1f mm"
+        .formatted(kwOptimal.getValue(), Metrics.toMilli(kwOptimal.getPoint()[1] * L), Metrics.toMilli(kwOptimal.getPoint()[2]))
     );
   }
 }
