56294be380d56d0fac140c155ec1166b46c0db90
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-02-26 17:15:05 +0300

    #F-215 In Progress Inverse Layer 2 solution when POSITION changed in-time

--- Util/src/test/java/com/ak/rsm/inverse/Inverse2Test.java
+++ Util/src/test/java/com/ak/rsm/inverse/Inverse2Test.java
@@ -14,7 +14,13 @@ import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import com.ak.math.Simplex;
+import com.ak.math.ValuePair;
+import com.ak.rsm.measurement.DerivativeMeasurement;
 import com.ak.rsm.measurement.Measurements;
+import com.ak.rsm.measurement.TetrapolarDerivativeMeasurement;
+import com.ak.rsm.medium.Layer2Medium;
+import com.ak.rsm.medium.MediumLayers;
+import com.ak.rsm.relative.Layer2RelativeMedium;
 import com.ak.rsm.resistance.DerivativeResistivity;
 import com.ak.rsm.resistance.TetrapolarDerivativeResistance;
 import com.ak.util.Metrics;
@@ -29,9 +35,9 @@ public class Inverse2Test {
     return new Object[][] {
         {
             List.of(
-                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0),
-                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 - 0.5),
-                TetrapolarDerivativeResistance.milli().dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 - 1.0)
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0),
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 - 0.5),
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105).system2(6.0).rho1(5.0).rho2(1.0).h(2.0 - 1.0)
             ),
             new double[] {0.0, Metrics.fromMilli(-0.5), Metrics.fromMilli(-1.0)}
         },
@@ -41,7 +47,7 @@ public class Inverse2Test {
 
   @Test(dataProvider = "derivative-resistance-with-fixed-indent", enabled = false)
   @ParametersAreNonnullByDefault
-  public void test(Collection<Collection<? extends DerivativeResistivity>> ms, double[] indentations) {
+  public void test(Collection<Collection<? extends DerivativeMeasurement>> ms, double[] indentations) {
     List<DynamicInverse> dynamicInverses = ms.stream().map(DynamicInverse::new).toList();
 
     DoubleSummaryStatistics statisticsL = ms.stream().mapToDouble(Measurements::getBaseL).summaryStatistics();
@@ -64,9 +70,11 @@ public class Inverse2Test {
         new SimpleBounds(new double[] {-1.0, 0}, new double[] {1.0, 1.0}),
         new double[] {0.01, 0.01}
     );
-    Logger.getAnonymousLogger().info(() -> "%.6f; h = %.1f mm"
-        .formatted(kwOptimal.getValue(), Metrics.toMilli(kwOptimal.getPoint()[1] * L))
-    );
+    List<Layer2Medium> mediumList = ms.stream().map(dm -> new Layer2Medium(dm, new Layer2RelativeMedium(kwOptimal.getPoint()))).toList();
+    var rho1 = mediumList.stream().map(MediumLayers::rho1).reduce(ValuePair::mergeWith).orElseThrow();
+    var rho2 = mediumList.stream().map(MediumLayers::rho2).reduce(ValuePair::mergeWith).orElseThrow();
+    var h = mediumList.stream().map(MediumLayers::h1).reduce(ValuePair::mergeWith).orElseThrow();
+    Logger.getAnonymousLogger().info(() -> "%.6f; %s; %s; %s".formatted(kwOptimal.getValue(), rho1, rho2, h));
   }
 
   @DataProvider(name = "derivative-resistance-with-unknown-indent")
@@ -112,7 +120,7 @@ public class Inverse2Test {
   }
 
   @Test(dataProvider = "derivative-resistance-with-unknown-indent", invocationCount = 3, enabled = false)
-  public void test(@Nonnull Collection<Collection<? extends DerivativeResistivity>> ms, double maxIndent) {
+  public void test2(@Nonnull Collection<Collection<? extends DerivativeResistivity>> ms, double maxIndent) {
     List<DynamicInverse> dynamicInverses = ms.stream().map(DynamicInverse::new).toList();
 
     DoubleSummaryStatistics statisticsL = ms.stream().mapToDouble(Measurements::getBaseL).summaryStatistics();
