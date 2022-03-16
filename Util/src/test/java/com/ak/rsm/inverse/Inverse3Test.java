986eed8e1d9669d85f44a938f8c377b661ffee28
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-03-16 18:35:19 +0300

    #F-215 In Progress Inverse Layer 2 solution when POSITION changed in-time: three-layer algorithm

--- Util/src/test/java/com/ak/rsm/inverse/Inverse3Test.java
+++ Util/src/test/java/com/ak/rsm/inverse/Inverse3Test.java
@@ -116,34 +116,66 @@ public class Inverse3Test {
   @Test(dataProvider = "noChanged", enabled = false)
   @ParametersAreNonnullByDefault
   public void testNoChanged(Collection<Collection<? extends DerivativeMeasurement>> ms, int[] indentations) {
-    double hStep = Metrics.fromMilli(0.1);
-    List<ToDoubleFunction<double[]>> dynamicInverses = ms.stream().map(dm -> DynamicInverse.of(dm, hStep)).toList();
-
     DoubleSummaryStatistics statisticsL = ms.stream().mapToDouble(Measurements::getBaseL).summaryStatistics();
     if (Double.compare(statisticsL.getMax(), statisticsL.getMin()) != 0) {
       throw new IllegalStateException("L is not equal for all electrode systems %s".formatted(statisticsL));
     }
 
-    PointValuePair kwOptimal = Simplex.optimizeAll(
-        kw -> {
-          Iterator<double[]> iterator = Arrays.stream(indentations).mapToObj(x -> {
-            double[] kwIndent = kw.clone();
-            kwIndent[3] += x;
-            return kwIndent;
-          }).iterator();
-
-          return dynamicInverses.stream().mapToDouble(value -> value.applyAsDouble(iterator.next()))
-              .reduce(StrictMath::hypot).orElseThrow();
-        },
-        new Simplex.Bounds(-1.0, 1.0), new Simplex.Bounds(-1.0, 1.0),
-        new Simplex.Bounds(1, 100), new Simplex.Bounds(1 + Arrays.stream(indentations).map(Math::abs).max().orElse(0), 100)
+    int p1 = 10;
+    Function<Integer, PointValuePair> cache = new ConcurrentCache<>(
+        p2mp1 -> Simplex.optimizeAll(
+            kw -> {
+              double hStep = kw[2];
+              List<ToDoubleFunction<double[]>> dynamicInverses = ms.stream().map(dm -> DynamicInverse.of(dm, hStep)).toList();
+              Iterator<double[]> iterator = Arrays.stream(indentations)
+                  .mapToObj(
+                      x -> new double[] {kw[0], kw[1], p1, p2mp1 + x}
+                  )
+                  .iterator();
+              return dynamicInverses.stream().mapToDouble(value -> value.applyAsDouble(iterator.next()))
+                  .reduce(StrictMath::hypot).orElseThrow();
+            },
+            new Simplex.Bounds(-1.0, 0.0, 1.0),
+            new Simplex.Bounds(-1.0, 0.0, 1.0),
+            new Simplex.Bounds(Metrics.fromMilli(0.001), Metrics.fromMilli(0.1), Metrics.fromMilli(2.0))
+        )
     );
 
-    var rho1 = ms.stream().map(dm -> getRho1(dm, kwOptimal.getPoint(), hStep)).reduce(ValuePair::mergeWith).orElseThrow();
-    var rho2 = ValuePair.Name.RHO_2.of(rho1.value() / Layers.getRho1ToRho2(kwOptimal.getPoint()[0]), 0.0);
-    var rho3 = ValuePair.Name.RHO_3.of(rho2.value() / Layers.getRho1ToRho2(kwOptimal.getPoint()[1]), 0.0);
-    Logger.getAnonymousLogger().info(() -> "%.6f %s; %s; %s; %s"
-        .formatted(kwOptimal.getValue(), Arrays.toString(kwOptimal.getPoint()), rho1, rho2, rho3));
+    Phenotype<IntegerGene, Double> phenotype = Engine
+        .builder(
+            p2mp1 -> cache.apply(p2mp1[0]).getValue(),
+            Codecs.ofVector(IntRange.of(25, 100))
+        )
+        .populationSize(1 << 3)
+        .optimize(Optimize.MINIMUM)
+        .alterers(new Mutator<>(0.03), new MeanAlterer<>(0.6))
+        .build().stream()
+        .limit(Limits.bySteadyFitness(7))
+        .limit(100)
+        .peek(r -> Logger.getAnonymousLogger().info(() -> r.bestPhenotype().toString()))
+        .collect(toBestPhenotype());
+
+    Genotype<IntegerGene> best = phenotype.genotype();
+    PointValuePair pOptimal = new PointValuePair(
+        IntStream.range(0, best.length()).mapToDouble(i -> best.get(i).get(0).doubleValue()).toArray(),
+        phenotype.fitness()
+    );
+
+    int p2mp1 = (int) pOptimal.getPoint()[0];
+    PointValuePair kwOptimal = cache.apply(p2mp1);
+    double[] kwpp = {kwOptimal.getPoint()[0], kwOptimal.getPoint()[1], p1, p2mp1};
+    double hStep = kwOptimal.getPoint()[2];
+
+    var rho1 = ms.stream().map(dm -> getRho1(dm, kwpp, hStep)).reduce(ValuePair::mergeWith).orElseThrow();
+    var rho2 = ValuePair.Name.RHO_2.of(rho1.value() / Layers.getRho1ToRho2(kwpp[0]), 0.0);
+    var rho3 = ValuePair.Name.RHO_3.of(rho2.value() / Layers.getRho1ToRho2(kwpp[1]), 0.0);
+    Logger.getAnonymousLogger().info(
+        () -> "%.6f %s; %s; %s; %s; %s; %s; %s".formatted(
+            kwOptimal.getValue(), ValuePair.Name.K12.of(kwpp[0], 0.0), ValuePair.Name.K23.of(kwpp[1], 0.0),
+            ValuePair.Name.H.of(hStep, 0.0),
+            Arrays.stream(kwpp).skip(2).map(p -> p * hStep).mapToObj(h -> ValuePair.Name.H.of(h, 0.0)).toList(),
+            rho1, rho2, rho3)
+    );
   }
 
   @Nonnegative
