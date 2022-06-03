583a5693270c007d35dc0368b51c88f1360f9f57
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-06-03 12:28:23 +0300

    #F-215 In Progress Inverse Layer 2 solution when POSITION changed in-time: simplify code

--- Util/src/main/java/com/ak/rsm/inverse/Layer3DynamicInverse.java
+++ Util/src/main/java/com/ak/rsm/inverse/Layer3DynamicInverse.java
@@ -1,7 +1,8 @@
 package com.ak.rsm.inverse;
 
 import java.util.Collection;
-import java.util.function.DoubleToIntFunction;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 import java.util.function.ToDoubleBiFunction;
 
 import javax.annotation.Nonnegative;
@@ -14,21 +15,25 @@ import org.apache.commons.math3.complex.Complex;
 import static java.lang.StrictMath.log;
 
 final class Layer3DynamicInverse extends AbstractLayerInverse {
-  private static final DoubleToIntFunction ROUND_TO_INT = value -> Math.toIntExact(Math.round(value));
-
   Layer3DynamicInverse(@Nonnull Collection<TetrapolarSystem> systems, @Nonnegative double hStep) {
-    super(systems, () -> {
-      ToDoubleBiFunction<TetrapolarSystem, double[]> logApparentPredicted = (s, kw) ->
-          Apparent3Rho.newLog1pApparent3Rho(s.relativeSystem())
-              .value(kw[0], kw[1], hStep / s.lCC(), ROUND_TO_INT.applyAsInt(kw[2]), ROUND_TO_INT.applyAsInt(kw[3]));
-      ToDoubleBiFunction<TetrapolarSystem, double[]> diffApparentPredicted = (s, kw) ->
-          Apparent3Rho.newDerivativeApparentByPhi2Rho(s, new double[] {kw[0], kw[1]}, hStep, ROUND_TO_INT
-              .applyAsInt(kw[2]), ROUND_TO_INT.applyAsInt(kw[3]));
-      return (s, kw) -> {
-        double dR = diffApparentPredicted.applyAsDouble(s, kw);
-        double real = logApparentPredicted.applyAsDouble(s, kw) - log(Math.abs(dR));
-        return new Complex(real, real * Math.signum(dR));
-      };
+    super(systems, new Supplier<>() {
+      @Override
+      public BiFunction<TetrapolarSystem, double[], Complex> get() {
+        ToDoubleBiFunction<TetrapolarSystem, double[]> logApparentPredicted = (s, kw) ->
+            Apparent3Rho.newLog1pApparent3Rho(s.relativeSystem())
+                .value(kw[0], kw[1], hStep / s.lCC(), toInt(kw[2]), toInt(kw[3]));
+        ToDoubleBiFunction<TetrapolarSystem, double[]> diffApparentPredicted = (s, kw) ->
+            Apparent3Rho.newDerivativeApparentByPhi2Rho(s, new double[] {kw[0], kw[1]}, hStep, toInt(kw[2]), toInt(kw[3]));
+        return (s, kw) -> {
+          double dR = diffApparentPredicted.applyAsDouble(s, kw);
+          double real = logApparentPredicted.applyAsDouble(s, kw) - log(Math.abs(dR));
+          return new Complex(real, real * Math.signum(dR));
+        };
+      }
+
+      private static int toInt(double value) {
+        return Math.toIntExact(Math.round(value));
+      }
     });
   }
 }
