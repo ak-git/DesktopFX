85192c03db9edde4cfedda708ec683bfdf60fa87
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-06-22 19:43:15 +0300

    #F-222 Fixed Special protocol for Andrey Briko test stand v3

--- src/main/java/com/ak/comm/converter/briko/BrikoVariable.java
+++ src/main/java/com/ak/comm/converter/briko/BrikoVariable.java
@@ -1,46 +1,12 @@
 package com.ak.comm.converter.briko;
 
-import java.util.Collections;
-import java.util.Set;
-
-import javax.measure.Unit;
-
 import com.ak.comm.converter.Variable;
-import com.ak.digitalfilter.DigitalFilter;
-import com.ak.digitalfilter.FilterBuilder;
-import com.ak.util.Metrics;
-import tec.uom.se.unit.Units;
 
 public enum BrikoVariable implements Variable<BrikoVariable> {
-  A {
-    @Override
-    public Set<Option> options() {
-      return Option.defaultOptions();
-    }
-
-    @Override
-    public DigitalFilter filter() {
-      return FilterBuilder.of().smoothingImpulsive(10)
-          .operator(() -> adc -> Math.toIntExact(Math.round(-0.0135 * adc + 10771)))
-          .operator(() -> gram -> Math.toIntExact(Math.round(gram / 1000.0 / (Metrics.fromMilli(100.0) * Metrics.fromMilli(60.0)))))
-          .operator(() -> x -> -x + 2420)
-          .build();
-    }
-
-    @Override
-    public Unit<?> getUnit() {
-      return Units.PASCAL;
-    }
-
-  },
+  A,
   B,
   C,
   D,
   E,
-  F;
-
-  @Override
-  public Set<Option> options() {
-    return Collections.emptySet();
-  }
+  F
 }
