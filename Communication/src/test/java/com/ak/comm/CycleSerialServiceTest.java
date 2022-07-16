bb7ddce785729f4927f9f564cb814e1206782de7
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-07-16 20:17:01 +0300

    #F-225 In Progress Migrate from TestNG to JUnit5 and AssertJ: package comm

--- Communication/src/test/java/com/ak/comm/CycleSerialServiceTest.java
+++ Communication/src/test/java/com/ak/comm/CycleSerialServiceTest.java
@@ -11,14 +11,15 @@ import com.ak.comm.converter.ToIntegerConverter;
 import com.ak.comm.interceptor.BytesInterceptor;
 import com.ak.comm.interceptor.simple.RampBytesInterceptor;
 import com.ak.comm.serial.CycleSerialService;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import static com.ak.util.UIConstants.UI_DELAY;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
 
-public class CycleSerialServiceTest {
+class CycleSerialServiceTest {
   @Test
-  public void testBytesInterceptor() throws InterruptedException {
+  void testBytesInterceptor() throws InterruptedException {
     CountDownLatch latch = new CountDownLatch(1);
 
     CycleSerialService<BufferFrame, BufferFrame, ADCVariable> service =
@@ -35,17 +36,17 @@ public class CycleSerialServiceTest {
 
       @Override
       public void onError(Throwable throwable) {
-        Assert.fail(throwable.getMessage(), throwable);
+        fail(throwable.getMessage(), throwable);
       }
 
       @Override
       public void onComplete() {
-        Assert.fail();
+        fail();
       }
     });
     service.write(new BufferFrame(new byte[] {1, 2}, ByteOrder.nativeOrder()));
     service.refresh(false);
-    Assert.assertFalse(latch.await(UI_DELAY.getSeconds(), TimeUnit.SECONDS));
+    assertFalse(latch.await(UI_DELAY.getSeconds(), TimeUnit.SECONDS));
     service.close();
   }
 }
