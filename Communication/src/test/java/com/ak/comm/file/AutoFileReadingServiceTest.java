25bff994551c1a2e11f363551846b69ae207725b
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-07-16 22:19:31 +0300

    #F-225 In Progress Migrate from TestNG to JUnit5 and AssertJ: package comm

--- Communication/src/test/java/com/ak/comm/file/AutoFileReadingServiceTest.java
+++ Communication/src/test/java/com/ak/comm/file/AutoFileReadingServiceTest.java
@@ -13,7 +13,7 @@ import com.ak.comm.converter.TwoVariables;
 import com.ak.comm.interceptor.BytesInterceptor;
 import com.ak.comm.interceptor.simple.RampBytesInterceptor;
 import com.ak.util.Strings;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -23,25 +23,47 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class AutoFileReadingServiceTest implements Flow.Subscriber<int[]> {
-  private final AutoFileReadingService<BufferFrame, BufferFrame, TwoVariables> service = new AutoFileReadingService<>(
-      () -> new RampBytesInterceptor(getClass().getName(), BytesInterceptor.BaudRate.BR_115200, 1 + TwoVariables.values().length * Integer.BYTES),
+class AutoFileReadingServiceTest {
+  private static final AutoFileReadingService<BufferFrame, BufferFrame, TwoVariables> SERVICE = new AutoFileReadingService<>(
+      () ->
+          new RampBytesInterceptor(
+              AutoFileReadingServiceTest.class.getName(),
+              BytesInterceptor.BaudRate.BR_115200, 1 + TwoVariables.values().length * Integer.BYTES
+          ),
       () -> new ToIntegerConverter<>(TwoVariables.class, 1000));
 
-  @BeforeEach
-  public void setUp() {
-    service.subscribe(this);
+  @BeforeAll
+  static void setUp() {
+    SERVICE.subscribe(new Flow.Subscriber<>() {
+      @Override
+      public void onSubscribe(Flow.Subscription s) {
+        s.request(Long.MAX_VALUE);
+      }
+
+      @Override
+      public void onNext(int[] item) {
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        fail(t.getMessage(), t);
+      }
+
+      @Override
+      public void onComplete() {
+      }
+    });
   }
 
   @ParameterizedTest
   @MethodSource("com.ak.comm.file.FileDataProvider#parallelRampFiles")
   void testAccept(@Nonnull Path file) {
-    assertTrue(service.accept(file.toFile()));
+    assertTrue(SERVICE.accept(file.toFile()));
     int countFrames = 10;
     ByteBuffer buffer = ByteBuffer.allocate(TwoVariables.values().length * Integer.BYTES * countFrames);
     while (!Thread.currentThread().isInterrupted()) {
       buffer.clear();
-      service.read(buffer, 0);
+      SERVICE.read(buffer, 0);
       buffer.flip();
       if (buffer.limit() == buffer.capacity()) {
         for (int i = 0; i < countFrames; i++) {
@@ -56,25 +78,7 @@ class AutoFileReadingServiceTest implements Flow.Subscriber<int[]> {
 
   @Test
   void testNotAccept() {
-    assertFalse(service.accept(Paths.get(Strings.EMPTY).toFile()));
-  }
-
-  @Override
-  public void onSubscribe(Flow.Subscription s) {
-    s.request(Long.MAX_VALUE);
-  }
-
-  @Override
-  public void onNext(int[] ints) {
-  }
-
-  @Override
-  public void onError(Throwable t) {
-    fail(t.getMessage(), t);
-  }
-
-  @Override
-  public void onComplete() {
+    assertFalse(SERVICE.accept(Paths.get(Strings.EMPTY).toFile()));
   }
 
   @Override
