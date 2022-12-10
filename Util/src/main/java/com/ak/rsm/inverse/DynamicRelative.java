3220af8d4080825228409cd455a309ba02815a30
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-12-10 16:02:18 +0300

    #F-228 In Progress PolynomialCurveFitter for Inverse Layer 3 solution: update to Java 19

--- Util/src/main/java/com/ak/rsm/inverse/DynamicRelative.java
+++ Util/src/main/java/com/ak/rsm/inverse/DynamicRelative.java
@@ -1,16 +1,16 @@
 package com.ak.rsm.inverse;
 
-import java.util.Collection;
-import java.util.function.ToDoubleFunction;
-import java.util.function.UnaryOperator;
-
-import javax.annotation.Nonnull;
-
 import com.ak.math.Simplex;
 import com.ak.rsm.measurement.DerivativeMeasurement;
 import com.ak.rsm.relative.Layer2RelativeMedium;
 import com.ak.rsm.relative.RelativeMediumLayers;
 import org.apache.commons.math3.optim.PointValuePair;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.function.ToDoubleFunction;
+import java.util.function.UnaryOperator;
+import java.util.logging.Logger;
 
 import static com.ak.rsm.relative.RelativeMediumLayers.NAN;
 
@@ -35,17 +35,28 @@ final class DynamicRelative extends AbstractRelative<DerivativeMeasurement, Rela
     }
     else if (measurements().stream().allMatch(d -> d.derivativeResistivity() < 0)) {
       kMinMax = new Simplex.Bounds(0.0, 1.0);
-    }
-    else if (measurements().stream().anyMatch(d -> d.derivativeResistivity() > 0) &&
-        measurements().stream().anyMatch(d -> d.derivativeResistivity() < 0)) {
+    } else if (measurements().stream().anyMatch(d -> d.derivativeResistivity() > 0) &&
+            measurements().stream().anyMatch(d -> d.derivativeResistivity() < 0)) {
       return NAN;
-    }
-    else {
+    } else {
       return new StaticRelative(measurements()).get();
     }
 
-    PointValuePair kwOptimal = Simplex.optimizeAll(dynamicInverse::applyAsDouble,
-        kMinMax, new Simplex.Bounds(0.0, getMaxHToL())
+    double alpha = 0.0;
+    Logger.getLogger(getClass().getName()).info(() -> "alpha = %.2f".formatted(alpha));
+
+    PointValuePair kwOptimal = Simplex.optimizeAll(kw -> {
+              double k = kw[0];
+              double hToL = kw[1];
+              double min = getMinHToL(k);
+              double max = getMaxHToL(k);
+              if (min < hToL && hToL < max) {
+                return StrictMath.hypot(dynamicInverse.applyAsDouble(kw), alpha * (StrictMath.log(hToL) - StrictMath.log(min)));
+              } else {
+                return Double.MAX_VALUE;
+              }
+            },
+            kMinMax, new Simplex.Bounds(0.0, getMaxHToL(1.0))
     );
     return apply(new Layer2RelativeMedium(kwOptimal.getPoint()));
   }
---
+++ Util/src/test/java/com/ak/rsm/inverse/InverseTestE7932Provider.java
@@ -0,0 +1,81 @@
+package com.ak.rsm.inverse;
+
+import com.ak.rsm.measurement.TetrapolarDerivativeMeasurement;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.ak.rsm.measurement.Measurements.fixOhms;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class InverseTestE7932Provider {
+    private InverseTestE7932Provider() {
+    }
+
+    static Stream<Arguments> meatFat() {
+        double r1 = 19.47067;
+        double r2 = 35.88068;
+        double r3i = 16.88488;
+        double r4i = 40.48156;
+        return Stream.of(
+                arguments(
+                        List.of(
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.21).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 - 0.5115441, r2 - 0.4903946, r3i - 0.2135771, r4i - 0.7176235
+                                        )),
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 - 0.3291593, r2 - 0.3336468, r3i - 0.1212310, r4i - 0.4532481
+                                        )),
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 + 0.2234464, r2 + 0.3055168, r3i + 0.1018700, r4i + 0.3963058
+                                        )),
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.21).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 + 0.5676624, r2 + 0.8385997, r3i + 0.2994841, r4i + 1.0384897
+                                        ))
+                        )
+                )
+        );
+    }
+
+    static Stream<Arguments> meat() {
+        double r1 = 5.619337;
+        double r2 = 8.976139;
+        double r3i = 4.31785;
+        double r4i = 9.188934;
+        return Stream.of(
+                arguments(
+                        List.of(
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.21).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 - 0.02665655, r2 - 0.010386613, r3i - 0.016149800, r4i - 0.010249956
+                                        )),
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 - 0.01803588, r2 - 0.004433184, r3i - 0.008536334, r4i - 0.008540883
+                                        )),
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 + 0.01107191, r2 + 0.005857321, r3i + 0.001531586, r4i + 0.013473002
+                                        )),
+                                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.21).system4(10.0)
+                                        .ofOhms(fixOhms(
+                                                r1, r2, r3i, r4i,
+                                                r1 + 0.04725428, r2 + 0.024173583, r3i + 0.045525415, r4i + 0.031669155
+                                        ))
+                        )
+                )
+        );
+    }
+}
