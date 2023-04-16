ebc61e585c5b7bef25121f2ca752aa2fe7a63a43
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-04-16 12:08:04 +0300

    #F-235 In Progress Refactoring Inverse calculations

--- Util/src/main/java/com/ak/rsm/inverse/AbstractInverseFunction.java
+++ Util/src/main/java/com/ak/rsm/inverse/AbstractInverseFunction.java
@@ -6,30 +6,29 @@ import com.ak.rsm.system.TetrapolarSystem;
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.ToDoubleBiFunction;
 import java.util.function.ToDoubleFunction;
 
 abstract class AbstractInverseFunction<R extends Resistivity> extends AbstractInverse
     implements ToDoubleFunction<double[]>, ToDoubleBiFunction<TetrapolarSystem, double[]> {
+  private record Experiment(@Nonnull TetrapolarSystem system, double measured) {
+  }
+
   @Nonnull
-  private final double[] measured;
+  private final Collection<Experiment> experiments;
 
   @ParametersAreNonnullByDefault
   AbstractInverseFunction(Collection<? extends R> r, ToDoubleFunction<? super R> toData) {
     super(r.stream().map(Resistivity::system).toList());
-    measured = r.stream().mapToDouble(toData).toArray();
+    experiments = r.stream().map(res -> new Experiment(res.system(), toData.applyAsDouble(res))).toList();
   }
 
   @Nonnegative
   @Override
   public final double applyAsDouble(@Nonnull double[] kw) {
-    double[] model = systems().stream().mapToDouble(s -> applyAsDouble(s, kw)).toArray();
-    double[] err = new double[Math.max(measured.length, model.length)];
-    for (int i = 0; i < err.length; i++) {
-      err[i] = StrictMath.log(measured[i] / model[i]);
-    }
-    return Arrays.stream(err).filter(Double::isFinite).reduce(Math::hypot).orElse(Double.POSITIVE_INFINITY);
+    return experiments.stream()
+        .map(e -> e.measured / applyAsDouble(e.system, kw)).mapToDouble(StrictMath::log)
+        .filter(Double::isFinite).reduce(Math::hypot).orElse(Double.POSITIVE_INFINITY);
   }
 }
