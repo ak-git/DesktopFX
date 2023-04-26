b51cfab701f442df39950623bd36e26c84559a23
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-04-26 17:52:07 +0300

    #F-235 In Progress Refactoring Inverse calculations: sonar An Enum-based Singleton implementation was detected. Make sure the use of the Singleton pattern is required and an Enum-based implementation is the right one for the context.

--- Util/src/main/java/com/ak/logging/CalibrateBuilders.java
+++ Util/src/main/java/com/ak/logging/CalibrateBuilders.java
@@ -1,37 +1,34 @@
 package com.ak.logging;
 
-import java.io.IOException;
-import java.lang.ref.Cleaner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.ak.util.Clean;
 import com.ak.util.Extension;
 import com.ak.util.LocalFileIO;
 import com.ak.util.LocalIO;
 
-public enum CalibrateBuilders implements Cleaner.Cleanable {
-  CALIBRATION;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public enum CalibrateBuilders {
+  ;
+
+  private static final LocalFileIO.AbstractBuilder BUILDER = new LogPathBuilder(Extension.JSON, LocalFileHandler.class)
+      .addPath(CalibrateBuilders.class.getSimpleName());
 
   static {
-    Clean.clean(values());
+    clean();
   }
 
-  @Override
-  public void clean() {
+  public static void clean() {
     try {
-      Clean.clean(newBuilder().build().getPath());
+      Clean.clean(BUILDER.build().getPath());
     }
     catch (IOException e) {
-      Logger.getLogger(getClass().getName()).log(Level.WARNING, e.getMessage(), e);
+      Logger.getLogger(CalibrateBuilders.class.getName()).log(Level.WARNING, e.getMessage(), e);
     }
   }
 
-  public final LocalIO build(String fileName) {
-    return newBuilder().fileName(fileName).build();
-  }
-
-  private LocalFileIO.AbstractBuilder newBuilder() {
-    return new LogPathBuilder(Extension.JSON, LocalFileHandler.class).addPath(name().toLowerCase());
+  public static LocalIO build(String fileName) {
+    return BUILDER.fileName(fileName).build();
   }
 }
