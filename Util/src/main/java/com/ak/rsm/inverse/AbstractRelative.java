da573a4f9eaceb8bb06358c95dc7eabf50e57925
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-04-26 13:32:09 +0300

    #F-235 In Progress Refactoring Inverse calculations: refactoring AbstractRelative

--- Util/src/main/java/com/ak/rsm/inverse/AbstractRelative.java
+++ Util/src/main/java/com/ak/rsm/inverse/AbstractRelative.java
@@ -1,25 +1,37 @@
 package com.ak.rsm.inverse;
 
 import com.ak.rsm.measurement.Measurement;
+import com.ak.rsm.measurement.Measurements;
+import com.ak.rsm.relative.RelativeMediumLayers;
+import com.ak.rsm.system.InexactTetrapolarSystem;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.function.Function;
 import java.util.function.ToDoubleFunction;
+import java.util.function.UnaryOperator;
 
-abstract sealed class AbstractRelative<M extends Measurement, L> extends AbstractErrors implements Inverse<L>, ToDoubleFunction<double[]>
+abstract sealed class AbstractRelative<M extends Measurement, L> implements Inverse<L>, ToDoubleFunction<double[]>
     permits DynamicRelative, StaticRelative {
   @Nonnull
   private final Collection<M> measurements;
   @Nonnull
   private final ToDoubleFunction<double[]> inverse;
+  @Nonnull
+  private final Regularization regularization;
+  @Nonnull
+  private final UnaryOperator<RelativeMediumLayers> errors;
 
   @ParametersAreNonnullByDefault
-  AbstractRelative(Collection<? extends M> measurements, ToDoubleFunction<double[]> inverse) {
-    super(measurements.stream().map(Measurement::inexact).toList());
+  AbstractRelative(Collection<? extends M> measurements, ToDoubleFunction<double[]> inverse,
+                   Function<Collection<InexactTetrapolarSystem>, Regularization> regularizationFunction,
+                   UnaryOperator<RelativeMediumLayers> errors) {
     this.measurements = Collections.unmodifiableCollection(measurements);
     this.inverse = inverse;
+    regularization = regularizationFunction.apply(Measurements.inexact(measurements));
+    this.errors = errors;
   }
 
   @Override
@@ -28,7 +40,18 @@ abstract sealed class AbstractRelative<M extends Measurement, L> extends Abstrac
   }
 
   @Nonnull
+  @Override
+  public final RelativeMediumLayers apply(@Nonnull RelativeMediumLayers layers) {
+    return errors.apply(layers);
+  }
+
+  @Nonnull
   final Collection<M> measurements() {
     return measurements;
+  }
+
+  @Nonnull
+  final Regularization regularization() {
+    return regularization;
   }
 }
--- Util/src/main/java/com/ak/rsm/inverse/StaticRelative.java
+++ Util/src/main/java/com/ak/rsm/inverse/StaticRelative.java
@@ -2,35 +2,26 @@ package com.ak.rsm.inverse;
 
 import com.ak.math.Simplex;
 import com.ak.rsm.measurement.Measurement;
+import com.ak.rsm.measurement.Measurements;
 import com.ak.rsm.relative.Layer2RelativeMedium;
 import com.ak.rsm.relative.RelativeMediumLayers;
 import org.apache.commons.math3.optim.PointValuePair;
 
 import javax.annotation.Nonnull;
 import java.util.Collection;
-import java.util.function.UnaryOperator;
 
 final class StaticRelative extends AbstractRelative<Measurement, RelativeMediumLayers> {
-  @Nonnull
-  private final UnaryOperator<RelativeMediumLayers> staticErrors;
-
   StaticRelative(@Nonnull Collection<? extends Measurement> measurements) {
-    super(measurements, new StaticInverse(measurements));
-    staticErrors = new StaticErrors(inexactSystems());
+    super(measurements, new StaticInverse(measurements), Regularization.Interval.ZERO_MAX.of(0.0),
+        new StaticErrors(Measurements.inexact(measurements)));
   }
 
   @Nonnull
   @Override
   public RelativeMediumLayers get() {
     PointValuePair kwOptimal = Simplex.optimizeAll(this::applyAsDouble,
-        new Simplex.Bounds(-1.0, 1.0), Regularization.Interval.ZERO_MAX.of(0.0).apply(inexactSystems()).hInterval(1.0)
+        new Simplex.Bounds(-1.0, 1.0), regularization().hInterval(1.0)
     );
     return apply(new Layer2RelativeMedium(kwOptimal.getPoint()));
-  }
-
-  @Nonnull
-  @Override
-  public RelativeMediumLayers apply(@Nonnull RelativeMediumLayers layers) {
-    return staticErrors.apply(layers);
   }
 }
