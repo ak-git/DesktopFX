edf2568e37037358def5b4dc16ec1ed268c920bb
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-05-05 13:28:24 +0300

    #F-235 In Progress Refactoring Inverse calculations: fix E7645

--- Util/src/test/java/com/ak/rsm/inverse/InverseTestE7645Provider.java
+++ Util/src/test/java/com/ak/rsm/inverse/InverseTestE7645Provider.java
@@ -1,12 +1,11 @@
 package com.ak.rsm.inverse;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 import com.ak.rsm.measurement.TetrapolarDerivativeMeasurement;
 import org.junit.jupiter.params.provider.Arguments;
 
-import static com.ak.rsm.measurement.Measurements.fixOhms;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class InverseTestE7645Provider {
@@ -30,17 +29,14 @@ class InverseTestE7645Provider {
   static Stream<Arguments> n18_09_44() {
     double r1 = 164.0202;
     double r2 = 239.5422;
-    double r3i = 106.2974;
-    double r4i = 238.2361;
-    double smmBase = 10.0;
+    double r3 = 212.5948;
+    double r4 = 263.8774;
     return Stream.of(
         arguments(
             List.of(
-                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105 * 2).system4(smmBase)
-                    .ofOhms(fixOhms(
-                        r1, r2, r3i, r4i,
-                        r1 + 0.09589374, r2 + 0.1857942, r3i + 0.09455904, r4i + 0.1690772
-                    ))
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105 * 2).system4(10.0)
+                    .ofOhms(r1, r2, r3, r4,
+                        r1 + 0.09589374, r2 + 0.1857942, r3 + 0.18911808, r4 + 0.14903632)
             )
         )
     );
@@ -63,17 +59,14 @@ class InverseTestE7645Provider {
   static Stream<Arguments> n18_10_42() {
     double r1 = 164.9797;
     double r2 = 240.6624;
-    double r3i = 106.4757;
-    double r4i = 238.2181;
-    double smmBase = 10.0;
+    double r3 = 212.9514;
+    double r4 = 263.4848;
     return Stream.of(
         arguments(
             List.of(
-                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105 * 2).system4(smmBase)
-                    .ofOhms(fixOhms(
-                        r1, r2, r3i, r4i,
-                        r1 + 0.0593253, r2 + 0.0954033, r3i + 0.03899084, r4i + 0.05644644
-                    ))
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105 * 2).system4(10.0)
+                    .ofOhms(r1, r2, r3, r4,
+                        r1 + 0.0593253, r2 + 0.0954033, r3 + 0.07798168, r4 + 0.0349112)
             )
         )
     );
@@ -96,17 +89,14 @@ class InverseTestE7645Provider {
   static Stream<Arguments> n18_12_22() {
     double r1 = 165.9119;
     double r2 = 249.7204;
-    double r3i = 111.4075;
-    double r4i = 253.5355;
-    double smmBase = 10.0;
+    double r3 = 222.815;
+    double r4 = 284.256;
     return Stream.of(
         arguments(
             List.of(
-                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105 * 2).system4(smmBase)
-                    .ofOhms(fixOhms(
-                        r1, r2, r3i, r4i,
-                        r1 - 0.1805104, r2 - 0.6408006, r3i - 0.2145646, r4i - 0.6304807
-                    ))
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(-0.105 * 2).system4(10.0)
+                    .ofOhms(r1, r2, r3, r4,
+                        r1 - 0.1805104, r2 - 0.6408006, r3 - 0.4291292, r4 - 0.8318322)
             )
         )
     );
@@ -126,11 +116,10 @@ class InverseTestE7645Provider {
   static Stream<Arguments> n18_12_59() {
     double r1 = 166.1417;
     double r2 = 250.6794;
-    double smmBase = 10.0;
     return Stream.of(
         arguments(
             List.of(
-                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105 * 2).system2(smmBase)
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105 * 2).system2(10.0)
                     .ofOhms(r1, r2, r1 + 0.1511312, r2 + 0.5706979)
             )
         )
