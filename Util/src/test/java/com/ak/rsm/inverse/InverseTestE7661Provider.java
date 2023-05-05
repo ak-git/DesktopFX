0a21ed2ad2c4be8074e2f9d426150c3df2b79a1a
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-05-05 13:33:03 +0300

    #F-235 In Progress Refactoring Inverse calculations: fix E7661

--- Util/src/test/java/com/ak/rsm/inverse/InverseTestE7661Provider.java
+++ Util/src/test/java/com/ak/rsm/inverse/InverseTestE7661Provider.java
@@ -1,12 +1,11 @@
 package com.ak.rsm.inverse;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 import com.ak.rsm.measurement.TetrapolarDerivativeMeasurement;
 import org.junit.jupiter.params.provider.Arguments;
 
-import static com.ak.rsm.measurement.Measurements.fixOhms;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class InverseTestE7661Provider {
@@ -30,17 +29,14 @@ class InverseTestE7661Provider {
   static Stream<Arguments> ak14_26_39() {
     double r1 = 96.9555;
     double r2 = 142.482;
-    double r3i = 61.62316;
-    double r4i = 138.7322;
-    double smmBase = 7.0;
+    double r3 = 123.24632;
+    double r4 = 154.21808;
     return Stream.of(
         arguments(
             List.of(
-                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105 * 2).system4(smmBase)
-                    .ofOhms(fixOhms(
-                        r1, r2, r3i, r4i,
-                        r1 + 0.1498728, r2 + 0.4866054, r3i + 0.1321608, r4i + 0.4280954
-                    ))
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105 * 2).system4(7.0)
+                    .ofOhms(r1, r2, r3, r4,
+                        r1 + 0.1498728, r2 + 0.4866054, r3 + 0.2643216, r4 + 0.5918692)
             )
         )
     );
@@ -63,17 +59,14 @@ class InverseTestE7661Provider {
   static Stream<Arguments> ak14_27_16layer3() {
     double r1 = 92.58523;
     double r2 = 133.918;
-    double r3i = 56.93037;
-    double r4i = 130.7278;
-    double smmBase = 7.0;
+    double r3 = 113.86074;
+    double r4 = 147.59486;
     return Stream.of(
         arguments(
             List.of(
-                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105 * 2).system4(smmBase)
-                    .ofOhms(fixOhms(
-                        r1, r2, r3i, r4i,
-                        r1 - 0.007848626, r2 + 0.1189516, r3i + 0.04099936, r4i + 0.1235003
-                    ))
+                TetrapolarDerivativeMeasurement.milli(0.1).dh(0.105 * 2).system4(7.0)
+                    .ofOhms(r1, r2, r3, r4,
+                        r1 - 0.007848626, r2 + 0.1189516, r3 + 0.08199872, r4 + 0.16500188)
             )
         )
     );
