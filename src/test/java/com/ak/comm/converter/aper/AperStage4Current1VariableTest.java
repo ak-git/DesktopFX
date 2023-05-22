b08e0a89d5675e94286df7e5847484fb2e73cd3b
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-05-22 20:45:26 +0300

    #F-66 Fixed Calibration for Aper: update

--- src/test/java/com/ak/comm/converter/aper/AperStage4Current1VariableTest.java
+++ src/test/java/com/ak/comm/converter/aper/AperStage4Current1VariableTest.java
@@ -1,19 +1,7 @@
 package com.ak.comm.converter.aper;
 
-import java.nio.ByteOrder;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Stream;
-
-import javax.annotation.Nonnull;
-
 import com.ak.comm.bytes.BufferFrame;
-import com.ak.comm.converter.Converter;
-import com.ak.comm.converter.DependentVariable;
-import com.ak.comm.converter.LinkedConverter;
-import com.ak.comm.converter.ToIntegerConverter;
-import com.ak.comm.converter.Variable;
+import com.ak.comm.converter.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -21,6 +9,13 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import tec.uom.se.unit.MetricPrefix;
 import tec.uom.se.unit.Units;
+
+import javax.annotation.Nonnull;
+import java.nio.ByteOrder;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -38,7 +33,7 @@ class AperStage4Current1VariableTest {
                 5, 0, 0, 0,
                 (byte) 0xd0, 0x07, 0, 0},
 
-            new int[] {55466, 5982, 330990, 5982, 1322}
+            new int[] {55318, 5982, 330990, 5982, 1302}
         )
     );
   }
