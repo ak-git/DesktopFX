58274162afa9250474563283d3b027e24f65c6e2
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-06-12 12:58:36 +0300

    #F-235 Fixed Refactoring Inverse calculations: calculate contribution

--- Util/src/test/java/com/ak/rsm/inverse/InverseDynamicTest.java
+++ Util/src/test/java/com/ak/rsm/inverse/InverseDynamicTest.java
@@ -33,6 +33,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.function.ObjDoubleConsumer;
 import java.util.logging.Level;
@@ -280,7 +281,7 @@ class InverseDynamicTest {
     try (DirectoryStream<Path> p = Files.newDirectoryStream(Paths.get(Strings.EMPTY), Extension.CSV.attachTo("*mm"))) {
       return StreamSupport.stream(p.spliterator(), false)
           .map(Path::toString)
-          .flatMap(file -> DoubleStream.of(0.2, 0.5, 1.0).mapToObj(alpha -> arguments(file, alpha, 3)))
+          .flatMap(file -> DoubleStream.of(0.2, 0.5, 1.0).mapToObj(alpha -> arguments(file, alpha)))
           .toList();
     }
   }
@@ -288,7 +289,7 @@ class InverseDynamicTest {
   @ParameterizedTest
   @MethodSource({"cvsFiles"})
   @Disabled("ignored com.ak.rsm.inverse.InverseDynamicTest.testInverseDynamicLayerFileResistivity")
-  void testInverseDynamicLayerFileResistivity(@Nonnull String fileName, @Nonnegative double alpha, @Nonnegative int eachSelect) {
+  void testInverseDynamicLayerFileResistivity(@Nonnull String fileName, @Nonnegative double alpha) {
     String T = "TIME";
     String POSITION = "POSITION";
     String RHO_S1 = "A1";
@@ -309,6 +310,30 @@ class InverseDynamicTest {
     outputMap.put("RMS_BASE", medium -> medium.getRMS()[0]);
     outputMap.put("RMS_DIFF", medium -> medium.getRMS()[1]);
 
+    String PREDICTED_R1 = "PREDICTED_R1";
+    String PREDICTED_R2 = "PREDICTED_R2";
+    String PREDICTED_DIFF_R1 = "PREDICTED_DIFF_R1";
+    String PREDICTED_DIFF_R2 = "PREDICTED_DIFF_R2";
+    String CONTRIBUTION_RHO1_1 = "CONTRIBUTION_rho1_1";
+    String CONTRIBUTION_RHO1_2 = "CONTRIBUTION_rho1_2";
+    String CONTRIBUTION_RHO2_1 = "CONTRIBUTION_rho2_1";
+    String CONTRIBUTION_RHO2_2 = "CONTRIBUTION_rho2_2";
+    String CONTRIBUTION_H_1 = "CONTRIBUTION_h_1";
+    String CONTRIBUTION_H_2 = "CONTRIBUTION_h_2";
+
+    Map<String, Object> predictedMap = new LinkedHashMap<>();
+    predictedMap.put(PREDICTED_R1, Double.NaN);
+    predictedMap.put(PREDICTED_DIFF_R1, Double.NaN);
+    predictedMap.put(CONTRIBUTION_RHO1_1, Double.NaN);
+    predictedMap.put(CONTRIBUTION_RHO2_1, Double.NaN);
+    predictedMap.put(CONTRIBUTION_H_1, Double.NaN);
+
+    predictedMap.put(PREDICTED_R2, Double.NaN);
+    predictedMap.put(PREDICTED_DIFF_R2, Double.NaN);
+    predictedMap.put(CONTRIBUTION_RHO1_2, Double.NaN);
+    predictedMap.put(CONTRIBUTION_RHO2_2, Double.NaN);
+    predictedMap.put(CONTRIBUTION_H_2, Double.NaN);
+
     Path path = Paths.get(Extension.CSV.attachTo(fileName));
     try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
       reader.mark(8192);
@@ -322,11 +347,12 @@ class InverseDynamicTest {
            CSVLineFileCollector collector = new CSVLineFileCollector(
                Paths.get(Extension.CSV.attachTo(
                    String.format(Locale.ROOT, "%s inverse - %.1f", Extension.CSV.clean(fileName), alpha))),
-               Stream.concat(inputHeaders.stream(), outputMap.keySet().stream()).toArray(String[]::new)
+               Stream.of(inputHeaders.stream(), outputMap.keySet().stream(), predictedMap.keySet().stream())
+                   .flatMap(Function.identity()).toArray(String[]::new)
            )
       ) {
+        AtomicReference<MediumLayers> prevMediumRC = new AtomicReference<>(null);
         assertTrue(StreamSupport.stream(parser.spliterator(), false)
-            .filter(r -> (r.getRecordNumber() - 1) % eachSelect == 0)
             .map(r -> {
               LOGGER.info(() -> "%.2f sec; %s mm".formatted(Double.parseDouble(r.get(T)), r.get(POSITION)));
               MediumLayers medium = new DynamicAbsolute(TetrapolarDerivativeMeasurement.milli(0.1)
@@ -336,10 +362,46 @@ class InverseDynamicTest {
                       Double.parseDouble(r.get(RHO_S1_DIFF)), Double.parseDouble(r.get(RHO_S2_DIFF))
                   ), Regularization.Interval.ZERO_MAX.of(alpha)).get();
               LOGGER.info(medium::toString);
-              return Stream.concat(
+
+              double[] ohms = TetrapolarResistance.milli().system2(sBase)
+                  .rho1(medium.rho1().value()).rho2(medium.rho2().value()).h(medium.h1().value())
+                  .stream().mapToDouble(Resistance::ohms).toArray();
+              predictedMap.keySet().forEach(s -> predictedMap.put(s, Double.NaN));
+              predictedMap.put(PREDICTED_R1, ohms[0]);
+              predictedMap.put(PREDICTED_R2, ohms[1]);
+
+              MediumLayers prevMedium = prevMediumRC.getAndSet(medium);
+              if (prevMedium != null && Double.isFinite(prevMedium.rho().value()) && Double.isFinite(medium.rho().value())) {
+                double[] baseOhms = TetrapolarResistance.milli().system2(sBase)
+                    .rho1(prevMedium.rho1().value()).rho2(prevMedium.rho2().value()).h(prevMedium.h1().value()).stream()
+                    .mapToDouble(Resistance::ohms).toArray();
+
+                double[] dRho1Ohms = TetrapolarResistance.milli().system2(sBase)
+                    .rho1(medium.rho1().value()).rho2(prevMedium.rho2().value()).h(prevMedium.h1().value()).stream()
+                    .mapToDouble(Resistance::ohms).toArray();
+                double[] dRho2Ohms = TetrapolarResistance.milli().system2(sBase)
+                    .rho1(prevMedium.rho1().value()).rho2(medium.rho2().value()).h(prevMedium.h1().value()).stream()
+                    .mapToDouble(Resistance::ohms).toArray();
+                double[] dHOhms = TetrapolarResistance.milli().system2(sBase)
+                    .rho1(prevMedium.rho1().value()).rho2(prevMedium.rho2().value()).h(medium.h1().value()).stream()
+                    .mapToDouble(Resistance::ohms).toArray();
+
+                predictedMap.put(PREDICTED_DIFF_R1, ohms[0] - baseOhms[0]);
+                predictedMap.put(CONTRIBUTION_RHO1_1, dRho1Ohms[0] - baseOhms[0]);
+                predictedMap.put(CONTRIBUTION_RHO2_1, dRho2Ohms[0] - baseOhms[0]);
+                predictedMap.put(CONTRIBUTION_H_1, dHOhms[0] - baseOhms[0]);
+
+                predictedMap.put(PREDICTED_DIFF_R2, ohms[1] - baseOhms[1]);
+                predictedMap.put(CONTRIBUTION_RHO1_2, dRho1Ohms[1] - baseOhms[1]);
+                predictedMap.put(CONTRIBUTION_RHO2_2, dRho2Ohms[1] - baseOhms[1]);
+                predictedMap.put(CONTRIBUTION_H_2, dHOhms[1] - baseOhms[1]);
+              }
+
+              return Stream.of(
                   inputHeaders.stream().map(r::get),
-                  outputMap.values().stream().map(f -> f.apply(medium))
-              ).toArray();
+                  outputMap.values().stream().map(f -> f.apply(medium)),
+                  predictedMap.values().stream()
+              ).flatMap(Function.identity()).toArray();
             })
             .collect(collector));
       }
