6c530802ff598cceb6d258aceca3fd8031676f86
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-09-15 16:36:12 +0300

    #F-222 Fixed Andrey Briko Fatal protocol parsing error

--- src/main/java/com/ak/comm/converter/briko/BrikoVariable.java
+++ src/main/java/com/ak/comm/converter/briko/BrikoVariable.java
@@ -2,24 +2,18 @@ package com.ak.comm.converter.briko;
 
 import com.ak.comm.converter.Variable;
 
-import java.util.Collections;
 import java.util.Set;
 
 public enum BrikoVariable implements Variable<BrikoVariable> {
-  C1 {
-    @Override
-    public Set<Option> options() {
-      return Variable.Option.addToDefault(Option.TEXT_VALUE_BANNER);
-    }
-  },
-  C2,
-  C3,
-  C4,
-  IGNORE1 {
-    @Override
-    public Set<Option> options() {
-      return Collections.emptySet();
-    }
-  },
-  IGNORE2
+  A,
+  B,
+  C,
+  D,
+  E,
+  F;
+
+  @Override
+  public Set<Option> options() {
+    return Variable.Option.addToDefault(Option.TEXT_VALUE_BANNER);
+  }
 }
