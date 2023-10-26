26f6c690442c77a52cf0b5b22fdc3db81e253a30
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-10-26 17:59:06 +0300

    #F-246 Fixed Andrey Briko test stand Black Small

--- src/test/java/com/ak/comm/converter/briko/BrikoStage1VariableTest.java
+++ src/test/java/com/ak/comm/converter/briko/BrikoStage1VariableTest.java
@@ -2,17 +2,15 @@ package com.ak.comm.converter.briko;
 
 import com.ak.comm.converter.Variable;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import tec.uom.se.AbstractUnit;
-import tec.uom.se.unit.Units;
 
-import javax.annotation.Nonnull;
 import java.util.EnumSet;
 import java.util.List;
 
-import static com.ak.util.Strings.ANGLE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static tec.uom.se.unit.MetricPrefix.MILLI;
+import static tec.uom.se.unit.Units.GRAM;
+import static tec.uom.se.unit.Units.METRE;
 
 class BrikoStage1VariableTest {
   @Test
@@ -21,10 +19,10 @@ class BrikoStage1VariableTest {
         .allMatch(option -> option == Variable.Option.VISIBLE);
   }
 
-  @ParameterizedTest
-  @EnumSource(value = BrikoStage1Variable.class)
-  void testFilterDelay(@Nonnull Variable<BrikoStage1Variable> variable) {
-    assertThat(variable.filter().getDelay()).isZero();
+  @Test
+  void testFilterDelay() {
+    assertThat(EnumSet.allOf(BrikoStage1Variable.class).stream().mapToDouble(value -> value.filter().getDelay()).toArray())
+        .containsExactly(34.0, 34.0, 34.0, 34.0, 0.0, 0.0);
   }
 
   @Test
@@ -32,8 +30,7 @@ class BrikoStage1VariableTest {
     assertThat(EnumSet.allOf(BrikoStage1Variable.class).stream().map(Variable::getUnit))
         .isEqualTo(
             List.of(
-                AbstractUnit.ONE, AbstractUnit.ONE, AbstractUnit.ONE, AbstractUnit.ONE,
-                Units.RADIAN.alternate(ANGLE).divide(1000.0), Units.RADIAN.alternate(ANGLE).divide(1000.0)
+                GRAM, GRAM, GRAM, GRAM, MILLI(METRE), AbstractUnit.ONE
             )
         );
   }
--- src/test/java/com/ak/comm/converter/briko/BrikoStage2VariableTest.java
+++ src/test/java/com/ak/comm/converter/briko/BrikoStage2VariableTest.java
@@ -9,29 +9,31 @@ import tec.uom.se.unit.Units;
 import javax.annotation.Nonnull;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.ak.comm.converter.Variable.Option.TEXT_VALUE_BANNER;
 import static com.ak.comm.converter.Variable.Option.VISIBLE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static tec.uom.se.unit.MetricPrefix.MILLI;
+import static tec.uom.se.unit.Units.METRE;
+import static tec.uom.se.unit.Units.PASCAL;
 
 class BrikoStage2VariableTest {
   @Test
   void testOptions() {
-    assertThat(EnumSet.allOf(BrikoStage2Variable.class).stream().flatMap(v -> v.options().stream()))
-        .isEqualTo(
-            List.of(VISIBLE, TEXT_VALUE_BANNER, VISIBLE, TEXT_VALUE_BANNER)
-        );
+    assertThat(EnumSet.allOf(BrikoStage2Variable.class).stream().flatMap(v -> v.options().stream()).collect(Collectors.toSet()))
+        .contains(VISIBLE, TEXT_VALUE_BANNER);
   }
 
   @ParameterizedTest
   @EnumSource(value = BrikoStage2Variable.class)
   void testFilterDelay(@Nonnull Variable<BrikoStage2Variable> variable) {
-    assertThat(variable.filter().getDelay()).isEqualTo(34.0);
+    assertThat(variable.filter().getDelay()).isZero();
   }
 
   @Test
   void testGetUnit() {
     assertThat(EnumSet.allOf(BrikoStage2Variable.class).stream().map(Variable::getUnit))
-        .isEqualTo(List.of(Units.GRAM, Units.GRAM));
+        .isEqualTo(List.of(Units.GRAM, Units.GRAM, MILLI(METRE), PASCAL));
   }
 }
