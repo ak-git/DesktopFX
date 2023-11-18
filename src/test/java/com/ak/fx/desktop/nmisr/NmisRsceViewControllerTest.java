58f644e38cc42dcac55c9d9c98febd728e58365c
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-11-18 12:14:55 +0300

    #F-243 In Progress Integrate SKTB-PR with NMIS-RSCE protocol: add test case

--- src/test/java/com/ak/fx/desktop/nmisr/NmisRsceViewControllerTest.java
+++ src/test/java/com/ak/fx/desktop/nmisr/NmisRsceViewControllerTest.java
@@ -6,13 +6,18 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.IntStream;
 
 class NmisRsceViewControllerTest {
   @Test
   void testGet() {
     try (NmisRsceViewController controller = new NmisRsceViewController(event -> Assertions.fail(event.toString()))) {
-      Assertions.assertThat(controller.get())
-          .isIn(EnumSet.allOf(NmisRequest.Sequence.class).stream().map(NmisRequest.Sequence::build).toArray());
+      controller.close();
+      List<NmisRequest> first6 = IntStream.range(0, 6).mapToObj(value -> controller.get()).toList();
+      List<NmisRequest> second6 = IntStream.range(0, 6).mapToObj(value -> controller.get()).toList();
+      Assertions.assertThat(first6).isEqualTo(second6)
+          .isSubsetOf(EnumSet.allOf(NmisRequest.Sequence.class).stream().map(NmisRequest.Sequence::build).toList());
     }
     catch (IOException e) {
       Assertions.fail(e.getMessage(), e);
