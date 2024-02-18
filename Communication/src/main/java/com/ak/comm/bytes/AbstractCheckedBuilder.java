dbe1531d8d796adc455ff35a28e046a6367a2c60
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-02-18 09:51:50 +0300

    #F-256 In Progress The package-info.java File: package com.ak.comm.bytes

--- Communication/src/main/java/com/ak/comm/bytes/AbstractCheckedBuilder.java
+++ Communication/src/main/java/com/ak/comm/bytes/AbstractCheckedBuilder.java
@@ -1,25 +1,23 @@
 package com.ak.comm.bytes;
 
+import com.ak.util.Builder;
+
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.logging.Logger;
-
-import javax.annotation.Nonnull;
-
-import com.ak.util.Builder;
 
 import static com.ak.comm.bytes.LogUtils.LOG_LEVEL_ERRORS;
 
 public abstract class AbstractCheckedBuilder<T> implements BytesChecker, Builder<T> {
-  @Nonnull
   private final ByteBuffer buffer;
 
-  protected AbstractCheckedBuilder(@Nonnull ByteBuffer buffer) {
-    this.buffer = buffer;
+  protected AbstractCheckedBuilder(ByteBuffer buffer) {
+    this.buffer = Objects.requireNonNull(buffer);
   }
 
   @Override
-  public final void bufferLimit(@Nonnull ByteBuffer buffer) {
+  public final void bufferLimit(ByteBuffer buffer) {
     throw new UnsupportedOperationException(Arrays.toString(buffer.array()));
   }
 
---
+++ Communication/src/main/java/com/ak/comm/bytes/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package com.ak.comm.bytes;
+
+import javax.annotation.ParametersAreNonnullByDefault;
