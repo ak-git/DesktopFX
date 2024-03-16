0e690112d8b51cf3d8a90c0d1d27e262dc00296c
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-03-16 18:14:41 +0300

    #F-258 Fixed Organize packages by domain: FIX NPE, Fields should be declared at the top of the class, before any method declarations, constructors, initializers or inner classes

--- Util/src/main/java/com/ak/logging/OutputOSDirectory.java
+++ Util/src/main/java/com/ak/logging/OutputOSDirectory.java
@@ -10,11 +10,15 @@ import static com.ak.util.OSDirectories.VENDOR_ID;
 public enum OutputOSDirectory implements OSDirectory {
   WINDOWS, MAC, UNIX;
 
-  public static final OSDirectory DIRECTORY = OSDirectory.of(OutputOSDirectory.class);
   private static final String[] CANDIDATES = {"Downloads", "Documents"};
 
   @Override
   public Path getDirectory() {
     return OSDirectories.getDirectory(CANDIDATES).resolve(VENDOR_ID);
+  }
+
+  public enum Constants {
+    ;
+    public static final OSDirectory DIRECTORY = OSDirectory.of(OutputOSDirectory.class);
   }
 }
