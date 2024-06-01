0e82b0f768432813a4e3f0d81baca274ea5996be
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-06-01 16:16:07 +0300

    #F-239 In Progress Inverse solution for three-layer model: add ZERO_MAX_LOG1P

--- Util/src/main/java/com/ak/math/ValuePair.java
+++ Util/src/main/java/com/ak/math/ValuePair.java
@@ -150,7 +150,7 @@ public class ValuePair {
       );
     }
     else {
-      return Double.isFinite(v) ? name.toString("%6.3f".formatted(v)) : name.toString(Double.toString(v));
+      return Double.isFinite(v) ? name.toString("%6.3f".formatted(v).strip()) : name.toString(Double.toString(v));
     }
   }
 
