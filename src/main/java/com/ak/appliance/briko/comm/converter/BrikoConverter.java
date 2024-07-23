76fad0331169ebf427a0b51fbbc80ecedca8b235
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-07-23 18:45:28 +0300

    #F-265 In Progress Special protocol for Andrey Briko MAX30001 aggregator: first try

--- src/main/java/com/ak/appliance/briko/comm/converter/BrikoConverter.java
+++ src/main/java/com/ak/appliance/briko/comm/converter/BrikoConverter.java
@@ -6,7 +6,6 @@ import com.ak.comm.converter.AbstractConverter;
 import java.util.stream.Stream;
 
 import static com.ak.appliance.briko.comm.converter.BrikoVariable.FREQUENCY;
-import static java.lang.Integer.BYTES;
 
 public final class BrikoConverter extends AbstractConverter<BufferFrame, BrikoVariable> {
   public BrikoConverter() {
@@ -17,7 +16,7 @@ public final class BrikoConverter extends AbstractConverter<BufferFrame, BrikoVa
   protected Stream<int[]> innerApply(BufferFrame frame) {
     var values = new int[variables().size()];
     for (var i = 0; i < values.length; i++) {
-      values[i] = frame.getInt(2 + i * (1 + BYTES) + 1);
+      values[i] = (frame.get(1 + (i * 4) + 3) << 16) + (frame.get(1 + (i * 4) + 2) << 8) + frame.get(1 + (i * 4) + 1);
     }
     return Stream.of(values);
   }
--- src/main/java/com/ak/appliance/briko/comm/converter/BrikoVariable.java
+++ src/main/java/com/ak/appliance/briko/comm/converter/BrikoVariable.java
@@ -14,41 +14,7 @@ public enum BrikoVariable implements Variable<BrikoVariable> {
   EMG9,
   EMG10,
   EMG11,
-  EMG12,
-
-  R1,
-  R2,
-  R3,
-  R4,
-  R5,
-  R6,
-  R7,
-  R8,
-  R9,
-  R10,
-  R11,
-  R12,
-
-  MTG1,
-  MTG2,
-  MTG3,
-  MTG4,
-  MTG5,
-  MTG6,
-  MTG7,
-  MTG8,
-  MTG9,
-  MTG10,
-  MTG11,
-  MTG12,
-
-  AX,
-  AY,
-  AZ,
-
-  GX,
-  GY,
-  GZ;
+  EMG12;
 
   public static final int FREQUENCY = 512;
 }
