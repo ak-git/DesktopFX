caf01f90ea3fd999c9708f384324f7d6b600cc63
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-08-04 14:53:35 +0300

    #F-265 In Progress Special protocol for Andrey Briko MAX30001 aggregator: 6 EMG + 6 R visible

--- src/main/java/com/ak/appliance/briko/comm/converter/BrikoVariable.java
+++ src/main/java/com/ak/appliance/briko/comm/converter/BrikoVariable.java
@@ -2,6 +2,9 @@ package com.ak.appliance.briko.comm.converter;
 
 import com.ak.comm.converter.Variable;
 
+import java.util.Collections;
+import java.util.Set;
+
 public enum BrikoVariable implements Variable<BrikoVariable> {
   EMG1,
   EMG2,
@@ -9,12 +12,84 @@ public enum BrikoVariable implements Variable<BrikoVariable> {
   EMG4,
   EMG5,
   EMG6,
-  EMG7,
-  EMG8,
-  EMG9,
-  EMG10,
-  EMG11,
-  EMG12;
+  EMG7 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  EMG8 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  EMG9 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  EMG10 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  EMG11 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  EMG12 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  R1,
+  R2,
+  R3,
+  R4,
+  R5,
+  R6,
+  R7 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  R8 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  R9 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  R10 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  R11 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  },
+  R12 {
+    @Override
+    public Set<Option> options() {
+      return Collections.emptySet();
+    }
+  };
 
   public static final int FREQUENCY = 512;
 }
