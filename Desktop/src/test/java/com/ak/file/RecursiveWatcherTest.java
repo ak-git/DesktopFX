ad008720d669be85ee4b35fd50b07c66fb878582
Alexander Kobelev <ak.mail.ru@gmail.com>
2025-07-05 16:53:24 +0300

    #F-275 Fixed Use @TempDir for File and Path test

--- Desktop/src/test/java/com/ak/file/RecursiveWatcherTest.java
+++ Desktop/src/test/java/com/ak/file/RecursiveWatcherTest.java
@@ -1,43 +1,22 @@
 package com.ak.file;
 
-import com.ak.util.Clean;
 import com.ak.util.Extension;
 import com.ak.util.Strings;
 import com.ak.util.UIConstants;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class RecursiveWatcherTest {
-  private static final Path PATH;
-
-  static {
-    try {
-      PATH = Files.createTempDirectory("test %s.".formatted(RecursiveWatcherTest.class.getPackageName()));
-    }
-    catch (IOException e) {
-      Assertions.fail(e);
-      throw new RuntimeException(e);
-    }
-  }
-
-  @AfterAll
-  static void cleanUp() {
-    Clean.clean(Objects.requireNonNull(PATH));
-  }
-
   @Test
-  void test() throws IOException, InterruptedException {
-    Path path = Objects.requireNonNull(PATH);
+  void test(@TempDir Path path) throws IOException, InterruptedException {
     assertNotNull(Files.createTempFile(Files.createDirectories(path), Strings.EMPTY, Extension.TXT.attachTo(Strings.EMPTY)));
     Path subDir = Files.createTempDirectory(path, Strings.EMPTY);
     assertNotNull(subDir, path::toString);
