02a3a04b255c33f6997aec2dff2cac0fe23fbde5
Alexander Kobelev <ak.mail.ru@gmail.com>
2026-03-20 12:41:07 +0300

    #F-285 In Progress New library for Resistive Sounding Inverse solution: MisfitTest.java

--- Util/src/test/java/com/ak/rsm2/MisfitTest.java
+++ Util/src/test/java/com/ak/rsm2/MisfitTest.java
@@ -10,11 +10,12 @@ import static org.assertj.core.api.Assertions.byLessThan;
 class MisfitTest {
   @ParameterizedTest
   @CsvSource(delimiter = ',', textBlock = """
-      30.971, 31.278, -0.05, 5.0
+      10.0, 30.0, 30.971, 31.278, -0.05, 5.0
+      50.0, 30.0, 62.479, 61.860,  0.05, 5.0
       """)
-  void water(double rBefore, double rAfter, double dHmm, double expectedHmm) {
+  void water(double smm, double lmm, double rBefore, double rAfter, double dHmm, double expectedHmm) {
     Misfit misfit = Misfit.builder()
-        .ofMilli(s -> s.tetrapolar(10.0, 30.0).absError(0.1))
+        .ofMilli(s -> s.tetrapolar(smm, lmm).absError(0.1))
         .measurements(m -> m.ohms(rBefore).dhMilli(dHmm).thenOhms(rAfter))
         .build();
 
