e504f6ab8d3e1e2773d3357c69eb22e0dc9caad8
Alexander Kobelev <ak.mail.ru@gmail.com>
2026-04-11 23:28:00 +0300

    #F-285 In Progress New library for Resistive Sounding Inverse solution: introduce Solver

---
+++ Util/src/main/java/com/ak/rsm2/Solver.java
@@ -0,0 +1,134 @@
+package com.ak.rsm2;
+
+import com.ak.math.Simplex;
+import com.ak.math.ValuePair;
+import com.ak.util.Builder;
+import com.ak.util.Metrics;
+import com.ak.util.Strings;
+import org.apache.commons.math4.legacy.optim.InitialGuess;
+import org.apache.commons.math4.legacy.optim.MaxEval;
+import org.apache.commons.math4.legacy.optim.PointValuePair;
+import org.apache.commons.math4.legacy.optim.nonlinear.scalar.GoalType;
+import org.apache.commons.math4.legacy.optim.nonlinear.scalar.ObjectiveFunction;
+import org.apache.commons.math4.legacy.optim.nonlinear.scalar.noderiv.NelderMeadTransform;
+import org.apache.commons.math4.legacy.optim.nonlinear.scalar.noderiv.SimplexOptimizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.function.DoubleFunction;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+
+public sealed interface Solver {
+  static Step1 of(double base, Metrics.Length units) {
+    return new SolverBuilder(base, units);
+  }
+
+  sealed interface Step1 {
+    Step2 system1x3(Function<TetrapolarMeasurement.Step1, Builder<TetrapolarMeasurement>> builderFunction);
+  }
+
+  sealed interface Step2 {
+    Builder<Solver> system5x3(Function<TetrapolarMeasurement.Step1, Builder<TetrapolarMeasurement>> builderFunction);
+  }
+
+  final class SolverBuilder implements Step1, Step2, Builder<Solver> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SolverBuilder.class);
+
+    private record SolverRecord() implements Solver {
+    }
+
+    private final double base;
+    private final Metrics.Length units;
+    private final Collection<Misfit> misfits = new ArrayList<>();
+
+    public SolverBuilder(double base, Metrics.Length units) {
+      if (base > 0) {
+        this.base = base;
+      }
+      else {
+        throw new IllegalArgumentException("base = %f must be positive".formatted(base));
+      }
+      this.units = units;
+    }
+
+    @Override
+    public Step2 system1x3(Function<TetrapolarMeasurement.Step1, Builder<TetrapolarMeasurement>> builderFunction) {
+      systemX3(1, builderFunction);
+      return this;
+    }
+
+    @Override
+    public Builder<Solver> system5x3(Function<TetrapolarMeasurement.Step1, Builder<TetrapolarMeasurement>> builderFunction) {
+      systemX3(5, builderFunction);
+      return this;
+    }
+
+    private void systemX3(int factorFirst, Function<TetrapolarMeasurement.Step1, Builder<TetrapolarMeasurement>> builderFunction) {
+      misfits.add(Misfit.builder(units)
+          .system(s -> s.tetrapolar(base * factorFirst, base * 3).absError(0.1))
+          .measurements(_ -> builderFunction.apply(TetrapolarMeasurement.builder(units)))
+          .build());
+    }
+
+    @Override
+    public Solver build() {
+      double dataErrorNorm = misfits.stream().mapToDouble(Misfit::dataErrorNorm).reduce(Math::hypot).orElseThrow();
+      LOGGER.atInfo().addKeyValue("data Error Norm", "%.4f".formatted(dataErrorNorm)).log(Strings.EMPTY);
+
+      DoubleFunction<Model.Layer2Relative> find = alpha -> {
+        PointValuePair optimized = Simplex.optimizeAll(point -> {
+              Model.Layer2Relative m = new Model.Layer2Relative(point[0], point[1]);
+              double s = misfits.stream().mapToDouble(f -> f.regularization(Misfit.Regularization.ZERO_MAX_LOG).applyAsDouble(m)).sum();
+              if (Double.isFinite(s)) {
+                double misfit = misfits.stream().mapToDouble(f -> f.misfit().applyAsDouble(m)).reduce(Math::hypot).orElseThrow();
+                return misfit * misfit + Math.abs(alpha) * s;
+              }
+              else {
+                return Double.POSITIVE_INFINITY;
+              }
+            },
+            new Simplex.Bounds(-1.0, 1.0), new Simplex.Bounds(0.0, 0.04));
+        double[] point = optimized.getPoint();
+        return new Model.Layer2Relative(point[0], point[1]);
+      };
+
+      DoubleUnaryOperator withAlpha = new DoubleUnaryOperator() {
+        private final double dataErrorNorm = misfits.stream().mapToDouble(Misfit::dataErrorNorm).reduce(Math::hypot).orElseThrow();
+
+        @Override
+        public double applyAsDouble(double alpha) {
+          if (alpha < 0) {
+            return Double.POSITIVE_INFINITY;
+          }
+          else {
+            Model.Layer2Relative m = find.apply(alpha);
+            double misfit = misfits.stream().mapToDouble(f -> f.misfit().applyAsDouble(m)).reduce(Math::hypot).orElseThrow();
+            LOGGER.atInfo().addKeyValue("alpha", "%.4f".formatted(alpha)).addKeyValue("misfit", "%.4f".formatted(misfit))
+                .log(() -> "%s; %s".formatted(
+                    ValuePair.Name.K12.of(find.apply(alpha).k().value(), 0.0),
+                    ValuePair.Name.H.of(find.apply(alpha).h(), 0.0))
+                );
+            double v = misfit - dataErrorNorm;
+            return v * v;
+          }
+        }
+      };
+
+      PointValuePair optimized = new SimplexOptimizer(0.000_000_1, 0.000_01)
+          .optimize(new MaxEval(100), new ObjectiveFunction(point -> withAlpha.applyAsDouble(point[0])),
+              GoalType.MINIMIZE, org.apache.commons.math4.legacy.optim.nonlinear.scalar.noderiv.Simplex.alongAxes(new double[] {0.001}),
+              new NelderMeadTransform(), new InitialGuess(new double[] {0.0})
+          );
+      double alpha = optimized.getPoint()[0];
+      LOGGER.atWarn().addKeyValue("alpha", () -> "%.4f".formatted(alpha))
+          .log("%s; %s".formatted(
+              ValuePair.Name.K12.of(find.apply(alpha).k().value(), 0.0),
+              ValuePair.Name.H.of(find.apply(alpha).h(), 0.0)));
+
+      return new SolverRecord();
+    }
+  }
+}
--- Util/src/test/java/com/ak/rsm2/InverseTest.java
+++ Util/src/test/java/com/ak/rsm2/InverseTest.java
@@ -1,17 +1,6 @@
 package com.ak.rsm2;
 
-import com.ak.math.Simplex;
-import com.ak.math.ValuePair;
 import com.ak.util.Metrics;
-import com.ak.util.Strings;
-import org.apache.commons.math4.legacy.optim.InitialGuess;
-import org.apache.commons.math4.legacy.optim.MaxEval;
-import org.apache.commons.math4.legacy.optim.PointValuePair;
-import org.apache.commons.math4.legacy.optim.nonlinear.scalar.GoalType;
-import org.apache.commons.math4.legacy.optim.nonlinear.scalar.ObjectiveFunction;
-import org.apache.commons.math4.legacy.optim.nonlinear.scalar.noderiv.NelderMeadTransform;
-import org.apache.commons.math4.legacy.optim.nonlinear.scalar.noderiv.SimplexOptimizer;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,26 +8,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.function.DoubleFunction;
-import java.util.function.DoubleUnaryOperator;
-import java.util.stream.Stream;
-
 class InverseTest {
   private static final Logger LOGGER = LoggerFactory.getLogger(InverseTest.class);
 
   @Nested
   class WaterTest {
-    @BeforeAll
-    static void dataErrorNorm() {
-      double dataErrorNorm = Stream.of(
-              ElectrodeSystem.builder(Metrics.Length.MILLI).tetrapolar(10.0, 30.0).absError(0.1).build(),
-              ElectrodeSystem.builder(Metrics.Length.MILLI).tetrapolar(50.0, 30.0).absError(0.1).build())
-          .mapToDouble(ElectrodeSystem.Inexact::dataErrorNorm).reduce(Math::hypot).orElseThrow();
-      LOGGER.atInfo().addKeyValue("data Error Norm", "%.4f".formatted(dataErrorNorm)).log(Strings.EMPTY);
-    }
-
     @Disabled("26.734, 46.074, 26.719, 46.028, 0.450")
     @ParameterizedTest
     @CsvSource(delimiter = ',', textBlock = """
@@ -51,66 +25,10 @@ class InverseTest {
         11.361, 17.674, 11.362, 17.678, -0.05
         """)
     void waterParameters(double r1, double r2, double r1After, double r2After, double dHmm) {
-      Collection<Misfit> misfits = List.of(
-          Misfit.builder(Metrics.Length.MILLI)
-              .system(s -> s.tetrapolar(10.0, 30.0).absError(0.1))
-              .measurements(m -> m.ohms(r1).dh(dHmm).thenOhms(r1After))
-              .build(),
-          Misfit.builder(Metrics.Length.MILLI)
-              .system(s -> s.tetrapolar(50.0, 30.0).absError(0.1))
-              .measurements(m -> m.ohms(r2).dh(dHmm).thenOhms(r2After))
-              .build()
-      );
-
-      DoubleFunction<Model.Layer2Relative> find = alpha -> {
-        PointValuePair optimized = Simplex.optimizeAll(point -> {
-              Model.Layer2Relative m = new Model.Layer2Relative(point[0], point[1]);
-              double s = misfits.stream().mapToDouble(f -> f.regularization(Misfit.Regularization.ZERO_MAX_LOG).applyAsDouble(m)).sum();
-              if (Double.isFinite(s)) {
-                double misfit = misfits.stream().mapToDouble(f -> f.misfit().applyAsDouble(m)).reduce(Math::hypot).orElseThrow();
-                return misfit * misfit + Math.abs(alpha) * s;
-              }
-              else {
-                return Double.POSITIVE_INFINITY;
-              }
-            },
-            new Simplex.Bounds(-1.0, 1.0), new Simplex.Bounds(0.0, 0.04));
-        double[] point = optimized.getPoint();
-        return new Model.Layer2Relative(point[0], point[1]);
-      };
-
-      DoubleUnaryOperator withAlpha = new DoubleUnaryOperator() {
-        private final double dataErrorNorm = misfits.stream().mapToDouble(Misfit::dataErrorNorm).reduce(Math::hypot).orElseThrow();
-
-        @Override
-        public double applyAsDouble(double alpha) {
-          if (alpha < 0) {
-            return Double.POSITIVE_INFINITY;
-          }
-          else {
-            Model.Layer2Relative m = find.apply(alpha);
-            double misfit = misfits.stream().mapToDouble(f -> f.misfit().applyAsDouble(m)).reduce(Math::hypot).orElseThrow();
-            LOGGER.atInfo().addKeyValue("alpha", "%.4f".formatted(alpha)).addKeyValue("misfit", "%.4f".formatted(misfit))
-                .log(() -> "%s; %s".formatted(
-                    ValuePair.Name.K12.of(find.apply(alpha).k().value(), 0.0),
-                    ValuePair.Name.H.of(find.apply(alpha).h(), 0.0))
-                );
-            double v = misfit - dataErrorNorm;
-            return v * v;
-          }
-        }
-      };
-
-      PointValuePair optimized = new SimplexOptimizer(0.000_000_1, 0.000_01)
-          .optimize(new MaxEval(100), new ObjectiveFunction(point -> withAlpha.applyAsDouble(point[0])),
-              GoalType.MINIMIZE, org.apache.commons.math4.legacy.optim.nonlinear.scalar.noderiv.Simplex.alongAxes(new double[] {0.001}),
-              new NelderMeadTransform(), new InitialGuess(new double[] {0.0})
-          );
-      double alpha = optimized.getPoint()[0];
-      LOGGER.atWarn().addKeyValue("alpha", () -> "%.4f".formatted(alpha))
-          .log("%s; %s".formatted(
-              ValuePair.Name.K12.of(find.apply(alpha).k().value(), 0.0),
-              ValuePair.Name.H.of(find.apply(alpha).h(), 0.0)));
+      Solver solver = Solver.of(10.0, Metrics.Length.MILLI)
+          .system1x3(m -> m.ohms(r1).dh(dHmm).thenOhms(r1After))
+          .system5x3(m -> m.ohms(r2).dh(dHmm).thenOhms(r2After)).build();
+      LOGGER.atInfo().log(solver::toString);
     }
   }
 }
