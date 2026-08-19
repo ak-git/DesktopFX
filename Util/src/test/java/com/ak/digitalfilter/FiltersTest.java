823fc81fa790bed08f403acf564d975b706519ae
Alexander Kobelev <ak.mail.ru@gmail.com>
2026-08-19 15:13:32 +0300

    #F-285 In Progress New library for Resistive Sounding Inverse solution: Simplify this regular expression to reduce its runtime, as it has super-linear performance due to backtracking

--- Util/src/test/java/com/ak/digitalfilter/FiltersTest.java
+++ Util/src/test/java/com/ak/digitalfilter/FiltersTest.java
@@ -3,7 +3,7 @@ package com.ak.digitalfilter;
 import com.ak.csv.CSVLineFileCollector;
 import com.ak.util.Extension;
 import com.ak.util.Strings;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -25,39 +25,38 @@ class FiltersTest {
     String filteredPrefix = "Filtered - ";
     int column = 0;
 
-    try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Path.of(Strings.EMPTY), Extension.CSV.attachTo("*"))) {
-      directoryStream.forEach(path -> {
-        if (!path.toString().startsWith(filteredPrefix)) {
-          DigitalFilter filter = FilterBuilder.of().smoothingImpulsive(10).buildNoDelay();
+    Assertions.assertDoesNotThrow(() -> {
+      try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Path.of(Strings.EMPTY), Extension.CSV.attachTo("*"))) {
+        directoryStream.forEach(path -> {
+          if (!path.toString().startsWith(filteredPrefix)) {
+            DigitalFilter filter = FilterBuilder.of().smoothingImpulsive(10).buildNoDelay();
 
-          try (CSVLineFileCollector collector = new CSVLineFileCollector(
-              Path.of(String.join(Strings.EMPTY, filteredPrefix, path.getFileName().toString())))) {
-            filter.forEach(values ->
-                collector.accept(Arrays.stream(values).mapToObj(String::valueOf).toArray()));
+            try (CSVLineFileCollector collector = new CSVLineFileCollector(
+                Path.of(String.join(Strings.EMPTY, filteredPrefix, path.getFileName().toString())))) {
+              filter.forEach(values ->
+                  collector.accept(Arrays.stream(values).mapToObj(String::valueOf).toArray()));
 
-            try (Stream<String> lines = Files.lines(path)) {
-              lines.filter(s -> s.matches("\\d+.*")).mapToInt(value -> {
-                try {
-                  return NumberFormat.getIntegerInstance().parse(value.split("\\t")[column]).intValue();
-                }
-                catch (ParseException e) {
-                  Logger.getLogger(FiltersTest.class.getName()).log(Level.INFO, e.getMessage(), e);
-                  return 0;
-                }
-              }).forEach(filter::accept);
+              try (Stream<String> lines = Files.lines(path)) {
+                lines.filter(s -> !s.isEmpty() && Character.isDigit(s.charAt(0))).mapToInt(value -> {
+                  try {
+                    return NumberFormat.getIntegerInstance().parse(value.split("\\t")[column]).intValue();
+                  }
+                  catch (ParseException e) {
+                    Logger.getLogger(FiltersTest.class.getName()).log(Level.INFO, e.getMessage(), e);
+                    return 0;
+                  }
+                }).forEach(filter::accept);
+              }
+              catch (IOException e) {
+                Logger.getLogger(FiltersTest.class.getName()).log(Level.INFO, e.getMessage(), e);
+              }
             }
             catch (IOException e) {
               Logger.getLogger(FiltersTest.class.getName()).log(Level.INFO, e.getMessage(), e);
             }
           }
-          catch (IOException e) {
-            Logger.getLogger(FiltersTest.class.getName()).log(Level.INFO, e.getMessage(), e);
-          }
-        }
-      });
-    }
-    catch (IOException e) {
-      Assertions.fail(e.getMessage(), e);
-    }
+        });
+      }
+    });
   }
 }
