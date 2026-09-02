b1f291fe413d5bedaefe1d5dc53c7f55f11c19a9
Alexander Kobelev <ak.mail.ru@gmail.com>
2026-09-02 21:14:53 +0300

    #F-285 In Progress New library for Resistive Sounding Inverse solution: generic cache

--- Util/src/test/java/com/ak/rsm2/GeneticInvertibleFilter.java
+++ Util/src/test/java/com/ak/rsm2/GeneticInvertibleFilter.java
@@ -1,5 +1,7 @@
 package com.ak.rsm2;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import io.jenetics.*;
 import io.jenetics.engine.*;
 import io.jenetics.util.Factory;
@@ -9,9 +11,14 @@ import org.slf4j.LoggerFactory;
 import java.io.Serializable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.LongAdder;
 
 public class GeneticInvertibleFilter {
   private static final Logger LOGGER = LoggerFactory.getLogger(GeneticInvertibleFilter.class);
+
+  private static final Cache<ProblemInput, Double> FITNESS_CACHE = Caffeine.newBuilder().maximumSize(1 << 16).build();
+  private static final LongAdder REAL_EVALUATIONS_COUNTER = new LongAdder();
+  private static final LongAdder TOTAL_EVALUATIONS_COUNTER = new LongAdder();
 
   private GeneticInvertibleFilter() {
   }
@@ -24,27 +31,31 @@ public class GeneticInvertibleFilter {
    * Глобальный минимум равен 0.0 и достигается при: x = 1.0, y = 1, z = true.
    */
   public static double fitness(ProblemInput input) {
-    double x = input.x();
-    double y = input.y();
-    boolean z = input.z();
+    TOTAL_EVALUATIONS_COUNTER.increment();
+    return FITNESS_CACHE.get(input, in -> {
+      REAL_EVALUATIONS_COUNTER.increment();
+      // Классический овраг Розенброка: f(x,y) = 100 * (y - x^2)^2 + (1 - x)^2
+      // Но y у нас дискретный (int), что превращает овраг в каскад ступеней.
+      double ravine = 100.0 * StrictMath.pow(in.y() - (in.x() * in.x()), 2) + StrictMath.pow(1.0 - in.x(), 2);
 
-    // Классический овраг Розенброка: f(x,y) = 100 * (y - x^2)^2 + (1 - x)^2
-    // Но y у нас дискретный (int), что превращает овраг в каскад ступеней.
-    double rosenbrockRavine = 100.0 * StrictMath.pow(y - (x * x), 2) + StrictMath.pow(1.0 - x, 2);
-
-    if (z) {
-      // Мир TRUE: Чистый овраг, ведущий к точке (1.0, 1, true), где fitness = 0.0
-      return rosenbrockRavine;
-    }
-    else {
-      // Мир FALSE: Овраг искажен синусоидальными ловушками (локальными минимумами).
-      // Даже если алгоритм дойдет до центра оврага, он получит штраф и застрянет.
-      double traps = 50.0 * (StrictMath.sin(5.0 * x) + 1.0);
-      return rosenbrockRavine + traps + 10.0; // +10 гарантирует, что здесь нет глобального минимума
-    }
+      if (in.z()) {
+        // Мир TRUE: Чистый овраг, ведущий к точке (1.0, 1, true), где fitness = 0.0
+        return ravine;
+      }
+      else {
+        // Мир FALSE: Овраг искажен синусоидальными ловушками (локальными минимумами).
+        // Даже если алгоритм дойдет до центра оврага, он получит штраф и застрянет.
+        double traps = 50.0 * (StrictMath.sin(5.0 * in.x()) + 1.0);
+        return ravine + traps + 10.0; // +10 гарантирует, что здесь нет глобального минимума
+      }
+    });
   }
 
   static void main() {
+    FITNESS_CACHE.cleanUp();
+    REAL_EVALUATIONS_COUNTER.reset();
+    TOTAL_EVALUATIONS_COUNTER.reset();
+
     InvertibleCodec<ProblemInput, AnyGene<Serializable>> compositeCodec = getCompositeCodec();
 
     Constraint<AnyGene<Serializable>, Double> codecFiniteConstraint = RetryConstraint.of(compositeCodec, input -> Double.isFinite(fitness(input)));
@@ -61,7 +72,7 @@ public class GeneticInvertibleFilter {
     // 1. Создаем пул виртуальных потоков Java 26
     try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
       Engine<AnyGene<Serializable>, Double> engine = Engine.builder(GeneticInvertibleFilter::fitness, compositeCodec)
-          .populationSize(1024)
+          .populationSize(1 << 16)
           .optimize(Optimize.MINIMUM)
           .executor(executor)
           // 4. Подключаем селекторы правильно через методы билдера Jenetics
@@ -89,7 +100,11 @@ public class GeneticInvertibleFilter {
           .collect(EvolutionResult.toBestPhenotype());
 
       ProblemInput bestInput = compositeCodec.decode(best.genotype());
-      LOGGER.atInfo().addKeyValue("fitness", best.fitness()).log(bestInput.toString());
+      LOGGER.atInfo().addKeyValue("Невязка", best::fitness)
+          .addKeyValue("Вычислений", REAL_EVALUATIONS_COUNTER::sum)
+          .addKeyValue("Всего попыток", TOTAL_EVALUATIONS_COUNTER::sum)
+          .addKeyValue("Экономия за счет глобального кэша", "%.0f%%".formatted((1.0 - REAL_EVALUATIONS_COUNTER.doubleValue() / TOTAL_EVALUATIONS_COUNTER.sum()) * 100))
+          .log(bestInput.toString());
     }
   }
 
